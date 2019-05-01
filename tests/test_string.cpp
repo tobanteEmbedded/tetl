690ec1e930fa5785ac4afce80c00aa3724aa524a
Tobias Hienzsch <post@tobias-hienzsch.de>
2019-05-02 01:03:47 +0200

    Switch test_string to micro_catch

--- tests/test_string.cpp
+++ tests/test_string.cpp
@@ -27,15 +27,12 @@ DAMAGE.
 #include <assert.h>  // assert
 #include <stdio.h>   // printf
 
+// MICROCATCH
+#include "micro_catch/micro_catch.hpp"
+
 // TAETL
 #include "taetl/algorithm.hpp"
 #include "taetl/string.hpp"
-
-/** Handy function for avoiding unused variables warning. */
-template <typename... Types>
-constexpr void ignoreUnused(Types&&...) noexcept
-{
-}
 
 int main()
 {
@@ -43,69 +40,81 @@ int main()
     taetl::String<char, 16> t_string{};
 
     // INIT
-    assert(t_string.empty() == true);
-    assert(t_string.capacity() == 16);
-    assert(t_string.size() == 0);
-    assert(t_string.length() == 0);
+    microcatch::EQUAL(t_string.empty(), true);
+    microcatch::EQUAL(t_string.capacity(), taetl::size_t(16));
+    microcatch::EQUAL(t_string.size(), taetl::size_t(0));
+    microcatch::EQUAL(t_string.length(), taetl::size_t(0));
+
+    taetl::for_each(t_string.begin(), t_string.end(), [](auto& c) {
+        microcatch::ignoreUnused(c);
+        microcatch::EQUAL(c, 0);
+    });
+
+    taetl::for_each(t_string.cbegin(), t_string.cend(), [](const auto& c) {
+        microcatch::ignoreUnused(c);
+        microcatch::EQUAL(c, 0);
+    });
 
     for (const auto& c : t_string)
     {
-        ignoreUnused(c);
-        assert(c == 0);
+        microcatch::ignoreUnused(c);
+        microcatch::EQUAL(c, 0);
     }
 
     // APPEND 4 CHARACTERS
     const char* cptr = "C-string";
     t_string.append(cptr, 4);
 
-    assert(t_string.empty() == false);
-    assert(t_string.capacity() == 16);
-    assert(t_string.size() == 4);
-    assert(t_string.length() == 4);
-    assert(t_string[0] == 'C');
-    assert(t_string[1] == '-');
-    assert(t_string[2] == 's');
-    assert(t_string[3] == 't');
-    assert(t_string[4] == 0);
-    assert(t_string.at(4) == 0);
+    microcatch::EQUAL(t_string.empty(), false);
+    microcatch::EQUAL(t_string.capacity(), taetl::size_t(16));
+    microcatch::EQUAL(t_string.size(), taetl::size_t(4));
+    microcatch::EQUAL(t_string.length(), taetl::size_t(4));
+    microcatch::EQUAL(t_string[0], 'C');
+    microcatch::EQUAL(t_string[1], '-');
+    microcatch::EQUAL(t_string[2], 's');
+    microcatch::EQUAL(t_string[3], 't');
+    microcatch::EQUAL(t_string[4], 0);
+    microcatch::EQUAL(t_string.at(4), 0);
 
     // APPEND 5X SAME CHARACTER
     t_string.append(5, 'a');
+    const char first_char = t_string[0];
 
-    assert(t_string.empty() == false);
-    assert(t_string.capacity() == 16);
-    assert(t_string.size() == 9);
-    assert(t_string.length() == 9);
-    assert(t_string[0] == 'C');
-    assert(t_string[1] == '-');
-    assert(t_string[2] == 's');
-    assert(t_string[3] == 't');
-    assert(t_string[4] == 'a');
-    assert(t_string[5] == 'a');
-    assert(t_string[6] == 'a');
-    assert(t_string[7] == 'a');
-    assert(t_string[8] == 'a');
-    assert(t_string[9] == 0);
-    assert(t_string.at(9) == 0);
+    microcatch::EQUAL(t_string.empty(), false);
+    microcatch::EQUAL(t_string.capacity(), taetl::size_t(16));
+    microcatch::EQUAL(t_string.size(), taetl::size_t(9));
+    microcatch::EQUAL(t_string.length(), taetl::size_t(9));
+    microcatch::EQUAL(first_char, 'C');
+    microcatch::EQUAL(t_string[0], 'C');
+    microcatch::EQUAL(t_string[1], '-');
+    microcatch::EQUAL(t_string[2], 's');
+    microcatch::EQUAL(t_string[3], 't');
+    microcatch::EQUAL(t_string[4], 'a');
+    microcatch::EQUAL(t_string[5], 'a');
+    microcatch::EQUAL(t_string[6], 'a');
+    microcatch::EQUAL(t_string[7], 'a');
+    microcatch::EQUAL(t_string[8], 'a');
+    microcatch::EQUAL(t_string[9], 0);
+    microcatch::EQUAL(t_string.at(9), 0);
 
     // APPLY ALGORITHM
     taetl::for_each(t_string.begin(), t_string.end(), [](auto& c) { c += 1; });
-    assert(t_string[4] == 'b');
-    assert(t_string[5] == 'b');
-    assert(t_string[6] == 'b');
-    assert(t_string[7] == 'b');
-    assert(t_string[8] == 'b');
+    microcatch::EQUAL(t_string[4], 'b');
+    microcatch::EQUAL(t_string[5], 'b');
+    microcatch::EQUAL(t_string[6], 'b');
+    microcatch::EQUAL(t_string[7], 'b');
+    microcatch::EQUAL(t_string[8], 'b');
 
     // CLEAR
     t_string.clear();
-    assert(t_string.capacity() == 16);
-    assert(t_string.empty());
-    assert(t_string.size() == 0);
+    microcatch::EQUAL(t_string.capacity(), taetl::size_t(16));
+    microcatch::EQUAL(t_string.empty(), true);
+    microcatch::EQUAL(t_string.size(), taetl::size_t(0));
 
     for (const auto& c : t_string)
     {
-        ignoreUnused(c);
-        assert(c == 0);
+        microcatch::ignoreUnused(c);
+        microcatch::EQUAL(c, 0);
     }
 
     return 0;
