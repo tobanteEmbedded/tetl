2104ddf828a579063cfb386e5902fadf2ae3c135
Tobias Hienzsch <post@tobias-hienzsch.de>
2019-05-02 00:05:36 +0200

    Add array tests for empty & pop_back

--- tests/test_array.cpp
+++ tests/test_array.cpp
@@ -9,6 +9,9 @@ int main()
     // Create array with capacity of 16 and size of 0
     taetl::Array<int, 16> t_array;
 
+    // Empty
+    assert(t_array.empty());
+
     // Add 2 elements to the back
     t_array.push_back(1);
     t_array.push_back(2);
@@ -19,6 +22,7 @@ int main()
         assert(item != 0);
     }
 
+    assert(t_array.empty() == false);
     assert(t_array[0] == 1);
     assert(t_array[1] == 2);
     assert(t_array.capacity() == 16);
@@ -30,10 +34,19 @@ int main()
         item += 1;
     }
 
+    assert(t_array.empty() == false);
     assert(t_array[0] == 2);
     assert(t_array[1] == 3);
     assert(t_array.capacity() == 16);
     assert(t_array.size() == 2);
+
+    // POP BACK
+    t_array.pop_back();
+
+    assert(t_array.empty() == false);
+    assert(t_array[0] == 2);
+    assert(t_array.capacity() == 16);
+    assert(t_array.size() == 1);
 
     return 0;
 }
