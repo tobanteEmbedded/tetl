5b7f89a3acf597c1fd361ec609051c09054d81cb
Tobias Hienzsch <post@tobias-hienzsch.de>
2020-09-01 23:34:13 +0200

    system_error: Add header with errc enum

---
+++ etl/system_error.hpp
@@ -0,0 +1,137 @@
+/*
+Copyright (c) 2019-2020, Tobias Hienzsch
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+ * Redistributions of source code must retain the above copyright notice,
+   this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright
+   notice, this list of conditions and the following disclaimer in the
+   documentation and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND ANY
+EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+DAMAGE.
+*/
+
+#ifndef TAETL_SYSTEM_ERROR_HPP
+#define TAETL_SYSTEM_ERROR_HPP
+
+#include "definitions.hpp"
+#include "type_traits.hpp"
+
+namespace etl
+{
+template <class T>
+struct is_error_code_enum : public false_type
+{
+};
+
+template <class T>
+struct is_error_condition_enum : public false_type
+{
+};
+
+/**
+ * @brief The scoped enumeration etl::errc defines the values of portable error
+ * conditions that correspond to the POSIX error codes.
+ */
+enum class errc
+{
+    address_family_not_supported,        // EAFNOSUPPORT
+    address_in_use,                      // EADDRINUSE
+    address_not_available,               // EADDRNOTAVAIL
+    already_connected,                   // EISCONN
+    argument_list_too_long,              // E2BIG
+    argument_out_of_domain,              // EDOM
+    bad_address,                         // EFAULT
+    bad_file_descriptor,                 // EBADF
+    bad_message,                         // EBADMSG
+    broken_pipe,                         // EPIPE
+    connection_aborted,                  // ECONNABORTED
+    connection_already_in_progress,      // EALREADY
+    connection_refused,                  // ECONNREFUSED
+    connection_reset,                    // ECONNRESET
+    cross_device_link,                   // EXDEV
+    destination_address_required,        // EDESTADDRREQ
+    device_or_resource_busy,             // EBUSY
+    directory_not_empty,                 // ENOTEMPTY
+    executable_format_error,             // ENOEXEC
+    file_exists,                         // EEXIST
+    file_too_large,                      // EFBIG
+    filename_too_long,                   // ENAMETOOLONG
+    function_not_supported,              // ENOSYS
+    host_unreachable,                    // EHOSTUNREACH
+    identifier_removed,                  // EIDRM
+    illegal_byte_sequence,               // EILSEQ
+    inappropriate_io_control_operation,  // ENOTTY
+    interrupted,                         // EINTR
+    invalid_argument,                    // EINVAL
+    invalid_seek,                        // ESPIPE
+    io_error,                            // EIO
+    is_a_directory,                      // EISDIR
+    message_size,                        // EMSGSIZE
+    network_down,                        // ENETDOWN
+    network_reset,                       // ENETRESET
+    network_unreachable,                 // ENETUNREACH
+    no_buffer_space,                     // ENOBUFS
+    no_child_process,                    // ECHILD
+    no_link,                             // ENOLINK
+    no_lock_available,                   // ENOLCK
+    no_message_available,                // ENODATA
+    no_message,                          // ENOMSG
+    no_protocol_option,                  // ENOPROTOOPT
+    no_space_on_device,                  // ENOSPC
+    no_stream_resources,                 // ENOSR
+    no_such_device_or_address,           // ENXIO
+    no_such_device,                      // ENODEV
+    no_such_file_or_directory,           // ENOENT
+    no_such_process,                     // ESRCH
+    not_a_directory,                     // ENOTDIR
+    not_a_socket,                        // ENOTSOCK
+    not_a_stream,                        // ENOSTR
+    not_connected,                       // ENOTCONN
+    not_enough_memory,                   // ENOMEM
+    not_supported,                       // ENOTSUP
+    operation_canceled,                  // ECANCELED
+    operation_in_progress,               // EINPROGRESS
+    operation_not_permitted,             // EPERM
+    operation_not_supported,             // EOPNOTSUPP
+    operation_would_block,               // EWOULDBLOCK
+    owner_dead,                          // EOWNERDEAD
+    permission_denied,                   // EACCES
+    protocol_error,                      // EPROTO
+    protocol_not_supported,              // EPROTONOSUPPORT
+    read_only_file_system,               // EROFS
+    resource_deadlock_would_occur,       // EDEADLK
+    resource_unavailable_try_again,      // EAGAIN
+    result_out_of_range,                 // ERANGE
+    state_not_recoverable,               // ENOTRECOVERABLE
+    stream_timeout,                      // ETIME
+    text_file_busy,                      // ETXTBSY
+    timed_out,                           // ETIMEDOUT
+    too_many_files_open_in_system,       // ENFILE
+    too_many_files_open,                 // EMFILE
+    too_many_links,                      // EMLINK
+    too_many_symbolic_link_levels,       // ELOOP
+    value_too_large,                     // EOVERFLOW
+    wrong_protocol_type,                 // EPROTOTYPE
+};
+
+template <>
+struct is_error_condition_enum<errc> : true_type
+{
+};
+}  // namespace etl
+
+#endif  // TAETL_SYSTEM_ERROR_HPP
