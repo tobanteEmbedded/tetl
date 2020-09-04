4b7a88c7dc3290308abaf7f15c971e5dcdff32e0
Tobias Hienzsch <post@tobias-hienzsch.de>
2020-09-04 23:25:15 +0200

    limits: Guard long long limits with macro

--- tests/test_limits.cpp
+++ tests/test_limits.cpp
@@ -78,8 +78,8 @@ TEST_CASE("limits: numeric_limits<bool>", "[limits]")
 }
 
 TEMPLATE_TEST_CASE("limits: numeric_limits<signed T>", "[limits]", char, short,
-                   int, long, signed char, signed short, signed int,
-                   signed long)
+                   int, long, long long, signed char, signed short, signed int,
+                   signed long, signed long long)
 {
     using l = etl::numeric_limits<TestType>;
 
@@ -99,7 +99,8 @@ TEMPLATE_TEST_CASE("limits: numeric_limits<signed T>", "[limits]", char, short,
 }
 
 TEMPLATE_TEST_CASE("limits: numeric_limits<unsigned T>", "[limits]",
-                   unsigned char, unsigned short, unsigned int, unsigned long)
+                   unsigned char, unsigned short, unsigned int, unsigned long,
+                   unsigned long long)
 {
     using l = etl::numeric_limits<TestType>;
 
