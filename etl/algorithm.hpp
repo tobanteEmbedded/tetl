0681b11f66f2e03d4337b9324a359637e0a6a6f7
Tobias Hienzsch <post@tobias-hienzsch.de>
2020-09-04 08:37:01 +0200

    algorithm: Simplify lexicographical_compare

--- etl/algorithm.hpp
+++ etl/algorithm.hpp
@@ -278,25 +278,6 @@ template <class InputIt, class UnaryPredicate>
 
 /**
  * @brief Checks if the first range [first1, last1) is lexicographically less
- * than the second range [first2, last2). Elements are compared using operator<.
- *
- * @ref https://en.cppreference.com/w/cpp/algorithm/lexicographical_compare
- */
-template <class InputIt1, class InputIt2>
-[[nodiscard]] constexpr auto
-lexicographical_compare(InputIt1 first1, InputIt1 last1, InputIt2 first2,
-                        InputIt2 last2) -> bool
-{
-    for (; (first1 != last1) && (first2 != last2); ++first1, (void)++first2)
-    {
-        if (*first1 < *first2) { return true; }
-        if (*first2 < *first1) { return false; }
-    }
-    return (first1 == last1) && (first2 != last2);
-}
-
-/**
- * @brief Checks if the first range [first1, last1) is lexicographically less
  * than the second range [first2, last2). Elements are compared using the given
  * binary comparison function comp.
  *
@@ -314,6 +295,22 @@ lexicographical_compare(InputIt1 first1, InputIt1 last1, InputIt2 first2,
     }
     return (first1 == last1) && (first2 != last2);
 }
+
+/**
+ * @brief Checks if the first range [first1, last1) is lexicographically less
+ * than the second range [first2, last2). Elements are compared using operator<.
+ *
+ * @ref https://en.cppreference.com/w/cpp/algorithm/lexicographical_compare
+ */
+template <class InputIt1, class InputIt2>
+[[nodiscard]] constexpr auto
+lexicographical_compare(InputIt1 first1, InputIt1 last1, InputIt2 first2,
+                        InputIt2 last2) -> bool
+{
+    return lexicographical_compare(first1, last1, first2, last2,
+                                   etl::less<decltype(*first1)> {});
+}
+
 }  // namespace etl
 
 #endif  // TAETL_ALGORITHM_HPP
