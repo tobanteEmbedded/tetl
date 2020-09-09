f40784fd1599ee6ff3edfd8bffe5637fb79aec6a
Tobias Hienzsch <post@tobias-hienzsch.de>
2020-09-09 04:33:52 +0200

    numbers: Add header

---
+++ etl/numbers.hpp
@@ -0,0 +1,92 @@
+/*
+Copyright (c) 2019-2020, Tobias Hienzsch
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+ * Redistributions of source code must retain the above copyright notice,
+   this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright
+   notice, this list of conditions and the following disclaimer in the
+   documentation and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND ANY
+EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+DAMAGE.
+*/
+
+#ifndef TAETL_NUMBERS_HPP
+#define TAETL_NUMBERS_HPP
+
+#include "type_traits.hpp"
+
+namespace etl::numbers
+{
+template <class T> inline constexpr T e_v      = 2.7182818284590452353602874713526625L;
+template <class T> inline constexpr T log2e_v  = 1.4426950408889634073599246810018921L;
+template <class T> inline constexpr T log10e_v = 0.4342944819032518276511289189166051L;
+template <class T> inline constexpr T pi_v     = 3.1415926535897932384626433832795028L;
+template <class T> inline constexpr T inv_sqrtpi_v = 0.564189583547756286948079451560772L;
+template <class T> inline constexpr T inv_pi_v    = 0.3183098861837906715377675267450287L;
+template <class T> inline constexpr T ln2_v       = 0.6931471805599453094172321214581766L;
+template <class T> inline constexpr T ln10_v      = 2.3025850929940456840179914546843642L;
+template <class T> inline constexpr T sqrt2_v     = 1.4142135623730950488016887242096981L;
+template <class T> inline constexpr T sqrt3_v     = 1.7320508075688772935274463415058724L;
+template <class T> inline constexpr T inv_sqrt3_v = 0.5773502691896257645091487805019574L;
+template <class T> inline constexpr T egamma_v    = 0.5772156649015328606065120900824024L;
+template <class T> inline constexpr T phi_v       = 1.6180339887498948482045868343656381L;
+
+inline constexpr float ef          = e_v<float>;
+inline constexpr float log2ef      = log2e_v<float>;
+inline constexpr float log10ef     = log10e_v<float>;
+inline constexpr float pif         = pi_v<float>;
+inline constexpr float inv_pif     = inv_pi_v<float>;
+inline constexpr float inv_sqrtpif = inv_sqrtpi_v<float>;
+inline constexpr float ln2f        = ln2_v<float>;
+inline constexpr float ln10f       = ln10_v<float>;
+inline constexpr float sqrt2f      = sqrt2_v<float>;
+inline constexpr float sqrt3f      = sqrt3_v<float>;
+inline constexpr float inv_sqrt3f  = inv_sqrt3_v<float>;
+inline constexpr float egammaf     = egamma_v<float>;
+inline constexpr float phif        = phi_v<float>;
+
+inline constexpr double e          = e_v<double>;
+inline constexpr double log2e      = log2e_v<double>;
+inline constexpr double log10e     = log10e_v<double>;
+inline constexpr double pi         = pi_v<double>;
+inline constexpr double inv_pi     = inv_pi_v<double>;
+inline constexpr double inv_sqrtpi = inv_sqrtpi_v<double>;
+inline constexpr double ln2        = ln2_v<double>;
+inline constexpr double ln10       = ln10_v<double>;
+inline constexpr double sqrt2      = sqrt2_v<double>;
+inline constexpr double sqrt3      = sqrt3_v<double>;
+inline constexpr double inv_sqrt3  = inv_sqrt3_v<double>;
+inline constexpr double egamma     = egamma_v<double>;
+inline constexpr double phi        = phi_v<double>;
+
+inline constexpr long double el          = e_v<long double>;
+inline constexpr long double log2el      = log2e_v<long double>;
+inline constexpr long double log10el     = log10e_v<long double>;
+inline constexpr long double pil         = pi_v<long double>;
+inline constexpr long double inv_pil     = inv_pi_v<long double>;
+inline constexpr long double inv_sqrtpil = inv_sqrtpi_v<long double>;
+inline constexpr long double ln2l        = ln2_v<long double>;
+inline constexpr long double ln10l       = ln10_v<long double>;
+inline constexpr long double sqrt2l      = sqrt2_v<long double>;
+inline constexpr long double sqrt3l      = sqrt3_v<long double>;
+inline constexpr long double inv_sqrt3l  = inv_sqrt3_v<long double>;
+inline constexpr long double egammal     = egamma_v<long double>;
+inline constexpr long double phil        = phi_v<long double>;
+
+}  // namespace etl::numbers
+
+#endif  // TAETL_NUMBERS_HPP
---
+++ tests/test_numbers.cpp
@@ -0,0 +1,46 @@
+/*
+Copyright (c) 2019-2020, Tobias Hienzsch
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+ * Redistributions of source code must retain the above copyright notice,
+   this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright
+   notice, this list of conditions and the following disclaimer in the
+   documentation and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND ANY
+EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+DAMAGE.
+*/
+
+#include "etl/numbers.hpp"
+
+#include "catch2/catch.hpp"
+
+TEMPLATE_TEST_CASE("numbers: constants", "[numbers]", float, double, long double)
+{
+    REQUIRE(etl::numbers::e_v<TestType> == Approx(2.7182818));
+    REQUIRE(etl::numbers::log2e_v<TestType> == Approx(1.44269504));
+    REQUIRE(etl::numbers::log10e_v<TestType> == Approx(0.4342944));
+    REQUIRE(etl::numbers::pi_v<TestType> == Approx(3.1415926));
+    REQUIRE(etl::numbers::inv_sqrtpi_v<TestType> == Approx(0.5641895));
+    REQUIRE(etl::numbers::inv_pi_v<TestType> == Approx(0.3183098));
+    REQUIRE(etl::numbers::ln2_v<TestType> == Approx(0.6931471));
+    REQUIRE(etl::numbers::ln10_v<TestType> == Approx(2.3025850));
+    REQUIRE(etl::numbers::sqrt2_v<TestType> == Approx(1.4142135));
+    REQUIRE(etl::numbers::sqrt3_v<TestType> == Approx(1.7320508));
+    REQUIRE(etl::numbers::inv_sqrt3_v<TestType> == Approx(0.5773502));
+    REQUIRE(etl::numbers::egamma_v<TestType> == Approx(0.5772156));
+    REQUIRE(etl::numbers::phi_v<TestType> == Approx(1.6180339));
+}
