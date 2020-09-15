e54f8ed2285e69b7e14ee1c1ca25a6a0aeceeb7f
Tobias Hienzsch <post@tobias-hienzsch.de>
2020-09-15 21:10:56 +0200

    cstdlib: Add header

---
+++ etl/cstdlib.hpp
@@ -0,0 +1,88 @@
+/*
+Copyright (c) 2019-2020, Tobias Hienzsch
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+ * Redistributions of source code must retain the above copyright notice,
+   this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright
+   notice, this list of conditions and the following disclaimer in the
+   documentation and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND ANY
+EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+DAMAGE.
+*/
+
+#ifndef TAETL_CSTDLIB_HPP
+#define TAETL_CSTDLIB_HPP
+
+#include "cassert.hpp"
+#include "definitions.hpp"
+
+namespace etl
+{
+/**
+ * @brief Converts an integer value to a null-terminated string using the specified base
+ * and stores the result in the array given by str parameter.
+ *
+ * @details If base is 10 and value is negative, the resulting string is preceded with a
+ * minus sign (-). With any other base, value is always considered unsigned.
+ *
+ * @todo Only base 10 is currently supported.
+ */
+auto iota(int val, char* const buffer, int base) -> char*
+{
+    switch (base)
+    {
+    case 10:
+    {
+        auto digits10 = [](auto x) {
+            auto result = 1;
+            while (true)
+            {
+                if (x < 10) { return result; }
+                if (x < 100) { return result + 1; }
+                if (x < 1'000) { return result + 2; }
+                if (x < 10'000) { return result + 3; }
+
+                x /= 10'000;
+                result += 4;
+            }
+
+            return result;
+        };
+
+        auto const result = digits10(val);
+        auto pos          = result - 1;
+        while (val >= 10)
+        {
+            auto const q  = val / 10;
+            auto const r  = static_cast<char>(val % 10);
+            buffer[pos--] = static_cast<char>('0' + r);
+            val           = q;
+        }
+
+        *buffer = static_cast<char>(val + '0');
+        return buffer;
+    }
+    default:
+    {
+        ETL_ASSERT(false);
+        return buffer;
+    }
+    }
+}
+}  // namespace etl
+
+#endif  // TAETL_CSTDLIB_HPP
---
+++ tests/test_cstdlib.cpp
@@ -0,0 +1,94 @@
+/*
+Copyright (c) 2019-2020, Tobias Hienzsch
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+ * Redistributions of source code must retain the above copyright notice,
+   this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright
+   notice, this list of conditions and the following disclaimer in the
+   documentation and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND ANY
+EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+DAMAGE.
+*/
+#include "etl/cstdlib.hpp"
+#include "etl/cstring.hpp"
+
+#include "catch2/catch.hpp"
+
+#include <cstring>
+
+TEST_CASE("cstdlib: iota(signed,base10)", "[cstdlib]")
+{
+    SECTION("0")
+    {
+        int val         = 0;
+        char buffer[12] = {};
+        auto* result    = etl::iota(val, buffer, 10);
+        REQUIRE(&buffer[0] == result);
+        REQUIRE(etl::strlen(buffer) == 1);
+        REQUIRE(std::strcmp(buffer, "0") == 0);
+    }
+
+    SECTION("10")
+    {
+        int val         = 10;
+        char buffer[12] = {};
+        auto* result    = etl::iota(val, buffer, 10);
+        REQUIRE(&buffer[0] == result);
+        REQUIRE(etl::strlen(buffer) == 2);
+        REQUIRE(std::strcmp(buffer, "10") == 0);
+    }
+
+    SECTION("999")
+    {
+        int val         = 999;
+        char buffer[12] = {};
+        auto* result    = etl::iota(val, buffer, 10);
+        REQUIRE(&buffer[0] == result);
+        REQUIRE(etl::strlen(buffer) == 3);
+        REQUIRE(std::strcmp(buffer, "999") == 0);
+    }
+
+    SECTION("1002")
+    {
+        int val         = 1002;
+        char buffer[12] = {};
+        auto* result    = etl::iota(val, buffer, 10);
+        REQUIRE(&buffer[0] == result);
+        REQUIRE(etl::strlen(buffer) == 4);
+        REQUIRE(std::strcmp(buffer, "1002") == 0);
+    }
+
+    SECTION("44444")
+    {
+        int val         = 44444;
+        char buffer[12] = {};
+        auto* result    = etl::iota(val, buffer, 10);
+        REQUIRE(&buffer[0] == result);
+        REQUIRE(etl::strlen(buffer) == 5);
+        REQUIRE(std::strcmp(buffer, "44444") == 0);
+    }
+
+    SECTION("123456789")
+    {
+        int val         = 123456789;
+        char buffer[12] = {};
+        auto* result    = etl::iota(val, buffer, 10);
+        REQUIRE(&buffer[0] == result);
+        REQUIRE(etl::strlen(buffer) == 9);
+        REQUIRE(std::strcmp(buffer, "123456789") == 0);
+    }
+}
