694c176527c0ed7000a81c8e7cd8f60112f7dc35
Tobias Hienzsch <post@tobias-hienzsch.de>
2020-09-24 06:36:16 +0200

    Cleanup

--- tests/experimental/strong_type/test_strong_type.cpp
+++ tests/experimental/strong_type/test_strong_type.cpp
@@ -56,7 +56,7 @@ TEMPLATE_TEST_CASE("experimental/strong_type: type_traits", "[experimental]",
     STATIC_REQUIRE(etl::is_nothrow_constructible_v<Kilogram>);
 
     STATIC_REQUIRE(std::is_destructible_v<Kilogram>);
-    STATIC_REQUIRE(std::is_trivially_destructible_v<Kilogram>);
+    STATIC_REQUIRE(etl::is_trivially_destructible_v<Kilogram>);
     STATIC_REQUIRE(std::is_nothrow_destructible_v<Kilogram>);
 
     STATIC_REQUIRE(etl::is_assignable_v<Kilogram, Kilogram>);
