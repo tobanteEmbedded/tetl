071af2de9da3943c002f2784aecd1ad50bf82818
Tobias Hienzsch <post@tobias-hienzsch.de>
2020-10-04 04:24:38 +0200

    cctype: Add header

--- etl/cctype.hpp
+++ etl/cctype.hpp
@@ -24,39 +24,7 @@ OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
 DAMAGE.
 */
 
-#ifndef TAETL_DETAIL_ALGO_SWAP_HPP
-#define TAETL_DETAIL_ALGO_SWAP_HPP
+#ifndef TAETL_CCTYPE_HPP
+#define TAETL_CCTYPE_HPP
 
-#include "etl/type_traits.hpp"
-
-namespace etl
-{
-namespace detail
-{
-template <typename T>
-constexpr auto internal_move(T&& t) noexcept -> etl::remove_reference_t<T>&&
-{
-    return static_cast<etl::remove_reference_t<T>&&>(t);
-}
-}  // namespace detail
-
-/**
- * @brief Exchanges the given values. Swaps the values a and b. This overload
- * does not participate in overload resolution unless
- * etl::is_move_constructible_v<T> && etl::is_move_assignable_v<T> is true.
- *
- * https://en.cppreference.com/w/cpp/algorithm/swap
- *
- * @todo Fix noexcept specifier.
- */
-template <typename T>
-constexpr auto swap(T& a, T& b) noexcept -> void
-{
-    T temp(etl::detail::internal_move(a));
-    a = etl::detail::internal_move(b);
-    b = etl::detail::internal_move(temp);
-}
-
-}  // namespace etl
-
-#endif  // TAETL_DETAIL_ALGO_SWAP_HPP
+#endif  // TAETL_CCTYPE_HPP
