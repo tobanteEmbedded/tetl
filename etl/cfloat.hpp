b17f57a9f83fa3a016b7d8213e3c5f328ff7c906
Tobias Hienzsch <post@tobias-hienzsch.de>
2021-01-20 01:10:01 +0100

    cfloat: Cleanup

--- etl/cfloat.hpp
+++ etl/cfloat.hpp
@@ -31,22 +31,29 @@ DAMAGE.
 #include <float.h>
 #else
 
-#define FLT_EVAL_METHOD __FLT_EVAL_METHOD__
-#define FLT_ROUNDS (__builtin_flt_rounds())
 #define FLT_RADIX __FLT_RADIX__
 
-#define FLT_MANT_DIG __FLT_MANT_DIG__
-#define DBL_MANT_DIG __DBL_MANT_DIG__
-#define LDBL_MANT_DIG __LDBL_MANT_DIG__
-
-#if __STDC_VERSION__ >= 199901L || !defined(__STRICT_ANSI__)                   \
-  || __cplusplus >= 201103L
 #define DECIMAL_DIG __DECIMAL_DIG__
-#endif
+
+#define FLT_MIN __FLT_MIN__
+#define DBL_MIN __DBL_MIN__
+#define LDBL_MIN __LDBL_MIN__
+
+#define FLT_MAX __FLT_MAX__
+#define DBL_MAX __DBL_MAX__
+#define LDBL_MAX __LDBL_MAX__
+
+#define FLT_EPSILON __FLT_EPSILON__
+#define DBL_EPSILON __DBL_EPSILON__
+#define LDBL_EPSILON __LDBL_EPSILON__
 
 #define FLT_DIG __FLT_DIG__
 #define DBL_DIG __DBL_DIG__
 #define LDBL_DIG __LDBL_DIG__
+
+#define FLT_MANT_DIG __FLT_MANT_DIG__
+#define DBL_MANT_DIG __DBL_MANT_DIG__
+#define LDBL_MANT_DIG __LDBL_MANT_DIG__
 
 #define FLT_MIN_EXP __FLT_MIN_EXP__
 #define DBL_MIN_EXP __DBL_MIN_EXP__
@@ -64,17 +71,9 @@ DAMAGE.
 #define DBL_MAX_10_EXP __DBL_MAX_10_EXP__
 #define LDBL_MAX_10_EXP __LDBL_MAX_10_EXP__
 
-#define FLT_MAX __FLT_MAX__
-#define DBL_MAX __DBL_MAX__
-#define LDBL_MAX __LDBL_MAX__
+#define FLT_ROUNDS (__builtin_flt_rounds())
 
-#define FLT_EPSILON __FLT_EPSILON__
-#define DBL_EPSILON __DBL_EPSILON__
-#define LDBL_EPSILON __LDBL_EPSILON__
-
-#define FLT_MIN __FLT_MIN__
-#define DBL_MIN __DBL_MIN__
-#define LDBL_MIN __LDBL_MIN__
+#define FLT_EVAL_METHOD __FLT_EVAL_METHOD__
 
 #endif  // has_include <float.h>
 
