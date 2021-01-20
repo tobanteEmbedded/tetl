860fc09f151173dbfd9952de3639899722d73f37
Tobias Hienzsch <post@tobias-hienzsch.de>
2021-01-20 05:40:27 +0100

    actions: Add arm C++20 workflow

--- examples/mk/arm_stm32f103.mk
+++ examples/mk/arm_stm32f103.mk
@@ -1,63 +1,52 @@
-# TOOLCHAIN_ROOT ?= $(HOME)/bin/gcc-arm-none-eabi-9-2020-q2-update
-# TOOLCHAIN_ROOT ?= /work/gcc-arm-none-eabi-9-2020-q2-update
 TOOLCHAIN_ROOT ?= $(shell which arm-none-eabi-gcc | sed 's/\/bin\/arm-none-eabi-gcc//g')
 TOOLCHAIN_BIN ?= $(TOOLCHAIN_ROOT)/bin
 TOOLCHAIN_VERSION := $(strip $(shell $(TOOLCHAIN_ROOT)/bin/arm-none-eabi-g++ --version | head -n 1 | grep -Po '\d+\.\d+\.\d+'))
 
+TOOLCHAIN 	= arm-none-eabi-
+
+AS 			= $(TOOLCHAIN_BIN)/$(TOOLCHAIN)gcc
+CC 			= $(TOOLCHAIN_BIN)/$(TOOLCHAIN)gcc
+CXX 		= $(TOOLCHAIN_BIN)/$(TOOLCHAIN)g++
+LD  		= $(TOOLCHAIN_BIN)/$(TOOLCHAIN)g++
+OBJCOPY		= $(TOOLCHAIN_BIN)/$(TOOLCHAIN)objcopy
+OBJDUMP		= $(TOOLCHAIN_BIN)/$(TOOLCHAIN)objdump
+SIZE		= $(TOOLCHAIN_BIN)/$(TOOLCHAIN)size
+
+
 DEFINES += -DSTM32F103xB
 
-INCLUDES += -I../../..
-INCLUDES += -Iinclude
-INCLUDES += -Iinclude/CMSIS/Include 
-INCLUDES += -Iinclude/CMSIS/Device/ST/STM32F1xx/Include 
+INCLUDES += -I../
 
 ARCH += -mcpu=cortex-m3
 ARCH += -mthumb
 ARCH += -mfloat-abi=soft
 
 COMMONFLAGS += $(ARCH)
-COMMONFLAGS += -flto
+COMMONFLAGS += $(DEBUGFLAGS)
 COMMONFLAGS += -ffreestanding
 COMMONFLAGS += -fno-unroll-loops
 COMMONFLAGS += -fomit-frame-pointer
-COMMONFLAGS += -ffunction-sections 
+COMMONFLAGS += -ffunction-sections
 COMMONFLAGS += -fdata-sections
 COMMONFLAGS += -Werror
 COMMONFLAGS += -Wall
 COMMONFLAGS += -Wextra
 COMMONFLAGS += -Wpedantic
 
-ifndef DEBUG
-	COMMONFLAGS += -Os
-	DEFINES += -DNDEBUG
-else
-	COMMONFLAGS += -Og
-	COMMONFLAGS += -g3
-	COMMONFLAGS += -ggdb
-endif 
-
-
 CFLAGS += $(COMMONFLAGS)
-CFLAGS += -std=gnu11
+CFLAGS += -std=c11
 CFLAGS += -MMD -MP
 CFLAGS += -Wno-int-to-pointer-cast
-CFLAGS += -Wno-unused-parameter 
+CFLAGS += -Wno-unused-parameter
 CFLAGS += -Wno-switch-default
 
 CXXFLAGS += $(COMMONFLAGS)
-CXXFLAGS += -std=gnu++2a
+CXXFLAGS += -std=c++${CXXSTD}
 CXXFLAGS += -fpermissive
 CXXFLAGS += -fno-rtti
 CXXFLAGS += -fno-exceptions
 CXXFLAGS += -fno-use-cxa-atexit
 CXXFLAGS += -fno-threadsafe-statics
 CXXFLAGS += -ftemplate-backtrace-limit=0
-
-LDSCRIPT = STM32F103C8TX_FLASH.ld
-LDFLAGS += -T$(LDSCRIPT) 
-LDFLAGS += $(COMMONFLAGS)
-LDFLAGS += -Wl,-Map="$(BIN).map"
-LDFLAGS += -Wl,--gc-sections
-LDFLAGS += -static 
-
+CXXFLAGS += $(INCLUDES)
 
