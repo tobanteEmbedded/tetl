fa36d7b143b649458c3a493665b7295e7b30697c
Tobias Hienzsch <post@tobias-hienzsch.de>
2021-03-27 21:03:26 +0100

    Update sanitizers to clang-12

--- cmake/CompilerWarnings.cmake
+++ cmake/CompilerWarnings.cmake
@@ -1,49 +1,26 @@
 add_library(compiler_warnings INTERFACE)
 add_library(tobanteAudio::CompilerWarnings ALIAS compiler_warnings)
 
-if(MSVC)
-  target_compile_options(compiler_warnings INTERFACE /W3)
-  if(TOBANTEAUDIO_ETL_BUILD_WERROR)
+if (MSVC)
+  if (TOBANTEAUDIO_ETL_BUILD_WERROR)
     target_compile_options(compiler_warnings INTERFACE /WX)
-  endif(TOBANTEAUDIO_ETL_BUILD_WERROR)
-else()
-  if(TOBANTEAUDIO_ETL_BUILD_WERROR)
+  endif (TOBANTEAUDIO_ETL_BUILD_WERROR)
+  target_compile_options(compiler_warnings INTERFACE /W3)
+else ()
+  if (TOBANTEAUDIO_ETL_BUILD_WERROR)
     target_compile_options(compiler_warnings INTERFACE -Werror)
-  endif(TOBANTEAUDIO_ETL_BUILD_WERROR)
-
-  # GCC & CLANG
-  target_compile_options(
-    compiler_warnings
-    INTERFACE
+  endif (TOBANTEAUDIO_ETL_BUILD_WERROR)
+    target_compile_options(compiler_warnings
+      INTERFACE
         -Wall
         -Wextra
         -Wpedantic
+        -Wcast-align
         -Wshadow
-        # -Wsign-conversion
-        # -Wconversion
-        # -Wdouble-promotion
-  )
-
-  # GCC
-  target_compile_options(
-    compiler_warnings
-    INTERFACE
-    $<$<CXX_COMPILER_ID:GNU>:
-        -Wmisleading-indentation
-        -Wlogical-op
-        -Wduplicated-branches
-        -Wduplicated-cond
-        -Wno-parentheses
-        -Wno-sequence-point
-    >
-  )
-
-  # CLANG
-  target_compile_options(
-    compiler_warnings
-    INTERFACE
-    $<$<CXX_COMPILER_ID:Clang>:
-        -Wshadow-all
-    >
-  )
-endif()
+        -Wunused-parameter
+        -Wnarrowing
+        $<$<OR:$<CXX_COMPILER_ID:Clang>,$<CXX_COMPILER_ID:AppleClang>>: -Wshadow-all >
+        $<$<CXX_COMPILER_ID:AppleClang>:  -Wno-poison-system-directories >
+        $<$<CXX_COMPILER_ID:GNU>: -Wmisleading-indentation -Wlogical-op -Wduplicated-branches -Wduplicated-cond -Wno-parentheses -Wno-sequence-point >
+    )
+endif (MSVC)
