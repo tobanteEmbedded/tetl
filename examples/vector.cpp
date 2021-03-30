7c4fea42e8c322a86152a89bb1ea69a96b51b2b1
Tobias Hienzsch <post@tobias-hienzsch.de>
2021-03-31 00:51:19 +0200

    [vector] Improve example

--- examples/vector.cpp
+++ examples/vector.cpp
@@ -24,25 +24,61 @@ OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
 DAMAGE.
 */
 
-#include <stdio.h>  // for printf
+#undef NDEBUG
 
 #include "etl/vector.hpp"  // for static_vector
 
+#include "etl/cctype.hpp"  // for toupper
+
+#include <assert.h>  // for assert
+#include <stdio.h>   // for printf
+#include <stdlib.h>  // for EXIT_SUCCESS
+
+struct Person
+{
+  constexpr Person(int a, int e) noexcept : age {a}, experience {e} { }
+  int age {};
+  int experience {};
+};
+
+constexpr auto operator==(Person lhs, Person rhs) noexcept -> bool
+{
+  return lhs.age == rhs.age && lhs.experience == rhs.experience;
+}
+
 auto main() -> int
 {
-  etl::static_vector<double, 16> vec1;
-  vec1.push_back(1.0);
-  vec1.push_back(2.0);
-  vec1.push_back(3.0);
-  vec1.push_back(4.0);
+  // Unlike a std::vector you will have to decide which maximum capacity you
+  // need. Apart from that it behaves almost the same as the standard version.
+  etl::static_vector<Person, 32> people {};
+  assert(people.empty());
+  assert(people.capacity() == 32);
 
-  for (auto item : vec1) { printf("%f\n", item); }
+  // You can push_back/emplace_back into the vector
+  people.push_back(Person {20, 0});
+  assert(people.size() == 1);
+  assert(people.back().age == 20);
 
-  auto foo = [](auto& vec2) {
-    for (auto item : vec2) { printf("%f\n", item); }
+  people.emplace_back(90, 100);
+  assert(people.size() == 2);
+  assert(people.back().age == 90);
+
+  // You can make copies.
+  auto const copy = people;
+
+  // You can compare vectors
+  assert(copy == people);
+
+  // You can apply algorithms.
+  auto levelUp = [](auto p) {
+    p.experience += 1;
+    return p;
   };
 
-  foo(vec1);
+  etl::transform(begin(people), end(people), begin(people), levelUp);
+  assert(people[0].experience == 1);
+  assert(people[1].experience == 101);
+  assert(copy != people);
 
-  return 0;
+  return EXIT_SUCCESS;
 }
