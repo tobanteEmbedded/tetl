90f6a28e7e12b5ed8893c0444001809e3ec5a990
Tobias Hienzsch <post@tobias-hienzsch.de>
2021-03-31 00:41:35 +0200

    [string] Improve example

--- examples/string.cpp
+++ examples/string.cpp
@@ -23,27 +23,72 @@ LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
 OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
 DAMAGE.
 */
+#undef NDEBUG
 
-// C STANDARD
-#include <stdio.h>
+#include "etl/string.hpp"  // for static_string
 
-// TAETL
-#include "etl/string.hpp"
+#include "etl/cctype.hpp"  // for toupper
+
+#include <assert.h>  // for assert
+#include <stdio.h>   // for printf
+#include <stdlib.h>  // for EXIT_SUCCESS
 
 auto main() -> int
 {
+  // Unlike a std::string you will have to decide which maximum capacity you
+  // need. Apart from that it behaves almost the same as the standard version.
   etl::static_string<32> str {};
+  assert(str.empty());
+  assert(str.capacity() == 32);
 
-  const char* cptr = "C-string";
-  str.append(cptr, 4);
+  // You can append/push_back characters, c-strings, string_view and other
+  // strings of same or different capacity.
+  str.append("Hello", 2);
+  assert(str.size() == 2);
+  assert(str == "He");
 
-  printf("\"%s\"\n", str.c_str());
+  str.append(2, 'l');
+  assert(str.size() == 4);
+  assert(str == "Hell");
 
-  for (auto& c : str) { printf("%c", c); }
+  str.push_back('o');
+  assert(!str.empty());
+  assert(str.size() == 5);
+  assert(str == "Hello");
 
-  printf("\nSize: %zu\n", str.size());
-  printf("Length: %zu\n", str.length());
-  printf("Capacity: %zu\n", str.capacity());
+  auto other = etl::string_view {" World"};
+  str.append(other, 0);
+  assert(!str.empty());
+  assert(str.size() == 11);
+  assert(str == "Hello World");
 
-  return 0;
+  // You can make copies.
+  auto const copy = str;
+
+  // You can compare strings
+  assert(copy == str);
+
+  // You can apply algorithms.
+  auto toUpper = [](char ch) -> char { return etl::toupper(ch); };
+  etl::transform(begin(str), end(str), begin(str), toUpper);
+  assert(str == "HELLO WORLD");
+  assert(copy != str);
+
+  // You can insert at any position
+  str.insert(0, 2, ' ');
+  assert(str == "  HELLO WORLD");
+  str.insert(7, " foo");
+  assert(str == "  HELLO foo WORLD");
+
+  // You can check if a static_string starts or ends with a substring
+  assert(str.starts_with("  "));
+  assert(str.ends_with("WORLD"));
+
+  // You can convert a static_string into a string_view
+  etl::string_view view = str;
+  assert(view.size() == str.size());
+
+  // TODO: find & friends
+
+  return EXIT_SUCCESS;
 }
