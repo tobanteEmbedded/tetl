4911bfcdcc80091cb3babfb5f5c34fb8b68ca270
Tobias Hienzsch <post@tobias-hienzsch.de>
2021-04-03 17:50:35 +0200

    [cstddef\ Don't redefine offsetof on MSVC

--- etl/detail/intrinsics.hpp
+++ etl/detail/intrinsics.hpp
@@ -92,21 +92,23 @@ DAMAGE.
 #define TAETL_BUILTIN_PTRDIFF __PTRDIFF_TYPE__
 #endif
 
+#if defined(__GNUC__)
+#define TAETL_COMPILER_GCC 1
+#elif defined(__clang__)
+#define TAETL_COMPILER_CLANG 1
+#elif defined(_MSC_VER)
+#define TAETL_COMPILER_MSVC 1
+#elif defined(__INTEL_COMPILER)
+#define TAETL_COMPILER_INTEL 1
+#elif defined(__EMSCRIPTEN__)
+#define TAETL_COMPILER_EMSCRIPTEN 1
+#else
+#error "unknown compiler"
+#endif
+
 // Resolve which function signature macro will be used.
-#if defined(__GNUC__) || (defined(__MWERKS__) && (__MWERKS__ >= 0x3000))       \
-  || (defined(__ICC) && (__ICC >= 600)) || defined(__ghs__)
+#if defined(__GNUC__)
 #define TAETL_FUNC_SIG __PRETTY_FUNCTION__
-#elif defined(__DMC__) && (__DMC__ >= 0x810)
-#define TAETL_FUNC_SIG __PRETTY_FUNCTION__
-#elif defined(__FUNCSIG__)
-#define TAETL_FUNC_SIG __FUNCSIG__
-#elif (defined(__INTEL_COMPILER) && (__INTEL_COMPILER >= 600))                 \
-  || (defined(__IBMCPP__) && (__IBMCPP__ >= 500))
-#define TAETL_FUNC_SIG __FUNCTION__
-#elif defined(__BORLANDC__) && (__BORLANDC__ >= 0x550)
-#define TAETL_FUNC_SIG __FUNC__
-#elif defined(__STDC_VERSION__) && (__STDC_VERSION__ >= 199901)
-#define TAETL_FUNC_SIG __func__
 #elif defined(__cplusplus) && (__cplusplus >= 201103)
 #define TAETL_FUNC_SIG __func__
 #else
