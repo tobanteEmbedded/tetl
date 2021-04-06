623bc882906b6d3023ca01a4728bd1ff1f9b4fe6
Tobias Hienzsch <post@tobias-hienzsch.de>
2021-04-06 22:48:43 +0200

    [expected] Improve tests for unexpect_t

--- tests/test_expected.cpp
+++ tests/test_expected.cpp
@@ -1,10 +1,15 @@
 #include "etl/expected.hpp"
-#include "etl/warning.hpp"
 
 #include "catch2/catch_template_test_macros.hpp"
 
 TEST_CASE("expected: unexpect_t", "[expected]")
 {
-  etl::ignore_unused(etl::unexpect);
-  STATIC_REQUIRE(etl::is_default_constructible_v<etl::unexpect_t>);
+  using etl::decay_t;
+  using etl::is_default_constructible_v;
+  using etl::is_same_v;
+  using etl::unexpect;
+  using etl::unexpect_t;
+
+  STATIC_REQUIRE(is_same_v<unexpect_t, decay_t<decltype(unexpect)>>);
+  STATIC_REQUIRE(is_default_constructible_v<unexpect_t>);
 }
