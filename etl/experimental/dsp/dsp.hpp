3bf390bcfab1e51877cc2a6449258a7b151f7c23
Tobias Hienzsch <post@tobias-hienzsch.de>
2021-04-07 13:58:21 +0200

    Drop the year from copyright headers and the LICENSE file

--- etl/experimental/dsp/dsp.hpp
+++ etl/experimental/dsp/dsp.hpp
@@ -1,6 +1,5 @@
 /*
-Copyright (c) 2019-2020, Tobias Hienzsch
-All rights reserved.
+Copyright (c) Tobias Hienzsch. All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions are met:
--- fuzzing/fuzzer.cpp
+++ fuzzing/fuzzer.cpp
@@ -1,6 +1,5 @@
 /*
-Copyright (c) 2019-2020, Tobias Hienzsch
-All rights reserved.
+Copyright (c) Tobias Hienzsch. All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions are met:
@@ -38,72 +37,83 @@ DAMAGE.
 template <typename IntType>
 [[nodiscard]] auto test_sort_integers(FuzzedDataProvider& provider) -> int
 {
-    auto generator = [&provider] { return provider.ConsumeIntegral<IntType>(); };
-    auto vec       = etl::static_vector<IntType, 128> {};
-    etl::generate_n(etl::back_inserter(vec), vec.capacity(), generator);
+  auto generator = [&provider] { return provider.ConsumeIntegral<IntType>(); };
+  auto vec       = etl::static_vector<IntType, 128> {};
+  etl::generate_n(etl::back_inserter(vec), vec.capacity(), generator);
 
-    auto etl_set = etl::static_set<IntType, 128> {begin(vec), end(vec)};
-    auto std_set = std::set<IntType> {begin(vec), end(vec)};
-    if (etl_set.size() != std_set.size()) { return 1; }
+  auto etl_set = etl::static_set<IntType, 128> {begin(vec), end(vec)};
+  auto std_set = std::set<IntType> {begin(vec), end(vec)};
+  if (etl_set.size() != std_set.size()) { return 1; }
 
-    etl::sort(begin(vec), end(vec));
-    if (!etl::is_sorted(begin(vec), end(vec))) { return 1; }
+  etl::sort(begin(vec), end(vec));
+  if (!etl::is_sorted(begin(vec), end(vec))) { return 1; }
 
-    return 0;
+  return 0;
 }
 
 template <typename FloatType>
 [[nodiscard]] auto test_sort_floats(FuzzedDataProvider& provider) -> int
 {
-    auto generator = [&provider] { return provider.ConsumeFloatingPoint<FloatType>(); };
-    auto vec       = etl::static_vector<FloatType, 128> {};
-    etl::generate_n(etl::back_inserter(vec), vec.capacity(), generator);
+  auto generator
+    = [&provider] { return provider.ConsumeFloatingPoint<FloatType>(); };
+  auto vec = etl::static_vector<FloatType, 128> {};
+  etl::generate_n(etl::back_inserter(vec), vec.capacity(), generator);
 
-    auto etl_set = etl::static_set<FloatType, 128> {begin(vec), end(vec)};
-    auto std_set = std::set<FloatType> {begin(vec), end(vec)};
-    if (etl_set.size() != std_set.size()) { return 1; }
+  auto etl_set = etl::static_set<FloatType, 128> {begin(vec), end(vec)};
+  auto std_set = std::set<FloatType> {begin(vec), end(vec)};
+  if (etl_set.size() != std_set.size()) { return 1; }
 
-    etl::sort(begin(vec), end(vec));
-    if (!etl::is_sorted(begin(vec), end(vec))) { return 1; }
+  etl::sort(begin(vec), end(vec));
+  if (!etl::is_sorted(begin(vec), end(vec))) { return 1; }
 
-    return 0;
+  return 0;
 }
 
 [[nodiscard]] auto test_string(FuzzedDataProvider& provider) -> int
 {
-    auto const chars = provider.ConsumeBytesWithTerminator<char>(127, 0);
+  auto const chars = provider.ConsumeBytesWithTerminator<char>(127, 0);
 
-    auto etl_string = etl::static_string<128> {};
-    etl::copy(chars.begin(), chars.end(), etl::back_inserter(etl_string));
+  auto etl_string = etl::static_string<128> {};
+  etl::copy(chars.begin(), chars.end(), etl::back_inserter(etl_string));
 
-    auto std_string = std::string {chars.begin(), chars.end()};
+  auto std_string = std::string {chars.begin(), chars.end()};
 
-    if (etl_string.size() != std_string.size()) { return 1; }
-    if (etl::strlen(chars.data()) != std::strlen(chars.data())) { return 1; }
+  if (etl_string.size() != std_string.size()) { return 1; }
+  if (etl::strlen(chars.data()) != std::strlen(chars.data())) { return 1; }
 
-    return 0;
+  return 0;
 }
 
-extern "C" int LLVMFuzzerTestOneInput(etl::uint8_t const* data, etl::size_t size)
+extern "C" int LLVMFuzzerTestOneInput(etl::uint8_t const* data,
+                                      etl::size_t size)
 {
-    if (size == 0) { return 0; }
-    auto provider = FuzzedDataProvider {data, size};
+  if (size == 0) { return 0; }
+  auto provider = FuzzedDataProvider {data, size};
 
-    if (auto rc = test_sort_integers<etl::uint8_t>(provider); rc != 0) { throw rc; }
-    if (auto rc = test_sort_integers<etl::uint16_t>(provider); rc != 0) { throw rc; }
-    if (auto rc = test_sort_integers<etl::uint32_t>(provider); rc != 0) { throw rc; }
-    if (auto rc = test_sort_integers<etl::uint64_t>(provider); rc != 0) { throw rc; }
+  if (auto rc = test_sort_integers<etl::uint8_t>(provider); rc != 0)
+  { throw rc; }
+  if (auto rc = test_sort_integers<etl::uint16_t>(provider); rc != 0)
+  { throw rc; }
+  if (auto rc = test_sort_integers<etl::uint32_t>(provider); rc != 0)
+  { throw rc; }
+  if (auto rc = test_sort_integers<etl::uint64_t>(provider); rc != 0)
+  { throw rc; }
 
-    if (auto rc = test_sort_integers<etl::uint8_t>(provider); rc != 0) { throw rc; }
-    if (auto rc = test_sort_integers<etl::uint16_t>(provider); rc != 0) { throw rc; }
-    if (auto rc = test_sort_integers<etl::uint32_t>(provider); rc != 0) { throw rc; }
-    if (auto rc = test_sort_integers<etl::uint64_t>(provider); rc != 0) { throw rc; }
+  if (auto rc = test_sort_integers<etl::uint8_t>(provider); rc != 0)
+  { throw rc; }
+  if (auto rc = test_sort_integers<etl::uint16_t>(provider); rc != 0)
+  { throw rc; }
+  if (auto rc = test_sort_integers<etl::uint32_t>(provider); rc != 0)
+  { throw rc; }
+  if (auto rc = test_sort_integers<etl::uint64_t>(provider); rc != 0)
+  { throw rc; }
 
-    if (auto rc = test_sort_floats<etl::float_t>(provider); rc != 0) { throw rc; }
-    if (auto rc = test_sort_floats<etl::double_t>(provider); rc != 0) { throw rc; }
-    if (auto rc = test_sort_floats<long double>(provider); rc != 0) { throw rc; }
+  if (auto rc = test_sort_floats<etl::float_t>(provider); rc != 0) { throw rc; }
+  if (auto rc = test_sort_floats<etl::double_t>(provider); rc != 0)
+  { throw rc; }
+  if (auto rc = test_sort_floats<long double>(provider); rc != 0) { throw rc; }
 
-    if (auto rc = test_string(provider); rc != 0) { throw rc; }
+  if (auto rc = test_string(provider); rc != 0) { throw rc; }
 
-    return 0;
+  return 0;
 }
