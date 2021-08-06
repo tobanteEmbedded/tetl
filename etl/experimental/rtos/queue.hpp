301b220331727036b2241557873e2b37f9a2cd30
Tobias Hienzsch <post@tobias-hienzsch.de>
2021-08-06 21:15:01 +0200

    [freertos] Queue cleanup

--- etl/experimental/rtos/queue.hpp
+++ etl/experimental/rtos/queue.hpp
@@ -35,78 +35,116 @@
 #endif
 
 namespace etl::experimental::rtos {
-/// Wrapper around a FreeRTOS queue.
-/// \tparam ValueType The type that's being stored inside the queue.
+
+/// \brief Wrapper around a FreeRTOS queue.
+///
+/// https://www.freertos.org/Embedded-RTOS-Queues.html
+///
+/// \tparam T The type that's being stored inside the queue.
 /// \tparam Size The maximum capacity of the queue.
-template <typename ValueType, etl::uint32_t Size>
-class queue {
-public:
-    /// The type that's being stored inside the queue
-    using value_type = ValueType;
-    /// The integer type used for the size
-    using size_type = etl::uint32_t;
+template <typename T, etl::uint32_t Size>
+struct queue {
+    using value_type = T;
+    using size_type  = etl::uint32_t;
 
-    /// Creates a new queue
-    queue() : handle_([]() { return xQueueCreate(Size, sizeof(ValueType)); }())
-    {
-    }
+    /// \brief Creates a new queue. RAM is automatically allocated from the
+    /// FreeRTOS heap.
+    ///
+    /// https://www.freertos.org/a00116.html
+    queue();
 
-    queue(queue&&)      = delete;
+    /// \brief Delete a queue - freeing all the memory allocated for storing of
+    /// items placed on the queue.
+    ///
+    /// https://www.freertos.org/a00018.html#vQueueDelete
+    ~queue();
+
     queue(queue const&) = delete;
-    auto operator=(queue &&) -> queue& = delete;
     auto operator=(queue const&) -> queue& = delete;
 
-    ~queue()
-    {
-        if (handle_ != nullptr) { vQueueDelete(handle_); }
-    }
+    queue(queue&&) = delete;
+    auto operator=(queue&&) -> queue& = delete;
 
     /// Returns the capacity of the internal buffer
-    [[nodiscard]] auto capacity() const -> size_type { return Size; }
+    [[nodiscard]] auto capacity() const -> size_type;
 
     /// Push an element on to the queue.
-    [[nodiscard]] auto send(
-        ValueType const& data, TickType_t ticksToWait = 0) const -> bool
-    {
-        const auto* const rawData = static_cast<const void*>(&data);
-        auto const success        = xQueueSend(handle_, rawData, ticksToWait);
-        return static_cast<bool>(success);
-    }
+    [[nodiscard]] auto send(T const& data, TickType_t ticksToWait = 0) const
+        -> bool;
 
     /// Pop an element of the queue.
-    auto receive(ValueType& data, TickType_t ticksToWait = 0) const -> bool
-    {
-        auto* const rawData = static_cast<void*>(&data);
-        auto const success  = xQueueReceive(handle_, rawData, ticksToWait);
-        return static_cast<bool>(success);
-    }
+    auto receive(T& data, TickType_t ticksToWait = 0) const -> bool;
 
     /// Pop an element of the queue.
     [[nodiscard]] auto receive(TickType_t ticksToWait = 0) const
-        -> pair<bool, ValueType>
-    {
-        auto value          = ValueType {};
-        auto* const rawData = static_cast<void*>(&value);
-        auto const success  = xQueueReceive(handle_, rawData, ticksToWait);
-        return { static_cast<bool>(success), value };
-    }
+        -> pair<bool, T>;
 
-    [[nodiscard]] auto reset() const -> bool
-    {
-        auto const result = xQueueReset(handle_);
-        return static_cast<bool>(result);
-    }
-
-    [[nodiscard]] auto messages_waiting() const -> etl::uint32_t
-    {
-        auto const result = uxQueueMessagesWaiting(handle_);
-        return static_cast<etl::uint32_t>(result);
-    }
+    [[nodiscard]] auto reset() const -> bool;
+    [[nodiscard]] auto messages_waiting() const -> etl::uint32_t;
 
 private:
     QueueHandle_t handle_ = nullptr;
 };
 
+template <typename T, etl::uint32_t Size>
+inline queue<T, Size>::queue()
+    : handle_ { []() { return xQueueCreate(Size, sizeof(T)); }() }
+{
+}
+
+template <typename T, etl::uint32_t Size>
+inline queue<T, Size>::~queue()
+{
+    if (handle_ != nullptr) { vQueueDelete(handle_); }
+}
+
+template <typename T, etl::uint32_t Size>
+inline auto queue<T, Size>::capacity() const -> size_type
+{
+    return Size;
+}
+
+template <typename T, etl::uint32_t Size>
+inline auto queue<T, Size>::send(T const& data, TickType_t ticksToWait) const
+    -> bool
+{
+    const auto* const rawData = static_cast<const void*>(&data);
+    auto const success        = xQueueSend(handle_, rawData, ticksToWait);
+    return static_cast<bool>(success);
+}
+
+template <typename T, etl::uint32_t Size>
+inline auto queue<T, Size>::receive(T& data, TickType_t ticksToWait) const
+    -> bool
+{
+    auto* const rawData = static_cast<void*>(&data);
+    auto const success  = xQueueReceive(handle_, rawData, ticksToWait);
+    return static_cast<bool>(success);
+}
+
+template <typename T, etl::uint32_t Size>
+inline auto queue<T, Size>::receive(TickType_t ticksToWait) const
+    -> pair<bool, T>
+{
+    auto value          = T {};
+    auto* const rawData = static_cast<void*>(&value);
+    auto const success  = xQueueReceive(handle_, rawData, ticksToWait);
+    return { static_cast<bool>(success), value };
+}
+
+template <typename T, etl::uint32_t Size>
+inline auto queue<T, Size>::reset() const -> bool
+{
+    auto const result = xQueueReset(handle_);
+    return static_cast<bool>(result);
+}
+
+template <typename T, etl::uint32_t Size>
+inline auto queue<T, Size>::messages_waiting() const -> etl::uint32_t
+{
+    auto const result = uxQueueMessagesWaiting(handle_);
+    return static_cast<etl::uint32_t>(result);
+}
 } // namespace etl::experimental::rtos
 
 #endif // TETL_RTOS_QUEUE_HPP
