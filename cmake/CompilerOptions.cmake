bdd720973455aee7758792080ec78871dbbea399
Tobias Hienzsch <post@tobias-hienzsch.de>
2021-08-07 07:34:35 +0200

    Improve sanitizer builds

--- cmake/CompilerOptions.cmake
+++ cmake/CompilerOptions.cmake
@@ -8,33 +8,12 @@ else()
   target_link_libraries(compiler_options INTERFACE -fno-builtin)
 endif(MSVC)
 
-# Sanitizers
-set(SANITIZERS "")
-# Address
 if(TETL_BUILD_ASAN AND CMAKE_CXX_COMPILER_ID MATCHES "GNU|Clang")
-  list(APPEND SANITIZERS "address")
+  target_compile_options(compiler_options INTERFACE -fsanitize=address -O1 -g -fno-omit-frame-pointer)
+  target_link_libraries(compiler_options INTERFACE -fsanitize=address -O1 -g -fno-omit-frame-pointer)
 endif()
 
-# Undefined
 if(TETL_BUILD_UBSAN AND CMAKE_CXX_COMPILER_ID MATCHES "GNU|Clang")
-  list(APPEND SANITIZERS "undefined")
-endif()
-
-# Memory
-if(TETL_BUILD_MSAN AND CMAKE_CXX_COMPILER_ID MATCHES "GNU|Clang")
-  list(APPEND SANITIZERS "memory")
-endif()
-
-# Thread
-if(TETL_BUILD_TSAN AND CMAKE_CXX_COMPILER_ID MATCHES "GNU|Clang")
-  list(APPEND SANITIZERS "thread")
-endif()
-
-
-list(JOIN SANITIZERS "," LIST_OF_SANITIZERS)
-if(LIST_OF_SANITIZERS)
-  if(NOT "${LIST_OF_SANITIZERS}" STREQUAL "")
-    target_compile_options(compiler_options INTERFACE -fsanitize=${LIST_OF_SANITIZERS} -O1 -g -fno-omit-frame-pointer)
-    target_link_libraries(compiler_options INTERFACE -fsanitize=${LIST_OF_SANITIZERS} -O1 -g -fno-omit-frame-pointer)
-  endif()
+  target_compile_options(compiler_options INTERFACE -fsanitize=undefined -fno-sanitize-recover=undefined -O1 -g -fno-omit-frame-pointer)
+  target_link_libraries(compiler_options INTERFACE -fsanitize=undefined -fno-sanitize-recover=undefined -O1 -g -fno-omit-frame-pointer)
 endif()
