1875a491e167bdc07b6c915bb5dca3acf8694887
Tobias Hienzsch <post@tobias-hienzsch.de>
2021-08-11 04:37:54 +0200

    [scope] Cleanup includes

--- etl/_scope/scope_exit.hpp
+++ etl/_scope/scope_exit.hpp
@@ -20,12 +20,33 @@
 // LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
 // OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
 // DAMAGE.
+#ifndef TETL_SCOPE_SCOPE_EXIT_HPP
+#define TETL_SCOPE_SCOPE_EXIT_HPP
 
-#ifndef TETL_SCOPE_HPP
-#define TETL_SCOPE_HPP
+#include "etl/_scope/scope_guard.hpp"
 
-#include "etl/version.hpp"
+namespace etl {
 
-#include "etl/_scope/scope_exit.hpp"
+/// \brief The class template `scope_exit` is a general-purpose scope guard
+/// intended to call its exit function when a scope is exited. \details A
+/// `scope_exit` may be either active, i.e. calls its exit function on
+/// destruction, or inactive, i.e. does nothing on destruction. A `scope_exit`
+/// is active after constructed from an exit function. A `scope_exit` can become
+/// inactive by calling `release()` on it either manually or automatically (by
+/// the move constructor). An inactive `scope_exit` may also be obtained by
+/// initializing with another inactive `scope_exit`. Once a `scope_exit` is
+/// inactive, it cannot become active again.
+template <typename FuncT>
+struct scope_exit : detail::scope_guard<FuncT, detail::scope_exit_impl> {
+    /// Creates a scope_exit from a function, a function object or another
+    /// scope_exit.
+    using detail::scope_guard<FuncT, detail::scope_exit_impl>::scope_guard;
+};
 
-#endif // TETL_SCOPE_HPP
+// Deduction guide
+template <typename FuncT>
+scope_exit(FuncT) -> scope_exit<decay_t<FuncT>>;
+
+} // namespace etl
+
+#endif // TETL_SCOPE_SCOPE_EXIT_HPP
--- etl/_scope/scope_guard.hpp
+++ etl/_scope/scope_guard.hpp
@@ -20,12 +20,58 @@
 // LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
 // OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
 // DAMAGE.
+#ifndef TETL_SCOPE_SCOPE_GUARD_HPP
+#define TETL_SCOPE_SCOPE_GUARD_HPP
 
-#ifndef TETL_SCOPE_HPP
-#define TETL_SCOPE_HPP
+#include "etl/_type_traits/decay.hpp"
+#include "etl/_utility/forward.hpp"
+#include "etl/_utility/move.hpp"
 
-#include "etl/version.hpp"
+namespace etl {
+namespace detail {
+template <typename FuncT, typename PolicyT>
+struct scope_guard {
+public:
+    template <typename Functor>
+    explicit scope_guard(Functor f)
+        : func_ { etl::forward<Functor>(f) }, policy_ {}
+    {
+    }
 
-#include "etl/_scope/scope_exit.hpp"
+    scope_guard(scope_guard&& rhs) noexcept
+        : func_ { etl::move(rhs.func_) }, policy_ { etl::move(rhs.policy_) }
+    {
+    }
 
-#endif // TETL_SCOPE_HPP
+    ~scope_guard()
+    {
+        if (policy_) { func_(); }
+    }
+
+    void release() noexcept { policy_.release(); }
+
+    scope_guard(scope_guard const&) = delete;
+    auto operator=(scope_guard const&) -> scope_guard& = delete;
+    auto operator=(scope_guard&&) -> scope_guard& = delete;
+
+private:
+    FuncT func_;
+    PolicyT policy_;
+};
+
+struct scope_exit_impl {
+    scope_exit_impl() = default;
+    scope_exit_impl(scope_exit_impl&& rhs) noexcept
+        : should_execute { rhs.should_execute }
+    {
+        rhs.release();
+    }
+    void release() noexcept { should_execute = false; }
+    explicit operator bool() const noexcept { return should_execute; }
+    bool should_execute = true;
+};
+} // namespace detail
+
+} // namespace etl
+
+#endif // TETL_SCOPE_SCOPE_GUARD_HPP
