d53514869194d6eb298bf2bebadfc9dca4b7622c
Tobias Hienzsch <post@tobias-hienzsch.de>
2021-08-13 00:45:56 +0200

    [span] Move implemenation to _span

--- etl/_span/as_bytes.hpp
+++ etl/_span/as_bytes.hpp
@@ -21,13 +21,48 @@
 // OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
 // DAMAGE.
 
-#ifndef TETL_SPAN_HPP
-#define TETL_SPAN_HPP
+#ifndef TETL_SPAN_AS_BYTES_HPP
+#define TETL_SPAN_AS_BYTES_HPP
 
-#include "etl/_config/all.hpp"
-
-#include "etl/_span/as_bytes.hpp"
+#include "etl/_cstddef/byte.hpp"
 #include "etl/_span/dynamic_extent.hpp"
 #include "etl/_span/span.hpp"
+#include "etl/_type_traits/enable_if.hpp"
+#include "etl/_type_traits/is_const.hpp"
 
-#endif // TETL_SPAN_HPP
+namespace etl {
+
+namespace detail {
+template <typename T, etl::size_t N>
+inline constexpr etl::size_t span_as_bytes_size
+    = N == etl::dynamic_extent ? etl::dynamic_extent : sizeof(T) * N;
+}
+
+/// \brief Obtains a view to the object representation of the elements of the
+/// span s.
+///
+/// \details If N is dynamic_extent, the extent of the returned span S is also
+/// dynamic_extent; otherwise it is sizeof(T) * N.
+template <typename T, size_t N>
+[[nodiscard]] auto as_bytes(span<T, N> s) noexcept
+    -> span<byte const, detail::span_as_bytes_size<T, N>>
+{
+    return { reinterpret_cast<byte const*>(s.data()), s.size_bytes() };
+}
+
+/// \brief Obtains a view to the object representation of the elements of the
+/// span s.
+///
+/// \details If N is dynamic_extent, the extent of the returned span S is also
+/// dynamic_extent; otherwise it is sizeof(T) * N. Only participates in overload
+/// resolution if is_const_v<T> is false.
+template <typename T, size_t N>
+[[nodiscard]] auto as_writable_bytes(span<T, N> s) noexcept
+    -> enable_if_t<!is_const_v<T>, span<byte, detail::span_as_bytes_size<T, N>>>
+{
+    return { reinterpret_cast<byte*>(s.data()), s.size_bytes() };
+}
+
+} // namespace etl
+
+#endif // TETL_SPAN_AS_BYTES_HPP
--- etl/_span/dynamic_extent.hpp
+++ etl/_span/dynamic_extent.hpp
@@ -21,13 +21,17 @@
 // OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
 // DAMAGE.
 
-#ifndef TETL_SPAN_HPP
-#define TETL_SPAN_HPP
+#ifndef TETL_SPAN_DYNAMIC_EXTENT_HPP
+#define TETL_SPAN_DYNAMIC_EXTENT_HPP
 
-#include "etl/_config/all.hpp"
+#include "etl/_cstddef/size_t.hpp"
 
-#include "etl/_span/as_bytes.hpp"
-#include "etl/_span/dynamic_extent.hpp"
-#include "etl/_span/span.hpp"
+namespace etl {
 
-#endif // TETL_SPAN_HPP
+/// \brief etl::dynamic_extent is a constant of type etl::size_t that is used
+/// to differentiate etl::span of static and dynamic extent.
+inline constexpr auto dynamic_extent = static_cast<etl::size_t>(-1);
+
+} // namespace etl
+
+#endif // TETL_SPAN_DYNAMIC_EXTENT_HPP
