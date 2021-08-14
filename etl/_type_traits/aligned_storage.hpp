10d3515b29973612e046ff07b3f84f9f7471d05f
Tobias Hienzsch <post@tobias-hienzsch.de>
2021-08-14 22:24:29 +0200

    [type_traits] Improve aligned_storage for small alignment requirements

--- etl/_type_traits/aligned_storage.hpp
+++ etl/_type_traits/aligned_storage.hpp
@@ -26,8 +26,38 @@
 
 #include "etl/_cstddef/max_align_t.hpp"
 #include "etl/_cstddef/size_t.hpp"
+#include "etl/_type_traits/conditional.hpp"
 
 namespace etl {
+
+namespace detail {
+// With this change, types smaller then 16 bytes are aligned to their size. This
+// equals the behavoir from libc++ and MSVC-STL. Copied from
+// https://github.com/WG21-SG14/SG14/commit/98baf1aeab
+// https://gcc.gnu.org/bugzilla/show_bug.cgi?id=61458#c10
+template <size_t Cap>
+union aligned_storage_impl {
+    template <typename T>
+    using maybe = conditional_t<(Cap >= sizeof(T)), T, char>;
+
+    struct double1 {
+        double a;
+    };
+    struct double4 {
+        double a[4];
+    };
+
+    char real_data[Cap];
+    maybe<int> a;
+    maybe<long> b;
+    maybe<long long> c;
+    maybe<void*> d;
+    maybe<void (*)()> e;
+    maybe<double1> f;
+    maybe<double4> g;
+    maybe<long double> h;
+};
+} // namespace detail
 
 /// \brief Provides the nested type type, which is a trivial standard-layout
 /// type suitable for use as uninitialized storage for any object whose size is
@@ -37,7 +67,7 @@ namespace etl {
 /// default value is not used, Align must be the value of alignof(T) for some
 /// type T, or the behavior is undefined.
 /// \group aligned_storage
-template <size_t Len, size_t Align = alignof(max_align_t)>
+template <size_t Len, size_t Align = alignof(detail::aligned_storage_impl<Len>)>
 struct aligned_storage {
     struct type {
         alignas(Align) unsigned char data[Len];
@@ -45,7 +75,7 @@ struct aligned_storage {
 };
 
 /// \group aligned_storage
-template <size_t Len, size_t Align = alignof(max_align_t)>
+template <size_t Len, size_t Align = alignof(detail::aligned_storage_impl<Len>)>
 using aligned_storage_t = typename aligned_storage<Len, Align>::type;
 
 } // namespace etl
