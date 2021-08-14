04d33f19a600b569510a1bcea57710497ecd3016
Tobias Hienzsch <post@tobias-hienzsch.de>
2021-08-15 01:25:22 +0200

    [functional] Reenable invoke implementation

--- etl/_functional/invoke.hpp
+++ etl/_functional/invoke.hpp
@@ -34,42 +34,42 @@
 
 namespace etl {
 
-// namespace detail {
+namespace detail {
 
-// template <typename C, typename Pointed, typename T1, typename... Args>
-// constexpr auto invoke_memptr(Pointed C::*f, T1&& t1, Args&&... args)
-//     -> decltype(auto)
-// {
-//     if constexpr (etl::is_function_v<Pointed>) {
-//         if constexpr (etl::is_base_of_v<C, etl::decay_t<T1>>) {
-//             return (etl::forward<T1>(t1).*f)(etl::forward<Args>(args)...);
-//         } else if constexpr (is_reference_wrapper_v<etl::decay_t<T1>>) {
-//             return (t1.get().*f)(etl::forward<Args>(args)...);
-//         } else {
-//             return ((*etl::forward<T1>(t1)).*f)(etl::forward<Args>(args)...);
-//         }
-//     } else {
-//         static_assert(etl::is_object_v<Pointed> && sizeof...(args) == 0);
-//         if constexpr (etl::is_base_of_v<C, etl::decay_t<T1>>) {
-//             return etl::forward<T1>(t1).*f;
-//         } else if constexpr (is_reference_wrapper_v<etl::decay_t<T1>>) {
-//             return t1.get().*f;
-//         } else {
-//             return (*etl::forward<T1>(t1)).*f;
-//         }
-//     }
-// }
-// } // namespace detail
+template <typename C, typename Pointed, typename T1, typename... Args>
+constexpr auto invoke_memptr(Pointed C::*f, T1&& t1, Args&&... args)
+    -> decltype(auto)
+{
+    if constexpr (etl::is_function_v<Pointed>) {
+        if constexpr (etl::is_base_of_v<C, etl::decay_t<T1>>) {
+            return (etl::forward<T1>(t1).*f)(etl::forward<Args>(args)...);
+        } else if constexpr (is_reference_wrapper_v<etl::decay_t<T1>>) {
+            return (t1.get().*f)(etl::forward<Args>(args)...);
+        } else {
+            return ((*etl::forward<T1>(t1)).*f)(etl::forward<Args>(args)...);
+        }
+    } else {
+        static_assert(etl::is_object_v<Pointed> && sizeof...(args) == 0);
+        if constexpr (etl::is_base_of_v<C, etl::decay_t<T1>>) {
+            return etl::forward<T1>(t1).*f;
+        } else if constexpr (is_reference_wrapper_v<etl::decay_t<T1>>) {
+            return t1.get().*f;
+        } else {
+            return (*etl::forward<T1>(t1)).*f;
+        }
+    }
+}
+} // namespace detail
 
-// // todo Add noexcept(is_nothrow_invocable_v<F, Args...>)
-// template <typename F, typename... Args>
-// constexpr auto invoke(F&& f, Args&&... args) -> invoke_result_t<F, Args...>
-// {
-//     if constexpr (is_member_pointer_v<decay_t<F>>)
-//         return detail::invoke_memptr(f, forward<Args>(args)...);
-//     else
-//         return forward<F>(f)(forward<Args>(args)...);
-// }
+// todo Add noexcept(is_nothrow_invocable_v<F, Args...>)
+template <typename F, typename... Args>
+constexpr auto invoke(F&& f, Args&&... args) -> invoke_result_t<F, Args...>
+{
+    if constexpr (is_member_pointer_v<decay_t<F>>)
+        return detail::invoke_memptr(f, forward<Args>(args)...);
+    else
+        return forward<F>(f)(forward<Args>(args)...);
+}
 
 } // namespace etl
 
