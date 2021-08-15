2ab363226a81ef2d42156fcf1a08ce8053798f40
Tobias Hienzsch <post@tobias-hienzsch.de>
2021-08-15 03:40:36 +0200

    [type_traits] Fix clang-tidy warnings in is_invocable

--- etl/_type_traits/is_invocable.hpp
+++ etl/_type_traits/is_invocable.hpp
@@ -44,11 +44,11 @@ struct is_invocable_impl<Result, Ret, true, etl::void_t<typename Result::type>> 
 // Check if the return type can be converted to T
 template <typename Result, typename Ret>
 struct is_invocable_impl<Result, Ret, false, etl::void_t<typename Result::type>> {
-    static auto _get() -> typename Result::type;
-    template <typename T> static auto _use(T) -> void;
-    template <typename T, typename = decltype(_use<T>(_get()))> static auto _check(int) -> etl::true_type;
-    template <typename T> static auto _check(...) -> etl::false_type;
-    using type = decltype(_check<Ret>(1));
+    static auto get_t() -> typename Result::type;
+    template <typename T> static auto use_t(T /*ignore*/) -> void;
+    template <typename T, typename = decltype(use_t<T>(get_t()))> static auto check_converts_to_t(int /*ignore*/) -> etl::true_type;
+    template <typename T> static auto check_converts_to_t(...) -> etl::false_type;
+    using type = decltype(check_converts_to_t<Ret>(1));
 };
 
 } // namespace detail
