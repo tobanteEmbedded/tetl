eced5369e4f0b3b5dcd45ba13b7401a804dc065e
Tobias Hienzsch <post@tobias-hienzsch.de>
2021-08-30 18:50:59 +0200

    [cmath] Add erf, fmin, fmax, fdim, log2 and log1p

---
+++ etl/_cmath/fdim.hpp
@@ -0,0 +1,61 @@
+/// \copyright Tobias Hienzsch 2019-2021
+/// Distributed under the Boost Software License, Version 1.0.
+/// See accompanying file LICENSE or copy at http://boost.org/LICENSE_1_0.txt
+
+#ifndef TETL_CMATH_FDIM_HPP
+#define TETL_CMATH_FDIM_HPP
+
+#include "etl/_cmath/fmax.hpp"
+
+namespace etl {
+
+/// \brief Returns the positive difference between x and y, that is, if x>y,
+/// returns x-y, otherwise (if x≤y), returns +0.
+///
+/// https://en.cppreference.com/w/cpp/numeric/math/fdim
+[[nodiscard]] constexpr auto fdim(float x, float y) noexcept -> float
+{
+    return etl::fmax(x - y, 0);
+}
+
+/// \brief Returns the positive difference between x and y, that is, if x>y,
+/// returns x-y, otherwise (if x≤y), returns +0.
+///
+/// https://en.cppreference.com/w/cpp/numeric/math/fdim
+[[nodiscard]] constexpr auto fdimf(float x, float y) noexcept -> float
+{
+    return etl::fmax(x - y, 0);
+}
+
+/// \brief Returns the positive difference between x and y, that is, if x>y,
+/// returns x-y, otherwise (if x≤y), returns +0.
+///
+/// https://en.cppreference.com/w/cpp/numeric/math/fdim
+[[nodiscard]] constexpr auto fdim(double x, double y) noexcept -> double
+{
+    return etl::fmax(x - y, 0);
+}
+
+/// \brief Returns the positive difference between x and y, that is, if x>y,
+/// returns x-y, otherwise (if x≤y), returns +0.
+///
+/// https://en.cppreference.com/w/cpp/numeric/math/fdim
+[[nodiscard]] constexpr auto fdim(long double x, long double y) noexcept
+    -> long double
+{
+    return etl::fmax(x - y, 0);
+}
+
+/// \brief Returns the positive difference between x and y, that is, if x>y,
+/// returns x-y, otherwise (if x≤y), returns +0.
+///
+/// https://en.cppreference.com/w/cpp/numeric/math/fdim
+[[nodiscard]] constexpr auto fdiml(long double x, long double y) noexcept
+    -> long double
+{
+    return etl::fmax(x - y, 0);
+}
+
+} // namespace etl
+
+#endif // TETL_CMATH_FDIM_HPP
---
+++ etl/_cmath/log2.hpp
@@ -0,0 +1,60 @@
+/// \copyright Tobias Hienzsch 2019-2021
+/// Distributed under the Boost Software License, Version 1.0.
+/// See accompanying file LICENSE or copy at http://boost.org/LICENSE_1_0.txt
+
+#ifndef TETL_CMATH_LOG2_HPP
+#define TETL_CMATH_LOG2_HPP
+
+#include "etl/_3rd_party/gcem/gcem.hpp"
+#include "etl/_type_traits/enable_if.hpp"
+#include "etl/_type_traits/is_integral.hpp"
+
+namespace etl {
+
+/// \brief Computes the binary (base-2) logarithm of arg.
+/// https://en.cppreference.com/w/cpp/numeric/math/log2
+[[nodiscard]] constexpr auto log2(float v) noexcept -> float
+{
+    return gcem::log2(v);
+}
+
+/// \brief Computes the binary (base-2) logarithm of arg.
+/// https://en.cppreference.com/w/cpp/numeric/math/log2
+[[nodiscard]] constexpr auto log2f(float v) noexcept -> float
+{
+    return gcem::log2(v);
+}
+
+/// \brief Computes the binary (base-2) logarithm of arg.
+/// https://en.cppreference.com/w/cpp/numeric/math/log2
+[[nodiscard]] constexpr auto log2(double v) noexcept -> double
+{
+    return gcem::log2(v);
+}
+
+/// \brief Computes the binary (base-2) logarithm of arg.
+/// https://en.cppreference.com/w/cpp/numeric/math/log2
+[[nodiscard]] constexpr auto log2(long double v) noexcept -> long double
+{
+    return gcem::log2(v);
+}
+
+/// \brief Computes the binary (base-2) logarithm of arg.
+/// https://en.cppreference.com/w/cpp/numeric/math/log2
+[[nodiscard]] constexpr auto log2l(long double v) noexcept -> long double
+{
+    return gcem::log2(v);
+}
+
+/// \brief Computes the binary (base-2) logarithm of arg.
+/// https://en.cppreference.com/w/cpp/numeric/math/log2
+template <typename T>
+[[nodiscard]] constexpr auto log2(T arg) noexcept
+    -> etl::enable_if<etl::is_integral_v<T>, double>
+{
+    return gcem::log2(static_cast<double>(arg));
+}
+
+} // namespace etl
+
+#endif // TETL_CMATH_LOG2_HPP
