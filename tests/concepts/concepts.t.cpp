a448793d2dc758f061ebfea7c6229ffe93626c88
Tobias Hienzsch <post@tobias-hienzsch.de>
2021-08-31 23:23:55 +0200

    [concepts] Remove Catch as a test dependency

---
+++ tests/concepts/concepts.t.cpp
@@ -0,0 +1,110 @@
+/// \copyright Tobias Hienzsch 2019-2021
+/// Distributed under the Boost Software License, Version 1.0.
+/// See accompanying file LICENSE or copy at http://boost.org/LICENSE_1_0.txt
+
+#include "etl/concepts.hpp"
+
+#include "etl/cstdint.hpp"
+
+#include "helper.hpp"
+
+#if defined(__cpp_concepts) && TETL_CPP_STANDARD >= 20
+
+namespace {
+auto floating_point_test(etl::floating_point auto /*v*/) { return true; }
+auto floating_point_test(auto /*v*/) { return false; }
+
+auto integral_test(etl::integral auto /*v*/) { return true; }
+auto integral_test(auto /*v*/) { return false; }
+
+auto signed_integral_test(etl::signed_integral auto /*v*/) { return true; }
+auto signed_integral_test(auto /*v*/) { return false; }
+
+auto unsigned_integral_test(etl::unsigned_integral auto /*v*/) { return true; }
+auto unsigned_integral_test(auto /*v*/) { return false; }
+
+auto destructible_test(etl::destructible auto /*v*/) { return true; }
+auto destructible_test(auto /*v*/) { return false; }
+} // namespace
+
+constexpr auto test() -> bool
+{
+    assert(floating_point_test(143.0));
+    assert(floating_point_test(143.0F));
+    assert(floating_point_test(143.0L));
+
+    assert(!floating_point_test(etl::int8_t(42)));
+    assert(!floating_point_test(etl::uint8_t(42)));
+    assert(!floating_point_test(etl::int16_t(143)));
+    assert(!floating_point_test(etl::uint16_t(143)));
+    assert(!floating_point_test(etl::int32_t(143)));
+    assert(!floating_point_test(etl::uint32_t(143)));
+    assert(!floating_point_test(etl::int64_t(143)));
+    assert(!floating_point_test(etl::uint64_t(143)));
+    assert(!floating_point_test(143));
+    assert(!floating_point_test(143U));
+
+    assert(integral_test(etl::int8_t(42)));
+    assert(integral_test(etl::uint8_t(42)));
+    assert(integral_test(etl::int16_t(143)));
+    assert(integral_test(etl::uint16_t(143)));
+    assert(integral_test(etl::int32_t(143)));
+    assert(integral_test(etl::uint32_t(143)));
+    assert(integral_test(etl::int64_t(143)));
+    assert(integral_test(etl::uint64_t(143)));
+    assert(integral_test(143));
+    assert(integral_test(143U));
+
+    assert(!integral_test(143.0));
+    assert(!integral_test(143.0F));
+    assert(!integral_test(143.0L));
+
+    assert(signed_integral_test(etl::int8_t(42)));
+    assert(signed_integral_test(etl::int16_t(143)));
+    assert(signed_integral_test(etl::int32_t(143)));
+    assert(signed_integral_test(etl::int64_t(143)));
+    assert(signed_integral_test(143));
+
+    assert(!signed_integral_test(etl::uint8_t(42)));
+    assert(!signed_integral_test(etl::uint16_t(143)));
+    assert(!signed_integral_test(etl::uint32_t(143)));
+    assert(!signed_integral_test(etl::uint64_t(143)));
+    assert(!signed_integral_test(143U));
+    assert(!signed_integral_test(143.0));
+    assert(!signed_integral_test(143.0F));
+    assert(!signed_integral_test(143.0L));
+
+    assert(unsigned_integral_test(etl::uint8_t(42)));
+    assert(unsigned_integral_test(etl::uint16_t(143)));
+    assert(unsigned_integral_test(etl::uint32_t(143)));
+    assert(unsigned_integral_test(etl::uint64_t(143)));
+    assert(unsigned_integral_test(143U));
+
+    assert(!unsigned_integral_test(etl::int8_t(42)));
+    assert(!unsigned_integral_test(etl::int16_t(143)));
+    assert(!unsigned_integral_test(etl::int32_t(143)));
+    assert(!unsigned_integral_test(etl::int64_t(143)));
+    assert(!unsigned_integral_test(143));
+    assert(!unsigned_integral_test(143.0));
+    assert(!unsigned_integral_test(143.0F));
+    assert(!unsigned_integral_test(143.0L));
+
+    assert(destructible_test(etl::uint8_t(42)));
+    assert(destructible_test(etl::uint16_t(143)));
+    assert(destructible_test(etl::uint32_t(143)));
+    assert(destructible_test(etl::uint64_t(143)));
+    assert(destructible_test(143U));
+
+    return true;
+}
+
+auto main() -> int
+{
+    assert(test());
+    static_assert(test());
+    return 0;
+}
+
+#else
+auto main() -> int { return 0; }
+#endif
