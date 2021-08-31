6db3854defd8b6d39614a1cbde5c493cb43fd52f
Tobias Hienzsch <post@tobias-hienzsch.de>
2021-08-31 23:31:20 +0200

    [cstdint] Remove Catch as a test dependency

---
+++ tests/cstdint/cstdint.t.cpp
@@ -0,0 +1,27 @@
+/// \copyright Tobias Hienzsch 2019-2021
+/// Distributed under the Boost Software License, Version 1.0.
+/// See accompanying file LICENSE or copy at http://boost.org/LICENSE_1_0.txt
+
+#include "etl/cstdint.hpp"
+
+#include "helper.hpp"
+
+constexpr auto test() -> bool
+{
+    assert(sizeof(etl::int8_t) == 1);
+    assert(sizeof(etl::int16_t) == 2);
+    assert(sizeof(etl::int32_t) == 4);
+    assert(sizeof(etl::int64_t) == 8);
+    assert(sizeof(etl::uint8_t) == 1);
+    assert(sizeof(etl::uint16_t) == 2);
+    assert(sizeof(etl::uint32_t) == 4);
+    assert(sizeof(etl::uint64_t) == 8);
+    return true;
+}
+
+auto main() -> int
+{
+    assert(test());
+    static_assert(test());
+    return 0;
+}
