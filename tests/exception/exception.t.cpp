3c938642cdf1b10aab1321290b747d34c44d3b04
Tobias Hienzsch <post@tobias-hienzsch.de>
2021-08-31 23:58:19 +0200

    [exception] Remove Catch as a test dependency

---
+++ tests/exception/exception.t.cpp
@@ -0,0 +1,21 @@
+/// \copyright Tobias Hienzsch 2019-2021
+/// Distributed under the Boost Software License, Version 1.0.
+/// See accompanying file LICENSE or copy at http://boost.org/LICENSE_1_0.txt
+
+#include "etl/exception.hpp"
+
+#include "helper.hpp"
+
+constexpr auto test() -> bool
+{
+    assert((etl::is_default_constructible_v<etl::exception>));
+    assert((etl::is_constructible_v<etl::exception, char const*>));
+    return true;
+}
+
+auto main() -> int
+{
+    assert(test());
+    static_assert(test());
+    return 0;
+}
---
+++ tests/exception/raise.t.cpp
@@ -0,0 +1,18 @@
+/// \copyright Tobias Hienzsch 2019-2021
+/// Distributed under the Boost Software License, Version 1.0.
+/// See accompanying file LICENSE or copy at http://boost.org/LICENSE_1_0.txt
+
+#include "etl/exception.hpp"
+
+#include "helper.hpp"
+
+auto main() -> int
+{
+    try {
+        etl::raise<etl::exception>("should fail");
+        assert(false);
+    } catch (etl::exception const& e) {
+        assert(true);
+    }
+    return 0;
+}
