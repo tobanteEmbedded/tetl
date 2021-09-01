8d21395de3f79444c117b6bac83ad81eaa5a0a64
Tobias Hienzsch <post@tobias-hienzsch.de>
2021-09-01 09:28:20 +0200

    [algorithm] Start replacing Catch2 tests

---
+++ tests/algorithm/for_each.t.cpp
@@ -0,0 +1,53 @@
+/// \copyright Tobias Hienzsch 2019-2021
+/// Distributed under the Boost Software License, Version 1.0.
+/// See accompanying file LICENSE or copy at http://boost.org/LICENSE_1_0.txt
+
+#include "etl/algorithm.hpp"
+
+#include "etl/array.hpp"
+#include "etl/cstdint.hpp"
+
+#include "testing.hpp"
+
+template <typename T>
+constexpr auto test() -> bool
+{
+    etl::array<T, 4> vec { T(1), T(2), T(3), T(4) };
+
+    // Check how often for_each calls the unary function
+    auto counter { 0 };
+    auto incrementCounter = [&counter](auto& /*unused*/) { counter += 1; };
+
+    // for_each
+    etl::for_each(vec.begin(), vec.end(), incrementCounter);
+    assert(counter == 4);
+
+    // for_each_n
+    counter = 0;
+    etl::for_each_n(vec.begin(), 2, incrementCounter);
+    assert(counter == 2);
+    return true;
+}
+
+constexpr auto test_all() -> bool
+{
+    assert(test<etl::uint8_t>());
+    assert(test<etl::int8_t>());
+    assert(test<etl::uint16_t>());
+    assert(test<etl::int16_t>());
+    assert(test<etl::uint32_t>());
+    assert(test<etl::int32_t>());
+    assert(test<etl::uint64_t>());
+    assert(test<etl::int64_t>());
+    assert(test<float>());
+    assert(test<double>());
+
+    return true;
+}
+
+auto main() -> int
+{
+    assert(test_all());
+    static_assert(test_all());
+    return 0;
+}
---
+++ tests/algorithm/mismatch.t.cpp
@@ -0,0 +1,57 @@
+/// \copyright Tobias Hienzsch 2019-2021
+/// Distributed under the Boost Software License, Version 1.0.
+/// See accompanying file LICENSE or copy at http://boost.org/LICENSE_1_0.txt
+
+#include "etl/algorithm.hpp"
+
+#include "etl/array.hpp"
+#include "etl/cstdint.hpp"
+#include "etl/iterator.hpp"
+
+#include "testing.hpp"
+
+template <typename T>
+constexpr auto test() -> bool
+{
+    // first1,last1,first2
+    {
+        auto lhs    = etl::array { T(0), T(1), T(2) };
+        auto rhs    = etl::array { T(0), T(1), T(3) };
+        auto result = etl::mismatch(begin(lhs), end(lhs), begin(rhs));
+        assert(*result.first == T(2));
+        assert(*result.second == T(3));
+    }
+
+    // first1,last1,first2,last2
+    {
+        auto lhs    = etl::array { T(0), T(1), T(2) };
+        auto rhs    = etl::array { T(0), T(1), T(4) };
+        auto result = etl::mismatch(begin(lhs), end(lhs), begin(rhs), end(rhs));
+        assert(*result.first == T(2));
+        assert(*result.second == T(4));
+    }
+    return true;
+}
+
+constexpr auto test_all() -> bool
+{
+    assert(test<etl::uint8_t>());
+    assert(test<etl::int8_t>());
+    assert(test<etl::uint16_t>());
+    assert(test<etl::int16_t>());
+    assert(test<etl::uint32_t>());
+    assert(test<etl::int32_t>());
+    assert(test<etl::uint64_t>());
+    assert(test<etl::int64_t>());
+    assert(test<float>());
+    assert(test<double>());
+
+    return true;
+}
+
+auto main() -> int
+{
+    assert(test_all());
+    static_assert(test_all());
+    return 0;
+}
