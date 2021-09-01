dbb42fd8f89958f8da7cb1655e6c4eb91c5a1b43
Tobias Hienzsch <post@tobias-hienzsch.de>
2021-09-01 02:54:13 +0200

    [mutex] Remove Catch as a test dependency

---
+++ tests/mutex/lock_guard.t.cpp
@@ -0,0 +1,45 @@
+/// \copyright Tobias Hienzsch 2019-2021
+/// Distributed under the Boost Software License, Version 1.0.
+/// See accompanying file LICENSE or copy at http://boost.org/LICENSE_1_0.txt
+
+#include "etl/mutex.hpp"
+#include "etl/warning.hpp"
+
+#include "test_mutex.hpp"
+#include "testing.hpp"
+
+auto test() -> bool
+{
+    //  "not locked"
+    {
+        test_mutex mtx {};
+        assert(!mtx.is_locked());
+        {
+            etl::lock_guard lock { mtx };
+            assert(mtx.is_locked());
+            etl::ignore_unused(lock);
+        }
+        assert(!mtx.is_locked());
+    }
+
+    // "already locked"
+    {
+        test_mutex mtx {};
+        mtx.lock();
+        {
+            etl::lock_guard lock { mtx, etl::adopt_lock };
+            assert(mtx.is_locked());
+            etl::ignore_unused(lock);
+        }
+        assert(!mtx.is_locked());
+    }
+
+    return true;
+}
+
+auto main() -> int
+{
+    assert(test());
+    // static_assert(test());
+    return 0;
+}
---
+++ tests/mutex/test_mutex.hpp
@@ -0,0 +1,52 @@
+/// \copyright Tobias Hienzsch 2019-2021
+/// Distributed under the Boost Software License, Version 1.0.
+/// See accompanying file LICENSE or copy at http://boost.org/LICENSE_1_0.txt
+
+#ifndef TETL_TEST_MUTEX_TEST_MUTEX_HPP
+#define TETL_TEST_MUTEX_TEST_MUTEX_HPP
+
+struct test_mutex {
+    constexpr test_mutex(bool failOnTryLock = false) noexcept
+        : failOnTryLock_ { failOnTryLock }
+    {
+    }
+
+    ~test_mutex() noexcept = default;
+
+    auto operator=(test_mutex const&) -> test_mutex& = delete;
+    test_mutex(test_mutex const&)                    = delete;
+
+    auto operator=(test_mutex&&) -> test_mutex& = default;
+    test_mutex(test_mutex&&)                    = default;
+
+    constexpr auto lock() noexcept
+    {
+        if (not isLocked_) { isLocked_ = true; }
+    }
+
+    constexpr auto try_lock() noexcept -> bool
+    {
+        if (not isLocked_ && not failOnTryLock_) {
+            isLocked_ = true;
+            return true;
+        }
+
+        return false;
+    }
+
+    constexpr auto unlock() noexcept
+    {
+        if (isLocked_) { isLocked_ = false; }
+    }
+
+    [[nodiscard]] constexpr auto is_locked() const noexcept
+    {
+        return isLocked_;
+    }
+
+private:
+    bool failOnTryLock_ { false };
+    bool isLocked_ = false;
+};
+
+#endif // TETL_TEST_MUTEX_TEST_MUTEX_HPP
