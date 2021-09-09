f1c610584cdc308b64663b8b2be813d1de7c4578
Tobias Hienzsch <post@tobias-hienzsch.de>
2021-09-09 02:09:23 +0200

    [cmath] Add hypot overloads

--- etl/_cmath/hypot.hpp
+++ etl/_cmath/hypot.hpp
@@ -90,6 +90,53 @@ namespace etl {
     return etl::sqrt(x * x + y * y);
 }
 
+/// \brief Computes the square root of the sum of the squares of x, y, and z,
+/// without undue overflow or underflow at intermediate stages of the
+/// computation.
+///
+/// - hypot(x,y) is INF if x or y is +INF or -INF; else
+/// - hypot(x,y) is NAN if x or y is NAN.
+///
+/// https://en.cppreference.com/w/cpp/numeric/math/hypot
+[[nodiscard]] constexpr auto hypot(float x, float y, float z) noexcept -> float
+{
+    if (etl::isinf(x) || etl::isinf(x)) { return TETL_BUILTIN_HUGE_VALF; }
+    if (etl::isnan(x) || etl::isnan(x)) { return TETL_BUILTIN_NANF; }
+    return etl::sqrt(x * x + y * y + z * z);
+}
+
+/// \brief Computes the square root of the sum of the squares of x, y, and z,
+/// without undue overflow or underflow at intermediate stages of the
+/// computation.
+///
+/// - hypot(x,y) is INF if x or y is +INF or -INF; else
+/// - hypot(x,y) is NAN if x or y is NAN.
+///
+/// https://en.cppreference.com/w/cpp/numeric/math/hypot
+[[nodiscard]] constexpr auto hypot(double x, double y, double z) noexcept
+    -> double
+{
+    if (etl::isinf(x) || etl::isinf(x)) { return TETL_BUILTIN_HUGE_VAL; }
+    if (etl::isnan(x) || etl::isnan(x)) { return TETL_BUILTIN_NAN; }
+    return etl::sqrt(x * x + y * y + z * z);
+}
+
+/// \brief Computes the square root of the sum of the squares of x, y, and z,
+/// without undue overflow or underflow at intermediate stages of the
+/// computation.
+///
+/// - hypot(x,y) is INF if x or y is +INF or -INF; else
+/// - hypot(x,y) is NAN if x or y is NAN.
+///
+/// https://en.cppreference.com/w/cpp/numeric/math/hypot
+[[nodiscard]] constexpr auto hypot(
+    long double x, long double y, long double z) noexcept -> long double
+{
+    if (etl::isinf(x) || etl::isinf(x)) { return TETL_BUILTIN_HUGE_VALL; }
+    if (etl::isnan(x) || etl::isnan(x)) { return TETL_BUILTIN_NANL; }
+    return etl::sqrt(x * x + y * y + z * z);
+}
+
 } // namespace etl
 
 #endif // TETL_CMATH_HYPOT_HPP
