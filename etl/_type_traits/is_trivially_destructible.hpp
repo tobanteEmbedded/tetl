019c097c924b69bd71a53361a226f2739385bec6
Tobias Hienzsch <post@tobias-hienzsch.de>
2022-08-21 20:25:11 +0200

    [type_traits] Fix clang-15 deprecated warning

--- etl/_type_traits/is_trivially_destructible.hpp
+++ etl/_type_traits/is_trivially_destructible.hpp
@@ -16,12 +16,27 @@ namespace etl {
 ///
 /// https://en.cppreference.com/w/cpp/types/is_destructible
 template <typename T>
+struct is_trivially_destructible;
+
+#if __has_builtin(__is_trivially_destructible)
+
+template <typename T>
+struct is_trivially_destructible : bool_constant<__is_trivially_destructible(T)> {
+};
+
+template <typename T>
+inline constexpr bool is_trivially_destructible_v = __is_trivially_destructible(T);
+
+#else
+
+template <typename T>
 struct is_trivially_destructible : bool_constant<__has_trivial_destructor(T)> {
 };
 
 template <typename T>
-inline constexpr auto is_trivially_destructible_v = __has_trivial_destructor(T);
+inline constexpr bool is_trivially_destructible_v = __has_trivial_destructor(T);
 
+#endif
 } // namespace etl
 
 #endif // TETL_TYPE_TRAITS_IS_TRVIALLY_DETRUCTIBLE_HPP
