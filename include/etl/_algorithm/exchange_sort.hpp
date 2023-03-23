971e98b89ba204fb8b3917f6596e56f20b7c6e07
Tobias Hienzsch <post@tobias-hienzsch.de>
2023-03-23 21:02:09 +0100

    [algorithm] Add exchange_sort

---
+++ include/etl/_algorithm/exchange_sort.hpp
@@ -0,0 +1,36 @@
+/// \copyright Tobias Hienzsch 2019-2021
+/// Distributed under the Boost Software License, Version 1.0.
+/// See accompanying file LICENSE or copy at http://boost.org/LICENSE_1_0.txt
+
+#ifndef TETL_ALGORITHM_EXCHANGE_SORT_HPP
+#define TETL_ALGORITHM_EXCHANGE_SORT_HPP
+
+#include "etl/_algorithm/iter_swap.hpp"
+#include "etl/_functional/less.hpp"
+#include "etl/_iterator/next.hpp"
+#include "etl/_iterator/prev.hpp"
+
+namespace etl {
+
+/// \brief Sorts the elements in the range `[first, last)` in non-descending
+/// order.
+/// https://en.wikipedia.org/wiki/Sorting_algorithm#Exchange_sort
+template <typename RandomIt, typename Compare>
+constexpr auto exchange_sort(RandomIt first, RandomIt last, Compare comp) -> void
+{
+    for (auto i = first; i < prev(last); ++i) {
+        for (auto j = next(i); j < last; ++j) {
+            if (comp(*j, *i)) { iter_swap(i, j); }
+        }
+    }
+}
+
+template <typename RandomIt>
+constexpr auto exchange_sort(RandomIt first, RandomIt last) -> void
+{
+    exchange_sort(first, last, less<> {});
+}
+
+} // namespace etl
+
+#endif // TETL_ALGORITHM_EXCHANGE_SORT_HPP
