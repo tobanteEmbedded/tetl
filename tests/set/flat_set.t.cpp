8a5131627958af92a4a26a3ebc1e578ab9741a8d
Tobias Hienzsch <post@tobias-hienzsch.de>
2023-03-27 21:04:44 +0200

    [set] Improve flat_set tests

--- tests/set/flat_set.t.cpp
+++ tests/set/flat_set.t.cpp
@@ -2,10 +2,11 @@
 /// Distributed under the Boost Software License, Version 1.0.
 /// See accompanying file LICENSE or copy at http://boost.org/LICENSE_1_0.txt
 
-#include "etl/set.hpp"
+#include <etl/set.hpp>
 
-#include "etl/cstdint.hpp"
-#include "etl/vector.hpp"
+#include <etl/cstdint.hpp>
+#include <etl/utility.hpp>
+#include <etl/vector.hpp>
 
 #include "testing/testing.hpp"
 
@@ -18,12 +19,34 @@ constexpr auto test() -> bool
     auto s1 = set_t {};
     assert(s1.size() == 0); // NOLINT
     assert(s1.empty());
+    assert(s1.begin() == s1.end());
+    assert(etl::as_const(s1).begin() == etl::as_const(s1).end());
+    assert(s1.cbegin() == s1.cend());
     assert(s1.max_size() == 8);
 
     auto s2 = set_t { vec_t {} };
     assert(s2.size() == 0); // NOLINT
+    assert(s2.begin() == s2.end());
+    assert(s1.begin() != s2.begin());
+    assert(s1.end() != s2.end());
     assert(s2.empty());
     assert(s2.max_size() == 8);
+    assert(s2.find(T(42)) == etl::end(s2));
+
+    auto r1 = s2.emplace(T(42));
+    assert(r1.second);
+    assert(s2.size() == 1);
+    assert(s2.find(T(42)) == etl::begin(s2));
+
+    auto r2 = s2.insert(T(42));
+    assert(not r2.second);
+    assert(s2.size() == 1);
+    assert(s2.find(T(42)) == etl::begin(s2));
+
+    auto v = etl::array<T, 3> { T(1), T(2), T(3) };
+    s2.insert(v.begin(), v.end());
+    assert(s2.size() == 4);
+
     return true;
 }
 
