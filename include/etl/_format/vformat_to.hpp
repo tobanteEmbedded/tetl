d3a2a7af126745490cc31353cb01c550cb970a2d
Tobias Hienzsch <post@tobias-hienzsch.de>
2023-03-28 17:14:53 +0200

    [format] Ignore unused variables

--- include/etl/_format/vformat_to.hpp
+++ include/etl/_format/vformat_to.hpp
@@ -9,12 +9,14 @@
 #include <etl/_format/basic_format_context.hpp>
 #include <etl/_iterator/back_insert_iterator.hpp>
 #include <etl/_string_view/basic_string_view.hpp>
+#include <etl/_warning/ignore_unused.hpp>
 
 namespace etl {
 
 template <typename OutputIt>
 auto vformat_to(OutputIt out, string_view fmt, format_args args) -> OutputIt
 {
+    ignore_unused(fmt, args);
     auto buffer = detail::fmt_buffer<char> { out };
     auto it     = back_inserter(buffer);
     return out;
