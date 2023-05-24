c36c6c10bd16ece50e0824ca9cd7712cb852c758
Tobias Hienzsch <post@tobias-hienzsch.de>
2023-05-24 15:48:19 +0200

    Start string_constant implementation.

---
+++ include/etl/experimental/string_constant/README.md
@@ -0,0 +1,8 @@
+# String Constant
+
+- [Embedded Logging Case Study: From C to Shining C++ - Luke Valenty -CppNow 2022](https://youtu.be/Dt0vx-7e_B0)
+
+```sh
+arm-none-eabi-g++.exe -std=c++23 -c -I include -fno-exceptions test.cpp
+arm-none-eabi-nm.exe -uC test.o > undef.txt
+```
---
+++ include/etl/experimental/string_constant/test.cpp
@@ -0,0 +1,54 @@
+#include <etl/array.hpp>
+#include <etl/cstdint.hpp>
+#include <etl/string_view.hpp>
+
+template <typename T, etl::size_t Size>
+using c_array = T[Size];
+
+using string_id = etl::uint32_t;
+
+template <typename StringType>
+extern auto catalog() -> string_id;
+
+template <typename StringType>
+inline auto catalog(StringType) -> string_id
+{
+    return catalog<StringType>();
+}
+
+template <typename CharT, CharT... chars>
+struct string_constant {
+private:
+    using This       = string_constant<CharT, chars...>;
+    using StringView = etl::basic_string_view<CharT>;
+
+    static constexpr etl::array<CharT, sizeof...(chars)> storage { chars... };
+};
+
+template <typename T, T... chars>
+constexpr auto operator""_sc()
+{
+    return string_constant<T, chars...> {};
+}
+
+template <auto Str>
+consteval auto to_string_constant()
+{
+    return []<etl::size_t... idx>(etl::index_sequence<idx...>)
+    {
+        return string_constant<char, etl::get<idx>(Str)...> {};
+    }
+    (etl::make_index_sequence<etl::size(Str)> {});
+}
+
+#define TETL_STRING_CONSTANT(str) to_string_constant<etl::to_array((str))>()
+
+auto bar()
+{
+    constexpr auto x = etl::to_array("test");
+    static_assert(x.size() == 5);
+
+    catalog("test"_sc);
+    catalog("bar"_sc);
+    catalog(TETL_STRING_CONSTANT("tobias hienzsch"));
+}
