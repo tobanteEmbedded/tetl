6548a1e1b7d910fd3559597f9862b28e22871a58
Tobias Hienzsch <post@tobias-hienzsch.de>
2023-06-16 02:10:42 +0200

    [complex] Add polar

---
+++ include/etl/_complex/polar.hpp
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: BSL-1.0
+
+#ifndef TETL_COMPLEX_POLAR_HPP
+#define TETL_COMPLEX_POLAR_HPP
+
+#include <etl/_cmath/cos.hpp>
+#include <etl/_cmath/sin.hpp>
+#include <etl/_complex/complex.hpp>
+
+namespace etl {
+
+template <typename T>
+[[nodiscard]] constexpr auto polar(T const& r, T const& theta = T()) noexcept -> complex<T>
+{
+    return complex<T> { r * cos(theta), r * sin(theta) };
+}
+
+} // namespace etl
+
+#endif // TETL_COMPLEX_POLAR_HPP
--- include/etl/complex.hpp
+++ include/etl/complex.hpp
@@ -3,21 +3,22 @@
 #ifndef TETL_COMPLEX_HPP
 #define TETL_COMPLEX_HPP
 
-#include "etl/_config/all.hpp"
+#include <etl/_config/all.hpp>
 
-#include "etl/_complex/abs.hpp"
-#include "etl/_complex/args.hpp"
-#include "etl/_complex/complex.hpp"
-#include "etl/_complex/conj.hpp"
-#include "etl/_complex/cos.hpp"
-#include "etl/_complex/cosh.hpp"
-#include "etl/_complex/imag.hpp"
-#include "etl/_complex/literals.hpp"
-#include "etl/_complex/norm.hpp"
-#include "etl/_complex/real.hpp"
-#include "etl/_complex/sin.hpp"
-#include "etl/_complex/sinh.hpp"
-#include "etl/_complex/tan.hpp"
-#include "etl/_complex/tanh.hpp"
+#include <etl/_complex/abs.hpp>
+#include <etl/_complex/args.hpp>
+#include <etl/_complex/complex.hpp>
+#include <etl/_complex/conj.hpp>
+#include <etl/_complex/cos.hpp>
+#include <etl/_complex/cosh.hpp>
+#include <etl/_complex/imag.hpp>
+#include <etl/_complex/literals.hpp>
+#include <etl/_complex/norm.hpp>
+#include <etl/_complex/polar.hpp>
+#include <etl/_complex/real.hpp>
+#include <etl/_complex/sin.hpp>
+#include <etl/_complex/sinh.hpp>
+#include <etl/_complex/tan.hpp>
+#include <etl/_complex/tanh.hpp>
 
 #endif // TETL_COMPLEX_HPP
