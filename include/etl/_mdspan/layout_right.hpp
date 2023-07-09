6c128fea017bce9d072d99cc9930e8599ed83b50
Tobias Hienzsch <post@tobias-hienzsch.de>
2023-07-09 19:40:23 +0200

    [mdspan] Add tests for layout_right

--- include/etl/_mdspan/layout_right.hpp
+++ include/etl/_mdspan/layout_right.hpp
@@ -3,7 +3,10 @@
 #ifndef TETL_MDSPAN_LAYOUT_RIGHT_HPP
 #define TETL_MDSPAN_LAYOUT_RIGHT_HPP
 
+#include "etl/_mdspan/extents.hpp"
+#include "etl/_mdspan/is_extents.hpp"
 #include "etl/_mdspan/layout.hpp"
+#include "etl/_type_traits/is_convertible.hpp"
 
 namespace etl {
 
@@ -19,13 +22,23 @@ struct layout_right::mapping {
     constexpr mapping() noexcept               = default;
     constexpr mapping(mapping const&) noexcept = default;
 
-    constexpr mapping(extents_type const&) noexcept;
+    constexpr mapping(extents_type const& ext) noexcept : extents_ { ext } { }
 
     template <typename OtherExtents>
-    constexpr explicit(!is_convertible_v<OtherExtents, extents_type>) mapping(mapping<OtherExtents> const&) noexcept;
+        requires is_constructible_v<extents_type, OtherExtents>
+    constexpr explicit(!is_convertible_v<OtherExtents, extents_type>)
+        mapping(mapping<OtherExtents> const& other) noexcept
+        : extents_ { other.extents() }
+    {
+    }
 
     template <typename OtherExtents>
-    constexpr explicit(false /* see description */) mapping(layout_left::mapping<OtherExtents> const&) noexcept;
+        requires(extents_type::rank() <= 1) && is_constructible_v<extents_type, OtherExtents>
+    constexpr explicit(!is_convertible_v<OtherExtents, extents_type>)
+        mapping(layout_left::mapping<OtherExtents> const& other) noexcept
+        : extents_ { other.extents() }
+    {
+    }
 
     template <typename OtherExtents>
     constexpr explicit(extents_type::rank() > 0) mapping(layout_stride::mapping<OtherExtents> const&) noexcept;
@@ -35,7 +48,10 @@ struct layout_right::mapping {
     // observers
     [[nodiscard]] constexpr auto extents() const noexcept -> extents_type const& { return extents_; }
 
-    [[nodiscard]] constexpr auto required_span_size() const noexcept -> index_type;
+    [[nodiscard]] constexpr auto required_span_size() const noexcept -> index_type
+    {
+        return static_cast<index_type>(detail::fwd_prod_of_extents(extents(), extents_type::rank()));
+    }
 
     template <typename... Indices>
     [[nodiscard]] constexpr auto operator()(Indices...) const noexcept -> index_type;
@@ -51,7 +67,10 @@ struct layout_right::mapping {
     [[nodiscard]] constexpr auto stride(rank_type) const noexcept -> index_type;
 
     template <typename OtherExtents>
-    friend constexpr auto operator==(mapping const&, mapping<OtherExtents> const&) noexcept -> bool;
+    friend constexpr auto operator==(mapping const& lhs, mapping<OtherExtents> const& rhs) noexcept -> bool
+    {
+        return lhs.extents() == rhs.extents();
+    }
 
 private:
     extents_type extents_ {};
---
+++ tests/mdspan/layout_right.t.cpp
@@ -0,0 +1,117 @@
+// SPDX-License-Identifier: BSL-1.0
+
+#include <etl/concepts.hpp>
+#include <etl/mdspan.hpp>
+#include <etl/type_traits.hpp>
+
+#include "testing/testing.hpp"
+
+template <typename IndexType>
+constexpr auto test_layout_right() -> bool
+{
+    using extents_2d_t = etl::extents<IndexType, 2, etl::dynamic_extent>;
+    using mapping_2d_t = etl::layout_right::mapping<extents_2d_t>;
+
+    static_assert(etl::is_trivial_v<etl::layout_right>);
+    static_assert(etl::is_trivially_copyable_v<mapping_2d_t>);
+
+    static_assert(etl::same_as<typename mapping_2d_t::extents_type, extents_2d_t>);
+    static_assert(etl::same_as<typename mapping_2d_t::index_type, IndexType>);
+    static_assert(etl::same_as<typename mapping_2d_t::size_type, typename extents_2d_t::size_type>);
+    static_assert(etl::same_as<typename mapping_2d_t::rank_type, typename extents_2d_t::rank_type>);
+    static_assert(etl::same_as<typename mapping_2d_t::layout_type, etl::layout_right>);
+
+    {
+        auto const mapping = mapping_2d_t {};
+        auto const copy    = mapping;
+        assert(copy == mapping);
+        static_assert(etl::is_nothrow_default_constructible_v<mapping_2d_t>);
+        static_assert(etl::is_nothrow_copy_constructible_v<mapping_2d_t>);
+    }
+
+    {
+        auto mapping = mapping_2d_t {};
+        auto copy    = mapping_2d_t {};
+        copy         = mapping;
+        assert(copy == mapping);
+        static_assert(etl::is_nothrow_copy_assignable_v<mapping_2d_t>);
+    }
+
+    {
+        auto ext     = extents_2d_t(42);
+        auto mapping = mapping_2d_t(ext);
+        assert(mapping.extents() == ext);
+        static_assert(etl::is_nothrow_constructible_v<mapping_2d_t, extents_2d_t>);
+    }
+
+    {
+        assert(mapping_2d_t::is_always_unique());
+        assert(mapping_2d_t::is_always_exhaustive());
+        assert(mapping_2d_t::is_always_strided());
+        assert(mapping_2d_t::is_unique());
+        assert(mapping_2d_t::is_exhaustive());
+        assert(mapping_2d_t::is_strided());
+
+        assert(mapping_2d_t(extents_2d_t(1)).required_span_size() == 2);
+        assert(mapping_2d_t(extents_2d_t(42)).required_span_size() == 84);
+        assert(mapping_2d_t(extents_2d_t(42)).required_span_size() == 84);
+    }
+
+    {
+        using extents_3d_t = etl::extents<IndexType, etl::dynamic_extent, 2, etl::dynamic_extent>;
+        using mapping_3d_t = etl::layout_right::mapping<extents_3d_t>;
+
+        assert(mapping_3d_t::is_always_unique());
+        assert(mapping_3d_t::is_always_exhaustive());
+        assert(mapping_3d_t::is_always_strided());
+        assert(mapping_3d_t::is_unique());
+        assert(mapping_3d_t::is_exhaustive());
+        assert(mapping_3d_t::is_strided());
+
+        assert(mapping_3d_t(extents_3d_t(1, 1)).required_span_size() == 2);
+        assert(mapping_3d_t(extents_3d_t(1, 2)).required_span_size() == 4);
+        assert(mapping_3d_t(extents_3d_t(2, 2)).required_span_size() == 8);
+    }
+
+    {
+        using extents_4d_t = etl::extents<IndexType, etl::dynamic_extent, etl::dynamic_extent, 2, etl::dynamic_extent>;
+        using mapping_4d_t = etl::layout_right::mapping<extents_4d_t>;
+
+        assert(mapping_4d_t::is_always_unique());
+        assert(mapping_4d_t::is_always_exhaustive());
+        assert(mapping_4d_t::is_always_strided());
+        assert(mapping_4d_t::is_unique());
+        assert(mapping_4d_t::is_exhaustive());
+        assert(mapping_4d_t::is_strided());
+
+        assert(mapping_4d_t(extents_4d_t(1, 1, 1)).required_span_size() == 2);
+        assert(mapping_4d_t(extents_4d_t(1, 1, 2)).required_span_size() == 4);
+        assert(mapping_4d_t(extents_4d_t(3, 2, 2)).required_span_size() == 24);
+    }
+
+    return true;
+}
+
+constexpr auto test_all() -> bool
+{
+    assert(test_layout_right<etl::uint8_t>());
+    assert(test_layout_right<etl::uint16_t>());
+    assert(test_layout_right<etl::uint32_t>());
+    assert(test_layout_right<etl::uint64_t>());
+
+    assert(test_layout_right<etl::int8_t>());
+    assert(test_layout_right<etl::int16_t>());
+    assert(test_layout_right<etl::int32_t>());
+    assert(test_layout_right<etl::int64_t>());
+
+    assert(test_layout_right<etl::size_t>());
+    assert(test_layout_right<etl::ptrdiff_t>());
+    return true;
+}
+
+auto main() -> int
+{
+    assert(test_all());
+    static_assert(test_all());
+    return 0;
+}
