19b80fbc56af74014d58f3e5e892e4fe93014403
Tobias Hienzsch <post@tobias-hienzsch.de>
2023-07-23 00:17:23 +0200

    [linalg] Add blas1 vector_two_norm

---
+++ include/etl/_linalg/blas1_vector_two_norm.hpp
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: BSL-1.0
+
+#ifndef TETL_LINALG_BLAS1_VECTOR_TWO_NORM_HPP
+#define TETL_LINALG_BLAS1_VECTOR_TWO_NORM_HPP
+
+#include <etl/_cmath/sqrt.hpp>
+#include <etl/_linalg/concepts.hpp>
+#include <etl/_type_traits/declval.hpp>
+#include <etl/_utility/cmp.hpp>
+
+namespace etl::linalg {
+
+template <detail::in_vector InVec, typename Scalar>
+constexpr auto vector_two_norm(InVec v, Scalar init) noexcept -> Scalar
+{
+    auto sum = init;
+    for (typename InVec::size_type i { 0 }; cmp_less(i, v.extent(0)); ++i) {
+        auto const val    = detail::abs_if_needed(v(i));
+        auto const square = val * val;
+        sum += square;
+    }
+
+    using etl::sqrt;
+    return static_cast<Scalar>(sqrt(sum));
+}
+
+template <detail::in_vector InVec>
+constexpr auto vector_two_norm(InVec v) noexcept -> typename InVec::value_type
+{
+    using abs_type   = decltype(detail::abs_if_needed(declval<typename InVec::value_type>()));
+    using value_type = decltype(declval<abs_type>() * declval<abs_type>());
+    return vector_two_norm(v, value_type {});
+}
+
+} // namespace etl::linalg
+
+#endif // TETL_LINALG_BLAS1_VECTOR_TWO_NORM_HPP
---
+++ tests/linalg/blas1_vector_two_norm.t.cpp
@@ -0,0 +1,59 @@
+// SPDX-License-Identifier: BSL-1.0
+
+#include <etl/linalg.hpp>
+
+#include <etl/array.hpp>
+#include <etl/concepts.hpp>
+#include <etl/mdspan.hpp>
+
+#include "testing/testing.hpp"
+
+template <typename T, typename IndexType>
+[[nodiscard]] static constexpr auto test_linalg_vector_two_norm_real() -> bool
+{
+    auto const data = etl::array<T, 4> { T(2), T(2), T(2), T(2) };
+    {
+        // static extents
+        auto const vec = etl::mdspan<T const, etl::extents<IndexType, 4>> { data.data() };
+        assert(etl::linalg::vector_two_norm(vec) == T(4));
+        assert(etl::linalg::vector_two_norm(etl::linalg::scaled(T(2), vec)) == T(8));
+
+        assert(etl::linalg::vector_two_norm(vec, T(0)) == T(4));
+        assert(etl::linalg::vector_two_norm(etl::linalg::scaled(T(2), vec), T(0)) == T(8));
+    }
+
+    return true;
+}
+
+template <typename IndexType>
+[[nodiscard]] static constexpr auto test_index_type() -> bool
+{
+    assert(test_linalg_vector_two_norm_real<float, IndexType>());
+    assert(test_linalg_vector_two_norm_real<double, IndexType>());
+
+    return true;
+}
+
+[[nodiscard]] static constexpr auto test_all() -> bool
+{
+    assert(test_index_type<signed char>());
+    assert(test_index_type<signed short>());
+    assert(test_index_type<signed int>());
+    assert(test_index_type<signed long>());
+    assert(test_index_type<signed long long>());
+
+    assert(test_index_type<unsigned char>());
+    assert(test_index_type<unsigned short>());
+    assert(test_index_type<unsigned int>());
+    assert(test_index_type<unsigned long>());
+    assert(test_index_type<unsigned long long>());
+
+    return true;
+}
+
+auto main() -> int
+{
+    assert(test_all());
+    static_assert(test_all());
+    return EXIT_SUCCESS;
+}
