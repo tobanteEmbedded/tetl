ce965284c2fb45989b47f6c18ef641d3d02ffa06
Tobias Hienzsch <post@tobias-hienzsch.de>
2023-11-29 06:10:45 +0100

    Modernize nested inline namespaces

--- include/etl/_string_view/literal.hpp
+++ include/etl/_string_view/literal.hpp
@@ -5,18 +5,15 @@
 
 #include "etl/_string_view/string_view.hpp"
 
-namespace etl {
-
-inline namespace literals {
-inline namespace string_view_literals {
+namespace etl::inline literals::inline string_view_literals {
 
 /// \brief Forms a string view of a character literal. Returns
 /// etl::string_view{str, len}
-constexpr auto operator""_sv(char const* str, etl::size_t len) noexcept -> etl::string_view { return { str, len }; }
+[[nodiscard]] constexpr auto operator""_sv(char const* str, etl::size_t len) noexcept -> etl::string_view
+{
+    return { str, len };
+}
 
-} // namespace string_view_literals
-
-} // namespace literals
-} // namespace etl
+} // namespace etl::inline literals::inline string_view_literals
 
 #endif // TETL_STRING_VIEW_LITERAL_HPP
