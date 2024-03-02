800939bbb4a0cfa1d8546eae69d6bec930436af7
Tobias Hienzsch <post@tobias-hienzsch.de>
2024-03-02 07:43:33 +0100

    [memory] Add uninitialized_fill

---
+++ include/etl/_memory/uninitialized_fill.hpp
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: BSL-1.0
+
+#ifndef TETL_MEMORY_UNINITIALIZED_FILL_HPP
+#define TETL_MEMORY_UNINITIALIZED_FILL_HPP
+
+#include <etl/_iterator/iterator_traits.hpp>
+#include <etl/_memory/addressof.hpp>
+
+namespace etl {
+
+template <typename ForwardIt, typename T>
+auto uninitialized_fill(ForwardIt first, ForwardIt last, T const& value) -> void
+{
+    using ValueType = typename etl::iterator_traits<ForwardIt>::value_type;
+
+#if defined(__cpp_exceptions)
+    auto current = first;
+    try {
+        for (; current != last; ++current) { ::new (static_cast<void*>(etl::addressof(*current))) ValueType(value); }
+    } catch (...) {
+        for (; first != current; ++first) { first->~V(); }
+        throw;
+    }
+#else
+    for (auto current = first; current != last; ++current) {
+        ::new (static_cast<void*>(etl::addressof(*current))) ValueType(value);
+    }
+#endif
+}
+
+} // namespace etl
+
+#endif // TETL_MEMORY_UNINITIALIZED_FILL_HPP
--- include/etl/memory.hpp
+++ include/etl/memory.hpp
@@ -5,23 +5,24 @@
 
 /// \example memory.cpp
 
-#include "etl/_config/all.hpp"
+#include <etl/_config/all.hpp>
 
-#include "etl/_memory/addressof.hpp"
-#include "etl/_memory/align.hpp"
-#include "etl/_memory/allocator_arg_t.hpp"
-#include "etl/_memory/assume_aligned.hpp"
-#include "etl/_memory/construct_at.hpp"
-#include "etl/_memory/default_delete.hpp"
-#include "etl/_memory/destroy.hpp"
-#include "etl/_memory/destroy_at.hpp"
-#include "etl/_memory/destroy_n.hpp"
-#include "etl/_memory/pointer_int_pair.hpp"
-#include "etl/_memory/pointer_int_pair_info.hpp"
-#include "etl/_memory/pointer_like_traits.hpp"
-#include "etl/_memory/pointer_traits.hpp"
-#include "etl/_memory/small_ptr.hpp"
-#include "etl/_memory/to_address.hpp"
-#include "etl/_memory/uses_allocator.hpp"
+#include <etl/_memory/addressof.hpp>
+#include <etl/_memory/align.hpp>
+#include <etl/_memory/allocator_arg_t.hpp>
+#include <etl/_memory/assume_aligned.hpp>
+#include <etl/_memory/construct_at.hpp>
+#include <etl/_memory/default_delete.hpp>
+#include <etl/_memory/destroy.hpp>
+#include <etl/_memory/destroy_at.hpp>
+#include <etl/_memory/destroy_n.hpp>
+#include <etl/_memory/pointer_int_pair.hpp>
+#include <etl/_memory/pointer_int_pair_info.hpp>
+#include <etl/_memory/pointer_like_traits.hpp>
+#include <etl/_memory/pointer_traits.hpp>
+#include <etl/_memory/small_ptr.hpp>
+#include <etl/_memory/to_address.hpp>
+#include <etl/_memory/uninitialized_fill.hpp>
+#include <etl/_memory/uses_allocator.hpp>
 
 #endif // TETL_MEMORY_HPP
