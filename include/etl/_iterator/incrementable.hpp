ee6df8105b60997fc78272b959c96bb1e1648845
Tobias Hienzsch <post@tobias-hienzsch.de>
2024-03-08 09:55:21 +0100

    [iterator] Fix clang-tidy warning in incrementable

--- include/etl/_iterator/incrementable.hpp
+++ include/etl/_iterator/incrementable.hpp
@@ -10,9 +10,9 @@
 namespace etl {
 
 // clang-format off
-template <typename I>
-concept incrementable = etl::regular<I> and etl::weakly_incrementable<I> and requires(I i) {
-    { i++ } -> etl::same_as<I>;
+template <typename T>
+concept incrementable = etl::regular<T> and etl::weakly_incrementable<T> and requires(T i) {
+    { i++ } -> etl::same_as<T>;
 };
 // clang-format on
 
