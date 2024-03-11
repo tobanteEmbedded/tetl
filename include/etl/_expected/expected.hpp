b4276ab0c93594b293c345cb97e66bb4614ff3b2
Tobias Hienzsch <post@tobias-hienzsch.de>
2024-03-12 00:01:20 +0100

    [expected] Add and_then and or_else monadic operations

--- include/etl/_expected/expected.hpp
+++ include/etl/_expected/expected.hpp
@@ -6,10 +6,13 @@
 #include <etl/_concepts/same_as.hpp>
 #include <etl/_expected/unexpect.hpp>
 #include <etl/_expected/unexpected.hpp>
+#include <etl/_functional/invoke.hpp>
+#include <etl/_type_traits/invoke_result.hpp>
 #include <etl/_type_traits/is_constructible.hpp>
 #include <etl/_type_traits/is_default_constructible.hpp>
 #include <etl/_type_traits/is_nothrow_constructible.hpp>
 #include <etl/_type_traits/is_nothrow_default_constructible.hpp>
+#include <etl/_type_traits/remove_cvref.hpp>
 #include <etl/_utility/forward.hpp>
 #include <etl/_utility/in_place.hpp>
 #include <etl/_utility/in_place_index.hpp>
@@ -83,6 +86,74 @@ struct expected {
     [[nodiscard]] constexpr auto value_or(U&& fallback) && -> T
     {
         return static_cast<bool>(*this) ? etl::move(**this) : static_cast<T>(etl::forward<U>(fallback));
+    }
+
+    template <typename F>
+    [[nodiscard]] constexpr auto and_then(F&& f) & requires(etl::is_constructible_v<E, decltype(error())>)
+    {
+        if (has_value()) { return etl::invoke(etl::forward<F>(f), **this); }
+        using U = etl::remove_cvref_t<etl::invoke_result_t<F, decltype(**this)>>;
+        return U(etl::unexpect, error());
+    }
+
+    template <typename F>
+    [[nodiscard]] constexpr auto and_then(F&& f) && requires(etl::is_constructible_v<E, decltype(error())>)
+    {
+        if (has_value()) { return etl::invoke(etl::forward<F>(f), **this); }
+        using U = etl::remove_cvref_t<etl::invoke_result_t<F, decltype(**this)>>;
+        return U(etl::unexpect, error());
+    }
+
+    template <typename F>
+    [[nodiscard]] constexpr auto and_then(F&& f) const&
+        requires(etl::is_constructible_v<E, decltype(etl::move(error()))>)
+    {
+        if (has_value()) { return etl::invoke(etl::forward<F>(f), etl::move(**this)); }
+        using U = etl::remove_cvref_t<etl::invoke_result_t<F, decltype(etl::move(**this))>>;
+        return U(etl::unexpect, etl::move(error()));
+    }
+
+    template <typename F>
+    [[nodiscard]] constexpr auto and_then(F&& f) const&&
+        requires(etl::is_constructible_v<E, decltype(etl::move(error()))>)
+    {
+        if (has_value()) { return etl::invoke(etl::forward<F>(f), etl::move(**this)); }
+        using U = etl::remove_cvref_t<etl::invoke_result_t<F, decltype(etl::move(**this))>>;
+        return U(etl::unexpect, etl::move(error()));
+    }
+
+    template <typename F>
+    [[nodiscard]] constexpr auto or_else(F&& f) & requires(etl::is_constructible_v<T, decltype(**this)>)
+    {
+        using G = etl::remove_cvref_t<etl::invoke_result_t<F, decltype(error())>>;
+        if (has_value()) { return G(etl::in_place, **this); }
+        return etl::invoke(etl::forward<F>(f), error());
+    }
+
+    template <typename F>
+    [[nodiscard]] constexpr auto or_else(F&& f) && requires(etl::is_constructible_v<T, decltype(**this)>)
+    {
+        using G = etl::remove_cvref_t<etl::invoke_result_t<F, decltype(error())>>;
+        if (has_value()) { return G(etl::in_place, **this); }
+        return etl::invoke(etl::forward<F>(f), error());
+    }
+
+    template <typename F>
+    [[nodiscard]] constexpr auto or_else(F&& f) const&
+        requires(etl::is_constructible_v<T, decltype(etl::move(**this))>)
+    {
+        using G = etl::remove_cvref_t<etl::invoke_result_t<F, decltype(etl::move(error()))>>;
+        if (has_value()) { return G(etl::in_place, etl::move(**this)); }
+        return etl::invoke(etl::forward<F>(f), etl::move(error()));
+    }
+
+    template <typename F>
+    [[nodiscard]] constexpr auto or_else(F&& f) const&&
+        requires(etl::is_constructible_v<T, decltype(etl::move(**this))>)
+    {
+        using G = etl::remove_cvref_t<etl::invoke_result_t<F, decltype(etl::move(error()))>>;
+        if (has_value()) { return G(etl::in_place, etl::move(**this)); }
+        return etl::invoke(etl::forward<F>(f), etl::move(error()));
     }
 
 private:
