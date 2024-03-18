3f2b8a819e54f59b1c3aaebe3f283569146b48e9
Tobias Hienzsch <post@tobias-hienzsch.de>
2024-03-18 23:39:19 +0100

    [tests] Add ASSERT_SAME_TYPE macro

--- tests/testing/testing.hpp
+++ tests/testing/testing.hpp
@@ -5,8 +5,10 @@
 
 #undef NDEBUG
 #include <etl/cassert.hpp>
+#include <etl/type_traits.hpp>
 
-#define ASSERT(...)          assert((__VA_ARGS__))
-#define ASSERT_NOEXCEPT(...) static_assert(noexcept(__VA_ARGS__), "Operation must be noexcept")
+#define ASSERT(...)           assert((__VA_ARGS__))
+#define ASSERT_NOEXCEPT(...)  static_assert(noexcept(__VA_ARGS__), "Operation must be noexcept")
+#define ASSERT_SAME_TYPE(...) static_assert(etl::is_same_v<__VA_ARGS__>, "Type mismatch")
 
 #endif // TETL_TEST_TESTING_TESTING_HPP
