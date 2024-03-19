278f40bb0cf10e6cada4a01e8d131f0b8326d588
Tobias Hienzsch <post@tobias-hienzsch.de>
2024-03-19 02:58:20 +0100

    [bit] Add tests for reset_bit

--- include/etl/_bit/reset_bit.hpp
+++ include/etl/_bit/reset_bit.hpp
@@ -3,21 +3,16 @@
 #ifndef TETL_BIT_RESET_BIT_HPP
 #define TETL_BIT_RESET_BIT_HPP
 
-#include <etl/_concepts/unsigned_integral.hpp>
+#include <etl/_concepts/standard_unsigned_integer.hpp>
 
 namespace etl {
 
-template <etl::unsigned_integral T>
-[[nodiscard]] constexpr auto reset_bit(T val, T bit) noexcept -> T
+template <etl::standard_unsigned_integer UInt>
+[[nodiscard]] constexpr auto reset_bit(UInt val, UInt bit) noexcept -> UInt
 {
-    return val & (~(T(1) << bit));
+    return static_cast<UInt>(val & static_cast<UInt>(~(UInt(1) << bit)));
 }
 
 } // namespace etl
-
-static_assert(etl::reset_bit(0b00000001U, 0U) == 0b00000000U);
-static_assert(etl::reset_bit(0b00000010U, 1U) == 0b00000000U);
-static_assert(etl::reset_bit(0b00000100U, 2U) == 0b00000000U);
-static_assert(etl::reset_bit(0b00000011U, 1U) == 0b00000001U);
 
 #endif // TETL_BIT_RESET_BIT_HPP
---
+++ tests/bit/reset_bit.t.cpp
@@ -0,0 +1,77 @@
+// SPDX-License-Identifier: BSL-1.0
+
+#include <etl/bit.hpp>
+
+#include <etl/cstddef.hpp>
+#include <etl/cstdint.hpp>
+#include <etl/limits.hpp>
+
+#include "testing/testing.hpp"
+
+namespace {
+
+template <typename T>
+concept has_reset_bit = requires(T val, T bit) { etl::reset_bit(val, bit); };
+
+template <typename T>
+constexpr auto test() -> bool
+{
+    ASSERT_NOEXCEPT(etl::reset_bit(T(1), T(0)));
+    ASSERT_SAME_TYPE(decltype(etl::reset_bit(T(1), T(0))), T);
+
+    ASSERT(etl::reset_bit(T(0b00000001), T(0)) == T(0b00000000));
+    ASSERT(etl::reset_bit(T(0b00000010), T(1)) == T(0b00000000));
+    ASSERT(etl::reset_bit(T(0b00000100), T(2)) == T(0b00000000));
+    ASSERT(etl::reset_bit(T(0b00000011), T(1)) == T(0b00000001));
+
+    return true;
+}
+
+constexpr auto test_all() -> bool
+{
+    ASSERT(has_reset_bit<etl::uint8_t>);
+    ASSERT(has_reset_bit<etl::uint16_t>);
+    ASSERT(has_reset_bit<etl::uint32_t>);
+    ASSERT(has_reset_bit<etl::uint64_t>);
+
+    ASSERT(has_reset_bit<unsigned char>);
+    ASSERT(has_reset_bit<unsigned short>);
+    ASSERT(has_reset_bit<unsigned int>);
+    ASSERT(has_reset_bit<unsigned long>);
+    ASSERT(has_reset_bit<unsigned long long>);
+
+    ASSERT(not has_reset_bit<etl::int8_t>);
+    ASSERT(not has_reset_bit<etl::int16_t>);
+    ASSERT(not has_reset_bit<etl::int32_t>);
+    ASSERT(not has_reset_bit<etl::int64_t>);
+    ASSERT(not has_reset_bit<etl::ptrdiff_t>);
+
+    ASSERT(not has_reset_bit<signed char>);
+    ASSERT(not has_reset_bit<signed short>);
+    ASSERT(not has_reset_bit<signed int>);
+    ASSERT(not has_reset_bit<signed long>);
+    ASSERT(not has_reset_bit<signed long long>);
+
+    ASSERT(not has_reset_bit<bool>);
+    ASSERT(not has_reset_bit<char>);
+    ASSERT(not has_reset_bit<char8_t>);
+    ASSERT(not has_reset_bit<char16_t>);
+    ASSERT(not has_reset_bit<char32_t>);
+
+    ASSERT(test<etl::uint8_t>());
+    ASSERT(test<etl::uint16_t>());
+    ASSERT(test<etl::uint32_t>());
+    ASSERT(test<etl::uint64_t>());
+    ASSERT(test<etl::size_t>());
+
+    return true;
+}
+
+} // namespace
+
+auto main() -> int
+{
+    ASSERT(test_all());
+    static_assert(test_all());
+    return 0;
+}
