149bc5f1b0f7b2699ef4a40a3959daca045b6fe4
Tobias Hienzsch <post@tobias-hienzsch.de>
2024-03-22 01:54:49 +0100

    [memory] Add constexpr to construct_at.

--- include/etl/_memory/construct_at.hpp
+++ include/etl/_memory/construct_at.hpp
@@ -3,16 +3,41 @@
 #ifndef TETL_MEMORY_CONSTRUCT_AT_HPP
 #define TETL_MEMORY_CONSTRUCT_AT_HPP
 
+#include <etl/_config/all.hpp>
+
+#include <etl/_new/operator.hpp>
 #include <etl/_type_traits/declval.hpp>
 #include <etl/_utility/forward.hpp>
+
+#if __has_include(<memory>)
+    // TODO: Only include private header that defines construct_at from each STL
+    // STL = https://github.com/microsoft/STL/blob/main/stl/inc/xutility
+    // libc++ = https://github.com/llvm/llvm-project/blob/main/libcxx/include/__memory/construct_at.h
+    // libstdc++ = https://github.com/gcc-mirror/gcc/blob/master/libstdc%2B%2B-v3/include/bits/stl_construct.h
+    #include <memory>
+#else
+// NOLINTBEGIN
+
+namespace std {
+
+template <typename T, typename... Args, typename = decltype(::new(etl::declval<void*>()) T(etl::declval<Args>()...))>
+constexpr auto construct_at(T* p, Args&&... args) -> T*
+{
+    return ::new (static_cast<void*>(p)) T(TETL_FORWARD(args)...);
+}
+
+} // namespace std
+
+// NOLINTEND
+#endif
 
 namespace etl {
 
 /// \brief Creates a T object initialized with arguments args... at given address p.
-template <typename T, typename... Args, typename = decltype(::new(declval<void*>()) T(declval<Args>()...))>
+template <typename T, typename... Args, typename = decltype(::new(etl::declval<void*>()) T(etl::declval<Args>()...))>
 constexpr auto construct_at(T* p, Args&&... args) -> T*
 {
-    return ::new (static_cast<void*>(p)) T(TETL_FORWARD(args)...);
+    return ::std::construct_at(p, TETL_FORWARD(args)...);
 }
 
 } // namespace etl
