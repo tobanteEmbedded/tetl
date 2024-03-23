983f08a9cb8fa38a72584a94ddd74864a0ecdab2
Tobias Hienzsch <post@tobias-hienzsch.de>
2024-03-23 14:29:14 +0100

    [variant] Use operator<=> for monostate

--- include/etl/_variant/monostate.hpp
+++ include/etl/_variant/monostate.hpp
@@ -3,31 +3,26 @@
 #ifndef TETL_VARIANT_MONOSTATE_HPP
 #define TETL_VARIANT_MONOSTATE_HPP
 
+#include <etl/_compare/strong_ordering.hpp>
+
 namespace etl {
 
 /// \brief Unit type intended for use as a well-behaved empty alternative in
 /// etl::variant. In particular, a variant of non-default-constructible types
 /// may list etl::monostate as its first alternative: this makes the variant
 /// itself default-constructible
-struct monostate { };
+///
+/// \headerfile etl/variant.hpp
+struct monostate {
+    /// \brief All instances of etl::monostate compare equal.
+    [[nodiscard]] friend constexpr auto operator==(monostate /*l*/, monostate /*r*/) noexcept -> bool { return true; }
 
-/// \brief All instances of etl::monostate compare equal.
-[[nodiscard]] constexpr auto operator==(monostate /*lhs*/, monostate /*rhs*/) noexcept -> bool { return true; }
-
-/// \brief All instances of etl::monostate compare equal.
-[[nodiscard]] constexpr auto operator!=(monostate /*lhs*/, monostate /*rhs*/) noexcept -> bool { return false; }
-
-/// \brief All instances of etl::monostate compare equal.
-[[nodiscard]] constexpr auto operator<(monostate /*lhs*/, monostate /*rhs*/) noexcept -> bool { return false; }
-
-/// \brief All instances of etl::monostate compare equal.
-[[nodiscard]] constexpr auto operator>(monostate /*lhs*/, monostate /*rhs*/) noexcept -> bool { return false; }
-
-/// \brief All instances of etl::monostate compare equal.
-[[nodiscard]] constexpr auto operator<=(monostate /*lhs*/, monostate /*rhs*/) noexcept -> bool { return true; }
-
-/// \brief All instances of etl::monostate compare equal.
-[[nodiscard]] constexpr auto operator>=(monostate /*lhs*/, monostate /*rhs*/) noexcept -> bool { return true; }
+    /// \brief All instances of etl::monostate compare equal.
+    [[nodiscard]] friend constexpr auto operator<=>(monostate /*l*/, monostate /*r*/) noexcept -> etl::strong_ordering
+    {
+        return etl::strong_ordering::equal;
+    }
+};
 
 } // namespace etl
 
