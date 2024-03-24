786327e3b638e5e369171f72030f49b8148f0123
Tobias Hienzsch <post@tobias-hienzsch.de>
2024-03-25 00:31:45 +0100

    [utility] Improve forward_like tests

--- tests/utility/forward_like.t.cpp
+++ tests/utility/forward_like.t.cpp
@@ -2,35 +2,128 @@
 
 #include <etl/utility.hpp>
 
-#include <etl/cstdint.hpp>
-#include <etl/type_traits.hpp>
-
 #include "testing/testing.hpp"
 
+namespace {
 template <typename T>
 constexpr auto test() -> bool
 {
-    // mutable l-value
-    auto t = T(0);
-    auto u = T(1);
-    CHECK_FALSE(etl::is_const_v<decltype(etl::forward_like<decltype(t)>(u))>);
-    CHECK(etl::is_rvalue_reference_v<decltype(etl::forward_like<decltype(t)>(u))>);
+    struct Class { };
+
+    using ConstClass = Class const;
+
+    auto val = Class{};
+    CHECK_NOEXCEPT(etl::forward_like<T>(val));
+
+    auto const& constVal = val;
+    CHECK_NOEXCEPT(etl::forward_like<T>(constVal));
+
+    // mutable value
+    CHECK_SAME_TYPE(decltype(etl::forward_like<T>(Class{})), Class&&);
+    CHECK_SAME_TYPE(decltype(etl::forward_like<T>(ConstClass{})), ConstClass&&);
+    CHECK_SAME_TYPE(decltype(etl::forward_like<T>(val)), Class&&);
+    CHECK_SAME_TYPE(decltype(etl::forward_like<T>(constVal)), ConstClass&&);
+    CHECK_SAME_TYPE(decltype(etl::forward_like<T>(etl::move(val))), Class&&);
+    CHECK_SAME_TYPE(decltype(etl::forward_like<T>(etl::move(constVal))), ConstClass&&);
+
+    // const value
+    CHECK_SAME_TYPE(decltype(etl::forward_like<T const>(Class{})), ConstClass&&);
+    CHECK_SAME_TYPE(decltype(etl::forward_like<T const>(ConstClass{})), ConstClass&&);
+    CHECK_SAME_TYPE(decltype(etl::forward_like<T const>(val)), ConstClass&&);
+    CHECK_SAME_TYPE(decltype(etl::forward_like<T const>(constVal)), ConstClass&&);
+    CHECK_SAME_TYPE(decltype(etl::forward_like<T const>(etl::move(val))), ConstClass&&);
+    CHECK_SAME_TYPE(decltype(etl::forward_like<T const>(etl::move(constVal))), ConstClass&&);
+
+    // l-value mutable
+    CHECK_SAME_TYPE(decltype(etl::forward_like<T&>(Class{})), Class&);
+    CHECK_SAME_TYPE(decltype(etl::forward_like<T&>(ConstClass{})), ConstClass&);
+    CHECK_SAME_TYPE(decltype(etl::forward_like<T&>(val)), Class&);
+    CHECK_SAME_TYPE(decltype(etl::forward_like<T&>(constVal)), ConstClass&);
+    CHECK_SAME_TYPE(decltype(etl::forward_like<T&>(etl::move(val))), Class&);
+    CHECK_SAME_TYPE(decltype(etl::forward_like<T&>(etl::move(constVal))), ConstClass&);
+
+    // l-value const
+    CHECK_SAME_TYPE(decltype(etl::forward_like<T const&>(Class{})), ConstClass&);
+    CHECK_SAME_TYPE(decltype(etl::forward_like<T const&>(ConstClass{})), ConstClass&);
+    CHECK_SAME_TYPE(decltype(etl::forward_like<T const&>(val)), ConstClass&);
+    CHECK_SAME_TYPE(decltype(etl::forward_like<T const&>(constVal)), ConstClass&);
+    CHECK_SAME_TYPE(decltype(etl::forward_like<T const&>(etl::move(val))), ConstClass&);
+    CHECK_SAME_TYPE(decltype(etl::forward_like<T const&>(etl::move(constVal))), ConstClass&);
+
+    // r-value mutable
+    CHECK_SAME_TYPE(decltype(etl::forward_like<T&&>(Class{})), Class&&);
+    CHECK_SAME_TYPE(decltype(etl::forward_like<T&&>(ConstClass{})), ConstClass&&);
+    CHECK_SAME_TYPE(decltype(etl::forward_like<T&&>(val)), Class&&);
+    CHECK_SAME_TYPE(decltype(etl::forward_like<T&&>(constVal)), ConstClass&&);
+    CHECK_SAME_TYPE(decltype(etl::forward_like<T&&>(etl::move(val))), Class&&);
+    CHECK_SAME_TYPE(decltype(etl::forward_like<T&&>(etl::move(constVal))), ConstClass&&);
+
+    // r-value const
+    CHECK_SAME_TYPE(decltype(etl::forward_like<T const&&>(Class{})), ConstClass&&);
+    CHECK_SAME_TYPE(decltype(etl::forward_like<T const&&>(ConstClass{})), ConstClass&&);
+    CHECK_SAME_TYPE(decltype(etl::forward_like<T const&&>(val)), ConstClass&&);
+    CHECK_SAME_TYPE(decltype(etl::forward_like<T const&&>(constVal)), ConstClass&&);
+    CHECK_SAME_TYPE(decltype(etl::forward_like<T const&&>(etl::move(val))), ConstClass&&);
+    CHECK_SAME_TYPE(decltype(etl::forward_like<T const&&>(etl::move(constVal))), ConstClass&&);
+
+    {
+        auto value    = T(1);
+        auto&& result = etl::forward_like<Class&>(value);
+        CHECK_SAME_TYPE(decltype(result), T&);
+        CHECK(&result == &value);
+    }
+
+    {
+        auto value    = T(1);
+        auto&& result = etl::forward_like<Class const&>(value);
+        CHECK_SAME_TYPE(decltype(result), T const&);
+        CHECK(&result == &value);
+    }
+
+    {
+        auto value    = T(1);
+        auto&& result = etl::forward_like<Class&&>(value);
+        CHECK_SAME_TYPE(decltype(result), T&&);
+        CHECK(&result == &value);
+    }
+
+    {
+        auto value    = T(1);
+        auto&& result = etl::forward_like<Class const&&>(value);
+        CHECK_SAME_TYPE(decltype(result), T const&&);
+        CHECK(&result == &value);
+    }
 
     return true;
 }
 
 constexpr auto test_all() -> bool
 {
-    CHECK(test<etl::uint8_t>());
-    CHECK(test<etl::int8_t>());
-    CHECK(test<etl::uint16_t>());
-    CHECK(test<etl::int16_t>());
-    CHECK(test<etl::uint32_t>());
-    CHECK(test<etl::int32_t>());
-    CHECK(test<etl::uint64_t>());
-    CHECK(test<etl::int64_t>());
+    CHECK(test<signed char>());
+    CHECK(test<signed short>());
+    CHECK(test<signed int>());
+    CHECK(test<signed long>());
+    CHECK(test<signed long long>());
+
+    CHECK(test<unsigned char>());
+    CHECK(test<unsigned short>());
+    CHECK(test<unsigned int>());
+    CHECK(test<unsigned long>());
+    CHECK(test<unsigned long long>());
+
+    CHECK(test<char>());
+    CHECK(test<char8_t>());
+    CHECK(test<char16_t>());
+    CHECK(test<char32_t>());
+    CHECK(test<wchar_t>());
+
+    CHECK(test<float>());
+    CHECK(test<double>());
+    CHECK(test<long double>());
+
     return true;
 }
+} // namespace
 
 auto main() -> int
 {
