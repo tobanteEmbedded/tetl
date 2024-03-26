edb6c275c8ad027be034b7e65904a804209b1034
Tobias Hienzsch <post@tobias-hienzsch.de>
2024-03-26 14:50:45 +0100

    [cmath] Cleanup fma

--- include/etl/_cmath/fma.hpp
+++ include/etl/_cmath/fma.hpp
@@ -6,64 +6,57 @@
 #include <etl/_config/all.hpp>
 
 #include <etl/_type_traits/is_constant_evaluated.hpp>
+#include <etl/_type_traits/is_same.hpp>
 
 namespace etl {
 
 namespace detail {
-template <typename T>
-[[nodiscard]] constexpr auto fma_fallback(T x, T y, T z) noexcept -> T
-{
-    return (x * y) + z;
-}
+
+inline constexpr struct fma {
+    template <typename Float>
+    [[nodiscard]] constexpr auto operator()(Float x, Float y, Float z) const noexcept -> Float
+    {
+        if (not etl::is_constant_evaluated()) {
+#if __has_builtin(__builtin_fmaf)
+            if constexpr (etl::is_same_v<Float, float>) {
+                return __builtin_fmaf(x, y, z);
+            }
+#endif
+#if __has_builtin(__builtin_fma)
+            if constexpr (etl::is_same_v<Float, double>) {
+                return __builtin_fma(x, y, z);
+            }
+#endif
+#if __has_builtin(__builtin_fmal)
+            if constexpr (etl::is_same_v<Float, long double>) {
+                return __builtin_fmal(x, y, z);
+            }
+#endif
+        }
+
+        return (x * y) + z;
+    }
+} fma;
+
 } // namespace detail
 
 /// \brief Computes (x*y) + z as if to infinite precision and rounded only once
 /// to fit the result type.
 ///
 /// https://en.cppreference.com/w/cpp/numeric/math/fma
-[[nodiscard]] constexpr auto fma(float x, float y, float z) noexcept -> float
-{
-    if (is_constant_evaluated()) {
-        return detail::fma_fallback(x, y, z);
-    }
-#if __has_builtin(__builtin_fmaf)
-    return __builtin_fmaf(x, y, z);
-#else
-    return detail::fma_fallback(x, y, z);
-#endif
-}
+[[nodiscard]] constexpr auto fma(float x, float y, float z) noexcept -> float { return etl::detail::fma(x, y, z); }
 
 /// \brief Computes (x*y) + z as if to infinite precision and rounded only once
 /// to fit the result type.
 ///
 /// https://en.cppreference.com/w/cpp/numeric/math/fma
-[[nodiscard]] constexpr auto fmaf(float x, float y, float z) noexcept -> float
-{
-    if (is_constant_evaluated()) {
-        return detail::fma_fallback(x, y, z);
-    }
-#if __has_builtin(__builtin_fmaf)
-    return __builtin_fmaf(x, y, z);
-#else
-    return detail::fma_fallback(x, y, z);
-#endif
-}
+[[nodiscard]] constexpr auto fmaf(float x, float y, float z) noexcept -> float { return etl::detail::fma(x, y, z); }
 
 /// \brief Computes (x*y) + z as if to infinite precision and rounded only once
 /// to fit the result type.
 ///
 /// https://en.cppreference.com/w/cpp/numeric/math/fma
-[[nodiscard]] constexpr auto fma(double x, double y, double z) noexcept -> double
-{
-    if (is_constant_evaluated()) {
-        return detail::fma_fallback(x, y, z);
-    }
-#if __has_builtin(__builtin_fma)
-    return __builtin_fma(x, y, z);
-#else
-    return detail::fma_fallback(x, y, z);
-#endif
-}
+[[nodiscard]] constexpr auto fma(double x, double y, double z) noexcept -> double { return etl::detail::fma(x, y, z); }
 
 /// \brief Computes (x*y) + z as if to infinite precision and rounded only once
 /// to fit the result type.
@@ -71,14 +64,7 @@ template <typename T>
 /// https://en.cppreference.com/w/cpp/numeric/math/fma
 [[nodiscard]] constexpr auto fma(long double x, long double y, long double z) noexcept -> long double
 {
-    if (is_constant_evaluated()) {
-        return detail::fma_fallback(x, y, z);
-    }
-#if __has_builtin(__builtin_fmal)
-    return __builtin_fmal(x, y, z);
-#else
-    return detail::fma_fallback(x, y, z);
-#endif
+    return etl::detail::fma(x, y, z);
 }
 
 /// \brief Computes (x*y) + z as if to infinite precision and rounded only once
@@ -87,14 +73,7 @@ template <typename T>
 /// https://en.cppreference.com/w/cpp/numeric/math/fma
 [[nodiscard]] constexpr auto fmal(long double x, long double y, long double z) noexcept -> long double
 {
-    if (is_constant_evaluated()) {
-        return detail::fma_fallback(x, y, z);
-    }
-#if __has_builtin(__builtin_fmal)
-    return __builtin_fmal(x, y, z);
-#else
-    return detail::fma_fallback(x, y, z);
-#endif
+    return etl::detail::fma(x, y, z);
 }
 
 } // namespace etl
