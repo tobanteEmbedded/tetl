e6a642584a6036cb476154dbcbb8892654129395
Tobias Hienzsch <post@tobias-hienzsch.de>
2024-04-02 03:13:44 +0200

    [bit] Add overloads to set_bit

--- include/etl/_bit/set_bit.hpp
+++ include/etl/_bit/set_bit.hpp
@@ -17,6 +17,15 @@ template <etl::builtin_unsigned_integer UInt>
     return static_cast<UInt>(word | static_cast<UInt>(UInt(1) << pos));
 }
 
+/// Set bit at position \p pos to \p value
+/// \note Non-standard extension
+/// \ingroup bit
+template <etl::builtin_unsigned_integer UInt>
+[[nodiscard]] constexpr auto set_bit(UInt word, UInt pos, bool value) -> UInt
+{
+    return static_cast<UInt>((word & static_cast<UInt>(~(UInt(1) << pos))) | (UInt(value) << pos));
+}
+
 /// Set bit at position `Pos`
 /// \note Non-standard extension
 /// \ingroup bit
@@ -24,6 +33,15 @@ template <etl::size_t Pos, etl::builtin_unsigned_integer UInt>
 [[nodiscard]] constexpr auto set_bit(UInt word) noexcept -> UInt
 {
     return etl::set_bit(word, static_cast<UInt>(Pos));
+}
+
+/// Set bit at position `Pos` to \p value
+/// \note Non-standard extension
+/// \ingroup bit
+template <etl::size_t Pos, etl::builtin_unsigned_integer UInt>
+[[nodiscard]] constexpr auto set_bit(UInt word, bool value) noexcept -> UInt
+{
+    return etl::set_bit(word, static_cast<UInt>(Pos), value);
 }
 
 } // namespace etl
