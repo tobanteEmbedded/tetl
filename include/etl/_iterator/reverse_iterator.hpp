d64549a8127fd58fdb8927eb548f65e34a414e96
Tobias Hienzsch <post@tobias-hienzsch.de>
2024-04-06 02:33:23 +0200

    [iterator] Add iterator_category to reverse_iterator

--- include/etl/_iterator/reverse_iterator.hpp
+++ include/etl/_iterator/reverse_iterator.hpp
@@ -8,7 +8,7 @@
 
 namespace etl {
 
-/// \brief reverse_iterator is an iterator adaptor that reverses the direction
+/// reverse_iterator is an iterator adaptor that reverses the direction
 /// of a given iterator. In other words, when provided with a bidirectional
 /// iterator, `reverse_iterator` produces a new iterator that moves from the end
 /// to the beginning of the sequence defined by the underlying bidirectional
@@ -20,39 +20,35 @@ namespace etl {
 /// \ingroup iterator
 template <typename Iter>
 struct reverse_iterator {
-    /// The underlying iterator type
-    using iterator_type = Iter;
-    /// The underlying value type
-    using value_type = typename iterator_traits<Iter>::value_type;
-    /// The type of subtracting to iterators
-    using difference_type = typename etl::iterator_traits<Iter>::difference_type;
-    /// The underlying reference type
-    using reference = typename etl::iterator_traits<Iter>::reference;
-    /// The underlying pointer type
-    using pointer = typename etl::iterator_traits<Iter>::pointer;
+    using iterator_type     = Iter;
+    using value_type        = typename iterator_traits<Iter>::value_type;
+    using difference_type   = typename etl::iterator_traits<Iter>::difference_type;
+    using reference         = typename etl::iterator_traits<Iter>::reference;
+    using pointer           = typename etl::iterator_traits<Iter>::pointer;
+    using iterator_category = typename etl::iterator_traits<Iter>::iterator_category;
 
-    /// \brief Constructs a new iterator adaptor.
+    /// Constructs a new iterator adaptor.
     ///
-    /// \details Default constructor. The underlying iterator is
+    /// Default constructor. The underlying iterator is
     /// value-initialized. Operations on the resulting iterator have defined
     /// behavior if and only if the corresponding operations on a
     /// value-initialized Iterator also have defined behavior.
     constexpr reverse_iterator() : _current() { }
 
-    /// \brief Constructs a new iterator adaptor.
+    /// Constructs a new iterator adaptor.
     ///
-    /// \details The underlying iterator is initialized with x.
+    /// The underlying iterator is initialized with x.
     constexpr explicit reverse_iterator(Iter x) : _current(x) { }
 
-    /// \brief Constructs a new iterator adaptor.
+    /// Constructs a new iterator adaptor.
     ///
-    /// \details The underlying iterator is initialized with that of other.
+    /// The underlying iterator is initialized with that of other.
     template <typename Other>
     constexpr reverse_iterator(reverse_iterator<Other> const& other) : _current(other.base())
     {
     }
 
-    /// \brief The underlying iterator is assigned the value of the underlying
+    /// The underlying iterator is assigned the value of the underlying
     /// iterator of other, i.e. other.base().
     template <typename Other>
     constexpr auto operator=(reverse_iterator<Other> const& other) -> reverse_iterator&
@@ -61,27 +57,27 @@ struct reverse_iterator {
         return *this;
     }
 
-    /// \brief Returns the underlying base iterator.
+    /// Returns the underlying base iterator.
     [[nodiscard]] constexpr auto base() const -> Iter { return _current; }
 
-    /// \brief Returns a reference to the element previous to current.
+    /// Returns a reference to the element previous to current.
     constexpr auto operator*() const -> reference
     {
         auto tmp = _current;
         return *--tmp;
     }
 
-    /// \brief Returns a pointer to the element previous to current.
+    /// Returns a pointer to the element previous to current.
     constexpr auto operator->() const -> pointer { return etl::addressof(operator*()); }
 
-    /// \brief Pre-increments by one respectively.
+    /// Pre-increments by one respectively.
     constexpr auto operator++() -> reverse_iterator&
     {
         --_current;
         return *this;
     }
 
-    /// \brief Pre-increments by one respectively.
+    /// Pre-increments by one respectively.
     constexpr auto operator++(int) -> reverse_iterator
     {
         auto tmp(*this);
@@ -89,14 +85,14 @@ struct reverse_iterator {
         return tmp;
     }
 
-    /// \brief Pre-decrements by one respectively.
+    /// Pre-decrements by one respectively.
     constexpr auto operator--() -> reverse_iterator&
     {
         ++_current;
         return *this;
     }
 
-    /// \brief Pre-decrements by one respectively.
+    /// Pre-decrements by one respectively.
     constexpr auto operator--(int) -> reverse_iterator
     {
         auto tmp(*this);
@@ -104,35 +100,34 @@ struct reverse_iterator {
         return tmp;
     }
 
-    /// \brief Returns an iterator which is advanced by n positions.
+    /// Returns an iterator which is advanced by n positions.
     constexpr auto operator+(difference_type n) const -> reverse_iterator { return reverse_iterator(_current - n); }
 
-    /// \brief Advances the iterator by n or -n positions respectively.
+    /// Advances the iterator by n or -n positions respectively.
     constexpr auto operator+=(difference_type n) -> reverse_iterator&
     {
         _current -= n;
         return *this;
     }
 
-    /// \brief Returns an iterator which is advanced by -n positions.
+    /// Returns an iterator which is advanced by -n positions.
     constexpr auto operator-(difference_type n) const -> reverse_iterator { return reverse_iterator(_current + n); }
 
-    /// \brief Advances the iterator by n or -n positions respectively.
+    /// Advances the iterator by n or -n positions respectively.
     constexpr auto operator-=(difference_type n) -> reverse_iterator&
     {
         _current += n;
         return *this;
     }
 
-    /// \brief Returns a reference to the element at specified relative
-    /// location.
+    /// Returns a reference to the element at specified relative location.
     constexpr auto operator[](difference_type n) const -> reference { return *(*this + n); }
 
 private:
     Iter _current;
 };
 
-/// \brief Convenience function template that constructs a etl::reverse_iterator
+/// Convenience function template that constructs a etl::reverse_iterator
 /// for the given iterator i (which must be a LegacyBidirectionalIterator) with
 /// the type deduced from the type of the argument.
 template <typename Iter>
@@ -141,7 +136,7 @@ template <typename Iter>
     return etl::reverse_iterator<Iter>(i);
 }
 
-/// \brief Compares the underlying iterators. Inverse comparisons are applied in
+/// Compares the underlying iterators. Inverse comparisons are applied in
 /// order to take into account that the iterator order is reversed.
 template <typename Iter1, typename Iter2>
 [[nodiscard]] constexpr auto
@@ -150,7 +145,7 @@ operator==(etl::reverse_iterator<Iter1> const& lhs, etl::reverse_iterator<Iter2>
     return lhs.base() == rhs.base();
 }
 
-/// \brief Compares the underlying iterators. Inverse comparisons are applied in
+/// Compares the underlying iterators. Inverse comparisons are applied in
 /// order to take into account that the iterator order is reversed.
 template <typename Iter1, typename Iter2>
 [[nodiscard]] constexpr auto
@@ -159,7 +154,7 @@ operator!=(etl::reverse_iterator<Iter1> const& lhs, etl::reverse_iterator<Iter2>
     return lhs.base() != rhs.base();
 }
 
-/// \brief Compares the underlying iterators. Inverse comparisons are applied in
+/// Compares the underlying iterators. Inverse comparisons are applied in
 /// order to take into account that the iterator order is reversed.
 template <typename Iter1, typename Iter2>
 [[nodiscard]] constexpr auto
@@ -168,7 +163,7 @@ operator<(etl::reverse_iterator<Iter1> const& lhs, etl::reverse_iterator<Iter2> 
     return lhs.base() < rhs.base();
 }
 
-/// \brief Compares the underlying iterators. Inverse comparisons are applied in
+/// Compares the underlying iterators. Inverse comparisons are applied in
 /// order to take into account that the iterator order is reversed.
 template <typename Iter1, typename Iter2>
 [[nodiscard]] constexpr auto
@@ -177,7 +172,7 @@ operator<=(etl::reverse_iterator<Iter1> const& lhs, etl::reverse_iterator<Iter2>
     return lhs.base() <= rhs.base();
 }
 
-/// \brief Compares the underlying iterators. Inverse comparisons are applied in
+/// Compares the underlying iterators. Inverse comparisons are applied in
 /// order to take into account that the iterator order is reversed.
 template <typename Iter1, typename Iter2>
 [[nodiscard]] constexpr auto
@@ -186,7 +181,7 @@ operator>(etl::reverse_iterator<Iter1> const& lhs, etl::reverse_iterator<Iter2> 
     return lhs.base() > rhs.base();
 }
 
-/// \brief Compares the underlying iterators. Inverse comparisons are applied in
+/// Compares the underlying iterators. Inverse comparisons are applied in
 /// order to take into account that the iterator order is reversed.
 template <typename Iter1, typename Iter2>
 [[nodiscard]] constexpr auto
@@ -195,7 +190,7 @@ operator>=(etl::reverse_iterator<Iter1> const& lhs, etl::reverse_iterator<Iter2>
     return lhs.base() >= rhs.base();
 }
 
-/// \brief Returns the iterator it incremented by n.
+/// Returns the iterator it incremented by n.
 template <typename Iter>
 [[nodiscard]] constexpr auto operator+(
     typename reverse_iterator<Iter>::difference_type n,
@@ -205,7 +200,7 @@ template <typename Iter>
     return reverse_iterator<Iter>(it.base() - n);
 }
 
-/// \brief Returns the distance between two iterator adaptors.
+/// Returns the distance between two iterator adaptors.
 template <typename Iterator1, typename Iterator2>
 constexpr auto operator-(reverse_iterator<Iterator1> const& lhs, reverse_iterator<Iterator2> const& rhs) noexcept(
     noexcept(rhs.base() - lhs.base())
