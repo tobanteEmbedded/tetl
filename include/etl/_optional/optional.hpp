afd7234d1450e18d6bce3ba9f84f9650b7f16737
Tobias Hienzsch <post@tobias-hienzsch.de>
2024-04-09 04:19:17 +0200

    [optional] Cleanup

--- include/etl/_optional/optional.hpp
+++ include/etl/_optional/optional.hpp
@@ -539,6 +539,24 @@ private:
     T* _ptr{nullptr};
 };
 
+/// \brief Creates an optional object from value.
+/// \relates optional
+/// \ingroup optional
+template <typename T>
+constexpr auto make_optional(T&& value) -> etl::optional<etl::decay_t<T>>
+{
+    return etl::optional<etl::decay_t<T>>(etl::forward<T>(value));
+}
+
+/// \brief Creates an optional object constructed in-place from args...
+/// \relates optional
+/// \ingroup optional
+template <typename T, typename... Args>
+constexpr auto make_optional(Args&&... args) -> etl::optional<T>
+{
+    return etl::optional<T>(etl::in_place, etl::forward<Args>(args)...);
+}
+
 // One deduction guide is provided for etl::optional to account for the
 // edge cases missed by the implicit deduction guides, in particular,
 // non-copyable arguments and array to pointer conversion.
