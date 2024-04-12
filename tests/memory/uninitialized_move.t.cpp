405a75424105284ad309ca3e5c1af91c2c2bae4c
Tobias Hienzsch <post@tobias-hienzsch.de>
2024-04-12 04:27:08 +0200

    [memory] Add tests for uninitialized_move

---
+++ tests/memory/uninitialized_move.t.cpp
@@ -0,0 +1,48 @@
+// SPDX-License-Identifier: BSL-1.0
+
+#include <etl/memory.hpp>
+
+#include <etl/array.hpp>
+
+#include "testing/testing.hpp"
+
+#if defined(__cpp_exceptions)
+namespace {
+
+bool moveWasCalled = false;
+
+struct ThrowOnMove {
+    ThrowOnMove()  = default;
+    ~ThrowOnMove() = default;
+
+    auto operator=(ThrowOnMove const& other) -> ThrowOnMove& = default;
+    auto operator=(ThrowOnMove&& other) -> ThrowOnMove&      = default;
+
+    ThrowOnMove(ThrowOnMove const& other) = default;
+    ThrowOnMove(ThrowOnMove&& /*other*/)
+    {
+        moveWasCalled = true;
+        throw 1; // NOLINT
+    }
+};
+
+auto test() -> bool
+{
+    auto src  = etl::array<ThrowOnMove, 2>{};
+    auto dest = etl::uninitialized_array<ThrowOnMove, 2>{};
+    CHECK_FALSE(moveWasCalled);
+    etl::uninitialized_move(src.begin(), src.end(), dest.data());
+    CHECK(moveWasCalled);
+    return true;
+}
+
+} // namespace
+
+auto main() -> int
+{
+    CHECK(test());
+    return 0;
+}
+#else
+auto main() -> int { return 0; }
+#endif
