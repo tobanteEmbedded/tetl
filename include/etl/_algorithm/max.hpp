daa38675a4c1a645814f3df3fce62c82344a767f
Tobias Hienzsch <post@tobias-hienzsch.de>
2024-04-17 20:28:42 +0200

    [algorithm] Cleanup min & max

--- include/etl/_algorithm/max.hpp
+++ include/etl/_algorithm/max.hpp
@@ -3,22 +3,24 @@
 #ifndef TETL_ALGORITHM_MAX_HPP
 #define TETL_ALGORITHM_MAX_HPP
 
-namespace etl {
+#include <etl/_functional/less.hpp>
 
-/// \brief Returns the greater of a and b.
-/// \ingroup algorithm
-template <typename Type>
-[[nodiscard]] constexpr auto max(Type const& a, Type const& b) noexcept -> Type const&
-{
-    return (a < b) ? b : a;
-}
+namespace etl {
 
 /// \brief Returns the greater of a and b, using a compare function.
 /// \ingroup algorithm
 template <typename Type, typename Compare>
 [[nodiscard]] constexpr auto max(Type const& a, Type const& b, Compare comp) noexcept -> Type const&
 {
-    return (comp(a, b)) ? b : a;
+    return comp(a, b) ? b : a;
+}
+
+/// \brief Returns the greater of a and b.
+/// \ingroup algorithm
+template <typename Type>
+[[nodiscard]] constexpr auto max(Type const& a, Type const& b) noexcept -> Type const&
+{
+    return etl::max(a, b, etl::less());
 }
 
 } // namespace etl
--- include/etl/_algorithm/min.hpp
+++ include/etl/_algorithm/min.hpp
@@ -3,22 +3,24 @@
 #ifndef TETL_ALGORITHM_MIN_HPP
 #define TETL_ALGORITHM_MIN_HPP
 
-namespace etl {
+#include <etl/_functional/less.hpp>
 
-/// \brief Returns the smaller of a and b.
-/// \ingroup algorithm
-template <typename Type>
-[[nodiscard]] constexpr auto min(Type const& a, Type const& b) noexcept -> Type const&
-{
-    return (b < a) ? b : a;
-}
+namespace etl {
 
 /// \brief Returns the smaller of a and b, using a compare function.
 /// \ingroup algorithm
 template <typename Type, typename Compare>
 [[nodiscard]] constexpr auto min(Type const& a, Type const& b, Compare comp) noexcept -> Type const&
 {
-    return (comp(b, a)) ? b : a;
+    return comp(b, a) ? b : a;
+}
+
+/// \brief Returns the smaller of a and b.
+/// \ingroup algorithm
+template <typename Type>
+[[nodiscard]] constexpr auto min(Type const& a, Type const& b) noexcept -> Type const&
+{
+    return etl::min(a, b, etl::less());
 }
 
 } // namespace etl
