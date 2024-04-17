2e34a254dff6d2eecc09489e31cc9580dd2c1a40
Tobias Hienzsch <post@tobias-hienzsch.de>
2024-04-18 01:12:22 +0200

    [memory] Improve align tests

--- tests/memory/align.t.cpp
+++ tests/memory/align.t.cpp
@@ -11,8 +11,8 @@ namespace {
 auto test() -> bool
 {
     auto buffer = etl::array<char, 128>{};
-    auto space  = buffer.size();
-    auto* ptr   = static_cast<void*>(buffer.data());
+    auto space  = buffer.size() - 1;
+    auto* ptr   = static_cast<void*>(buffer.data() + 1);
     CHECK(etl::align(32, 32, ptr, space) != nullptr);
     CHECK(etl::align(32, buffer.size() * 2, ptr, space) == nullptr);
     return true;
