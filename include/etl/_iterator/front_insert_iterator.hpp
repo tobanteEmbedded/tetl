5936c6c5060ae640721cdfa4cd62088501e28364
Tobias Hienzsch <post@tobias-hienzsch.de>
2024-04-18 04:04:13 +0200

    [iterator] Improve front_inserter tests

--- include/etl/_iterator/front_insert_iterator.hpp
+++ include/etl/_iterator/front_insert_iterator.hpp
@@ -8,18 +8,18 @@
 
 namespace etl {
 
-/// \brief front_insert_iterator is an LegacyOutputIterator that prepends
-/// elements to a container for which it was constructed. The container's
-/// push_front() member function is called whenever the iterator (whether
-/// dereferenced or not) is assigned to. Incrementing the
+/// front_insert_iterator is an LegacyOutputIterator that prepends
+/// elements to a container for which it was constructed.
+///
+/// The container's push_front() member function is called whenever the
+/// iterator (whether dereferenced or not) is assigned to. Incrementing the
 /// front_insert_iterator is a no-op.
 ///
-/// \todo Add tests when a container with push_front has been implemented.
 /// \ingroup iterator
 template <typename Container>
 struct front_insert_iterator {
 protected:
-    Container* container_ = nullptr;
+    Container* _container = nullptr;
 
 public:
     using iterator_category = output_iterator_tag;
@@ -29,50 +29,49 @@ public:
     using reference         = void;
     using container_type    = Container;
 
-    /// \brief Initializes the underlying pointer to container with nullptr.
+    /// Initializes the underlying pointer to container with nullptr.
     constexpr front_insert_iterator() noexcept = default;
 
-    /// \brief Initializes the underlying pointer to the container to
-    /// addressof(c).
+    /// Initializes the underlying pointer to the container to addressof(c).
     constexpr explicit front_insert_iterator(Container& container)
-        : container_{addressof(container)}
+        : _container{etl::addressof(container)}
     {
     }
 
-    /// \brief Inserts the given value value to the container.
+    /// Inserts the given value value to the container.
     constexpr auto operator=(typename Container::value_type const& value) -> front_insert_iterator&
     {
-        container_->push_front(value);
+        _container->push_front(value);
         return *this;
     }
 
-    /// \brief Inserts the given value value to the container.
+    /// Inserts the given value value to the container.
     constexpr auto operator=(typename Container::value_type&& value) -> front_insert_iterator&
     {
-        container_->push_front(etl::move(value));
+        _container->push_front(etl::move(value));
         return *this;
     }
 
-    /// \brief Does nothing, this member function is provided to satisfy the
+    /// Does nothing, this member function is provided to satisfy the
     /// requirements of LegacyOutputIterator. It returns the iterator itself,
     /// which makes it possible to use code such as *iter = value to output
     /// (insert) the value into the underlying container.
     constexpr auto operator*() -> front_insert_iterator& { return *this; }
 
-    /// \brief Does nothing. These operator overloads are provided to satisfy
+    /// Does nothing. These operator overloads are provided to satisfy
     /// the requirements of LegacyOutputIterator. They make it possible for the
     /// expressions *iter++=value and *++iter=value to be used to output
     /// (insert) a value into the underlying container.
     constexpr auto operator++() -> front_insert_iterator& { return *this; }
 
-    /// \brief Does nothing. These operator overloads are provided to satisfy
+    /// Does nothing. These operator overloads are provided to satisfy
     /// the requirements of LegacyOutputIterator. They make it possible for the
     /// expressions *iter++=value and *++iter=value to be used to output
     /// (insert) a value into the underlying container.
     constexpr auto operator++(int) -> front_insert_iterator { return *this; }
 };
 
-/// \brief front_inserter is a convenience function template that constructs a
+/// front_inserter is a convenience function template that constructs a
 /// front_insert_iterator for the container c with the type deduced from
 /// the type of the argument.
 template <typename Container>
--- tests/iterator/insert_iterator.t.cpp
+++ tests/iterator/insert_iterator.t.cpp
@@ -2,16 +2,27 @@
 
 #include <etl/iterator.hpp>
 
-#include <etl/cstdint.hpp>
+#include <etl/algorithm.hpp>
 #include <etl/vector.hpp>
 
 #include "testing/testing.hpp"
+
+namespace {
+
+template <typename T, etl::size_t Capacity>
+struct push_front_vector {
+    using value_type = T;
+    constexpr auto push_front(T const& val) -> void { vector.insert(vector.begin(), val); }
+    constexpr auto push_front(T&& val) -> void { vector.insert(vector.begin(), etl::move(val)); }
+    etl::static_vector<T, Capacity> vector;
+};
 
 template <typename T>
 constexpr auto test() -> bool
 {
     // back_inserter
-    { // "insert rvalue"
+    {
+        // "insert rvalue"
         {
             auto vec  = etl::static_vector<T, 5>{};
             auto iter = etl::back_inserter(vec);
@@ -43,14 +54,20 @@ constexpr auto test() -> bool
             *iter;
             CHECK(vec.size() == 1);
         }
+    }
 
-        // front_inserter
-        {
-            auto vec  = etl::static_vector<T, 5>{};
-            auto iter = etl::front_inserter(vec);
-            CHECK(&++iter == &iter);    // NOLINT
-            CHECK(&++iter == &(*iter)); // NOLINT
-        }
+    // front_inserter
+    {
+        auto src  = etl::static_vector<T, 5>({T(0), T(1), T(2), T(3), T(4)});
+        auto dest = push_front_vector<T, 5>{};
+        etl::copy(src.begin(), src.end(), etl::front_inserter(dest));
+        CHECK(dest.vector.front() == T(4));
+        CHECK(dest.vector.back() == T(0));
+
+        dest.vector.clear();
+        etl::move(src.rbegin(), src.rend(), etl::front_inserter(dest));
+        CHECK(dest.vector.front() == T(0));
+        CHECK(dest.vector.back() == T(4));
     }
 
     return true;
@@ -58,18 +75,32 @@ constexpr auto test() -> bool
 
 constexpr auto test_all() -> bool
 {
-    CHECK(test<etl::int8_t>());
-    CHECK(test<etl::int16_t>());
-    CHECK(test<etl::int32_t>());
-    CHECK(test<etl::int64_t>());
-    CHECK(test<etl::uint8_t>());
-    CHECK(test<etl::uint16_t>());
-    CHECK(test<etl::uint32_t>());
-    CHECK(test<etl::uint64_t>());
+    CHECK(test<signed char>());
+    CHECK(test<signed short>());
+    CHECK(test<signed int>());
+    CHECK(test<signed long>());
+    CHECK(test<signed long long>());
+
+    CHECK(test<unsigned char>());
+    CHECK(test<unsigned short>());
+    CHECK(test<unsigned int>());
+    CHECK(test<unsigned long>());
+    CHECK(test<unsigned long long>());
+
+    CHECK(test<char>());
+    CHECK(test<char8_t>());
+    CHECK(test<char16_t>());
+    CHECK(test<char32_t>());
+    CHECK(test<wchar_t>());
+
     CHECK(test<float>());
     CHECK(test<double>());
+    CHECK(test<long double>());
+
     return true;
 }
+
+} // namespace
 
 auto main() -> int
 {
