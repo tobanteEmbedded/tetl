28da74798c08d2f3aeb6815d5dc6150bac509b8d
Tobias Hienzsch <post@tobias-hienzsch.de>
2024-04-20 11:12:08 +0200

    [contracts] Add TETL_PRECONDITION_SAFE macro

--- include/etl/_contracts/check.hpp
+++ include/etl/_contracts/check.hpp
@@ -4,11 +4,17 @@
 #define TETL_CONTRACTS_CHECK_HPP
 
 #include <etl/_cassert/assert.hpp>
+#include <etl/_type_traits/is_constant_evaluated.hpp>
 
-#if defined(TETL_ENABLE_CONTRACT_CHECKS)
+#if defined(TETL_ENABLE_CONTRACT_CHECKS_SAFE)
+    #define TETL_PRECONDITION_SAFE(...) TETL_ASSERT_IMPL(__VA_ARGS__)
+    #define TETL_PRECONDITION(...)      TETL_ASSERT_IMPL(__VA_ARGS__)
+#elif defined(TETL_ENABLE_CONTRACT_CHECKS)
     #define TETL_PRECONDITION(...) TETL_ASSERT_IMPL(__VA_ARGS__)
+    #define TETL_PRECONDITION_SAFE(...)
 #else
     #define TETL_PRECONDITION(...)
+    #define TETL_PRECONDITION_SAFE(...)
 #endif
 
 #endif // TETL_CONTRACTS_CHECK_HPP
