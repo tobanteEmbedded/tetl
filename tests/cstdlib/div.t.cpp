1c941a97f6004dff58c59805f0469d3315f5cf4f
Tobias Hienzsch <post@tobias-hienzsch.de>
2024-05-01 14:51:11 +0200

    [cstdlib] Improve div tests

--- tests/cstdlib/div.t.cpp
+++ tests/cstdlib/div.t.cpp
@@ -6,17 +6,19 @@
 template <typename T, typename F>
 constexpr auto test(F func) -> bool
 {
-    CHECK(etl::div(T(2), T(1)).quot == T(2));
-    CHECK(etl::div(T(2), T(1)).rem == T(0));
-
-    CHECK(etl::div(T(1), T(2)).quot == T(0));
-    CHECK(etl::div(T(1), T(2)).rem == T(1));
-
     CHECK(func(T(2), T(1)).quot == T(2));
     CHECK(func(T(2), T(1)).rem == T(0));
 
     CHECK(func(T(1), T(2)).quot == T(0));
     CHECK(func(T(1), T(2)).rem == T(1));
+
+    if constexpr (not etl::is_same_v<etl::intmax_t, long long>) {
+        CHECK(etl::div(T(2), T(1)).quot == T(2));
+        CHECK(etl::div(T(2), T(1)).rem == T(0));
+
+        CHECK(etl::div(T(1), T(2)).quot == T(0));
+        CHECK(etl::div(T(1), T(2)).rem == T(1));
+    }
 
     return true;
 }
@@ -26,6 +28,7 @@ constexpr auto test_all() -> bool
     CHECK(test<int>(static_cast<etl::div_t (*)(int, int)>(etl::div)));
     CHECK(test<long>(etl::ldiv));
     CHECK(test<long long>(etl::lldiv));
+    CHECK(test<etl::intmax_t>(etl::imaxdiv));
     return true;
 }
 
