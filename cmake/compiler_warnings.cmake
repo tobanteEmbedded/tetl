81b471ca220314cc3dc6bb86699e182d8962d376
Tobias Hienzsch <post@tobias-hienzsch.de>
2025-03-08 15:02:43 +0100

    Fix MSVC warning surpression

--- cmake/compiler_warnings.cmake
+++ cmake/compiler_warnings.cmake
@@ -5,7 +5,7 @@ if (MSVC)
   target_compile_options(compiler_warnings
     INTERFACE
       /W3
-      /wdC4723
+      /wd4723 # potential divide by 0
       $<$<BOOL:${TETL_BUILD_WERROR}>:/WX>
   )
 else ()
