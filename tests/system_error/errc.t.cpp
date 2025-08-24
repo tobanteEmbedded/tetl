b75305ad96604c587e1893d348614d1dcf92d5b2
Tobias Hienzsch <post@tobias-hienzsch.de>
2025-08-24 23:31:58 +0200

    [system_error] Add tests for errc

---
+++ tests/system_error/errc.t.cpp
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: BSL-1.0
+
+#include "testing/testing.hpp"
+
+#if defined(TETL_ENABLE_CXX_MODULES)
+import etl;
+#else
+    #include <etl/system_error.hpp>
+    #include <etl/type_traits.hpp>
+#endif
+
+static constexpr auto test_all() -> bool
+{
+    CHECK(etl::is_scoped_enum_v<etl::errc>);
+    CHECK(etl::is_error_condition_enum_v<etl::errc>);
+    CHECK(sizeof(etl::errc) == sizeof(unsigned char));
+
+    // default construction should be equal to the first named enumeration
+    CHECK(etl::errc{} != etl::errc::address_family_not_supported);
+
+    return true;
+}
+
+auto main() -> int
+{
+    STATIC_CHECK(test_all());
+    return 0;
+}
