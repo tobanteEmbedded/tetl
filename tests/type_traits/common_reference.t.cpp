640f6d657f775f8d2667442b25e6e80ccf715b79
Tobias Hienzsch <post@tobias-hienzsch.de>
2025-09-05 01:48:15 +0200

    [type_traits] Improve common_reference

--- tests/type_traits/common_reference.t.cpp
+++ tests/type_traits/common_reference.t.cpp
@@ -19,8 +19,18 @@ constexpr auto test() -> bool
     // empty
     CHECK_FALSE(has_common_reference<>);
 
-    // same type
-    CHECK(has_common_reference<T&, T&>);
+    // single type
+    CHECK(has_common_reference<void>);
+    CHECK(has_common_reference<T>);
+    CHECK(has_common_reference<T&>);
+    CHECK(has_common_reference<T const&>);
+    CHECK(has_common_reference<T volatile&>);
+    CHECK(has_common_reference<T const volatile&>);
+    CHECK(has_common_reference<T&&>);
+    CHECK(has_common_reference<T const&&>);
+    CHECK(has_common_reference<T volatile&&>);
+    CHECK(has_common_reference<T const volatile&&>);
+
     CHECK_SAME_TYPE(etl::common_reference_t<void>, void);
     CHECK_SAME_TYPE(etl::common_reference_t<T>, T);
     CHECK_SAME_TYPE(etl::common_reference_t<T&>, T&);
@@ -28,6 +38,24 @@ constexpr auto test() -> bool
     CHECK_SAME_TYPE(etl::common_reference_t<T const>, T const);
     CHECK_SAME_TYPE(etl::common_reference_t<T const&>, T const&);
     CHECK_SAME_TYPE(etl::common_reference_t<T const&&>, T const&&);
+
+    // lvalue-ref type
+    CHECK_SAME_TYPE(etl::common_reference_t<T&, T&>, T&);
+
+    CHECK_SAME_TYPE(etl::common_reference_t<T&, T const&>, T const&);
+    CHECK_SAME_TYPE(etl::common_reference_t<T const&, T&>, T const&);
+    CHECK_SAME_TYPE(etl::common_reference_t<T const&, T const&>, T const&);
+
+    CHECK_SAME_TYPE(etl::common_reference_t<T&, T volatile&>, T volatile&);
+    CHECK_SAME_TYPE(etl::common_reference_t<T volatile&, T&>, T volatile&);
+    CHECK_SAME_TYPE(etl::common_reference_t<T volatile&, T volatile&>, T volatile&);
+
+    CHECK_SAME_TYPE(etl::common_reference_t<T&, T const volatile&>, T const volatile&);
+    CHECK_SAME_TYPE(etl::common_reference_t<T const volatile&, T&>, T const volatile&);
+    CHECK_SAME_TYPE(etl::common_reference_t<T const volatile&, T&>, T const volatile&);
+    CHECK_SAME_TYPE(etl::common_reference_t<T const volatile&, T const volatile&>, T const volatile&);
+    CHECK_SAME_TYPE(etl::common_reference_t<T const volatile&, T const&>, T const volatile&);
+    CHECK_SAME_TYPE(etl::common_reference_t<T const volatile&, T volatile&>, T const volatile&);
 
     return true;
 }
