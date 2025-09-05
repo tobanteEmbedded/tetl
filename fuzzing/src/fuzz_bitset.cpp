d806ecf0fb074b6b76d3033b710fbe0552ba4971
Tobias Hienzsch <post@tobias-hienzsch.de>
2025-09-05 10:58:30 +0200

    [fuzzing] Add targets for bitset & string_view

---
+++ fuzzing/src/fuzz_bitset.cpp
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: BSL-1.0
+
+#include "fuzzing.hpp"
+
+#include <etl/bitset.hpp>
+
+#include <bitset>
+#include <cstdio>
+
+auto fuzz_bitset(FuzzedDataProvider& p) -> int
+{
+    auto const val = p.ConsumeIntegral<etl::uint32_t>();
+
+    auto const eset = etl::bitset<32>{val};
+    auto const sset = std::bitset<32>{val};
+
+    auto const estr = eset.to_string<32>();
+    auto const sstr = sset.to_string();
+
+    auto const eview = etl::string_view{estr.data(), estr.size()};
+    auto const sview = etl::string_view{sstr.data(), sstr.size()};
+
+    auto const elong = eset.to_ullong();
+    auto const slong = sset.to_ullong();
+
+    if ((eview != sview) or (elong != slong)) {
+        std::printf("etl::bitset::to_ullong\n");
+        std::printf("val: '%u'\n", val);
+        std::printf("estr: '%s'\nsstr: '%s'\n", estr.c_str(), sstr.c_str());
+        std::printf("elong: '%llu' slong: '%llu'\n", elong, slong);
+        return 1;
+    }
+
+    return 0;
+}
+
+extern "C" auto LLVMFuzzerTestOneInput(std::uint8_t const* data, std::size_t size) -> int
+{
+    if (size == 0) {
+        return 0;
+    }
+    auto p = FuzzedDataProvider{data, size};
+    RUN(fuzz_bitset(p));
+    return 0;
+}
---
+++ fuzzing/src/fuzz_string_view.cpp
@@ -0,0 +1,81 @@
+// SPDX-License-Identifier: BSL-1.0
+
+#include "fuzzing.hpp"
+
+#include <etl/string_view.hpp>
+
+#include <cstdio>
+#include <string>
+
+// auto fuzz_string_view_compare(FuzzedDataProvider& p) -> int
+// {
+//     auto sign = [](int x) { return x == 0 ? 0 : (x < 0 ? -1 : 1); };
+
+// auto const a = p.ConsumeRandomLengthString(64);
+// auto const b = p.ConsumeRandomLengthString(64);
+
+// auto const eview = etl::string_view{a.data(), a.size()};
+// auto const sview = std::string_view{a.data(), a.size()};
+
+// auto const ecmp = eview.compare(b.c_str());
+// auto const scmp = sview.compare(b.c_str());
+
+// if (sign(ecmp) != sign(scmp)) {
+//     std::printf("etl::string_view::compare\n");
+//     std::printf("this: '%s' str: '%s'\n", a.c_str(), b.c_str());
+//     std::printf("len(this): '%zu' len(str): '%zu'\n", a.size(), b.size());
+//     std::printf("ecmp: '%d' scmp: '%d'\n", ecmp, scmp);
+//     return 1;
+// }
+// return 0;
+// }
+
+auto fuzz_string_view_find(FuzzedDataProvider& p) -> int
+{
+    auto const haystack = p.ConsumeRandomLengthString(64);
+    auto const needle   = p.ConsumeRandomLengthString(64);
+
+    auto const eview = etl::string_view{haystack.data(), haystack.size()};
+    auto const sview = std::string_view{haystack.data(), haystack.size()};
+
+    auto const epos = eview.find(needle.c_str());
+    auto const spos = sview.find(needle.c_str());
+    if (epos != spos) {
+        std::printf("etl::string_view::find\n");
+        std::printf("haystack: '%s' needle: '%s'\n", haystack.c_str(), needle.c_str());
+        std::printf("epos: '%zu' spos: '%zu'\n", epos, spos);
+        return 1;
+    }
+    return 0;
+}
+
+auto fuzz_string_view_rfind(FuzzedDataProvider& p) -> int
+{
+    auto const haystack = p.ConsumeRandomLengthString(64);
+    auto const needle   = p.ConsumeRandomLengthString(64);
+
+    auto const eview = etl::string_view{haystack.data(), haystack.size()};
+    auto const sview = std::string_view{haystack.data(), haystack.size()};
+
+    auto const epos = eview.rfind(needle.c_str());
+    auto const spos = sview.rfind(needle.c_str());
+    if (epos != spos) {
+        std::printf("etl::string_view::rfind\n");
+        std::printf("haystack: '%s' needle: '%s'\n", haystack.c_str(), needle.c_str());
+        std::printf("epos: '%zu' spos: '%zu'\n", epos, spos);
+        return 1;
+    }
+    return 0;
+}
+
+extern "C" auto LLVMFuzzerTestOneInput(std::uint8_t const* data, std::size_t size) -> int
+{
+    if (size == 0) {
+        return 0;
+    }
+    auto p = FuzzedDataProvider{data, size};
+    // RUN(fuzz_string_view_compare(p));
+    RUN(fuzz_string_view_find(p));
+    RUN(fuzz_string_view_rfind(p));
+    return 0;
+}
