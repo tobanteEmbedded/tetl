3bef7793cfcf77ce53acc361e851b3b2d9c215b5
Tobias Hienzsch <post@tobias-hienzsch.de>
2025-09-16 19:52:18 +0200

    [cmath] Add tests for pow

--- include/etl/_cmath/pow.hpp
+++ include/etl/_cmath/pow.hpp
@@ -11,6 +11,52 @@
 
 namespace etl {
 
+namespace detail {
+
+inline constexpr struct pow {
+    template <typename Float>
+    [[nodiscard]] constexpr auto operator()(Float base, Float exponent) const noexcept -> Float
+    {
+        if (is_constant_evaluated()) {
+#if __has_constexpr_builtin(__builtin_powf)
+            if constexpr (etl::same_as<Float, float>) {
+                return __builtin_powf(base, exponent);
+            }
+#endif
+#if __has_constexpr_builtin(__builtin_pow)
+            if constexpr (etl::same_as<Float, double>) {
+                return __builtin_pow(base, exponent);
+            }
+#endif
+#if __has_constexpr_builtin(__builtin_powl)
+            if constexpr (etl::same_as<Float, long double>) {
+                return __builtin_powl(base, exponent);
+            }
+#endif
+        } else {
+#if __has_builtin(__builtin_powf)
+            if constexpr (etl::same_as<Float, float>) {
+                return __builtin_powf(base, exponent);
+            }
+#endif
+#if __has_builtin(__builtin_pow)
+            if constexpr (etl::same_as<Float, double>) {
+                return __builtin_pow(base, exponent);
+            }
+#endif
+#if __has_builtin(__builtin_powl)
+            if constexpr (etl::same_as<Float, long double>) {
+                return __builtin_powl(base, exponent);
+            }
+#endif
+        }
+
+        return etl::detail::gcem::pow(base, exponent);
+    }
+} pow;
+
+} // namespace detail
+
 /// \ingroup cmath
 /// @{
 
@@ -18,78 +64,56 @@ namespace etl {
 /// \details https://en.cppreference.com/w/cpp/numeric/math/pow
 [[nodiscard]] constexpr auto pow(float base, float exp) -> float
 {
-    if (is_constant_evaluated()) {
-#if __has_constexpr_builtin(__builtin_powf)
-        return __builtin_powf(base, exp);
-#else
-        return etl::detail::gcem::pow(base, exp);
-#endif
-    }
-#if __has_builtin(__builtin_powf)
-    return __builtin_powf(base, exp);
-#else
-    return etl::detail::gcem::pow(base, exp);
-#endif
+    return etl::detail::pow(base, exp);
 }
 
 /// Computes the value of base raised to the power exp
 /// \details https://en.cppreference.com/w/cpp/numeric/math/pow
 [[nodiscard]] constexpr auto powf(float base, float exp) -> float
 {
-    return etl::pow(base, exp);
+    return etl::detail::pow(base, exp);
 }
 
 /// Computes the value of base raised to the power exp
 /// \details https://en.cppreference.com/w/cpp/numeric/math/pow
 [[nodiscard]] constexpr auto pow(double base, double exp) -> double
 {
-    if (is_constant_evaluated()) {
-#if __has_constexpr_builtin(__builtin_pow)
-        return __builtin_pow(base, exp);
-#else
-        return etl::detail::gcem::pow(base, exp);
-#endif
-    }
-#if __has_builtin(__builtin_pow)
-    return __builtin_pow(base, exp);
-#else
-    return etl::detail::gcem::pow(base, exp);
-#endif
+    return etl::detail::pow(base, exp);
 }
 
 /// Computes the value of base raised to the power exp
 /// \details https://en.cppreference.com/w/cpp/numeric/math/pow
 [[nodiscard]] constexpr auto pow(long double base, long double exp) -> long double
 {
-    return detail::gcem::pow(base, exp);
+    return etl::detail::pow(base, exp);
 }
 
 /// Computes the value of base raised to the power exp
 /// \details https://en.cppreference.com/w/cpp/numeric/math/pow
 [[nodiscard]] constexpr auto powl(long double base, long double exp) -> long double
 {
-    return etl::pow(base, exp);
+    return etl::detail::pow(base, exp);
 }
 
 /// Computes the value of base raised to the power exp
 /// \details https://en.cppreference.com/w/cpp/numeric/math/pow
 [[nodiscard]] constexpr auto pow(float base, int iexp) -> float
 {
-    return etl::pow(base, static_cast<float>(iexp));
+    return etl::detail::pow(base, static_cast<float>(iexp));
 }
 
 /// Computes the value of base raised to the power exp
 /// \details https://en.cppreference.com/w/cpp/numeric/math/pow
 [[nodiscard]] constexpr auto pow(double base, int iexp) -> double
 {
-    return etl::pow(base, static_cast<double>(iexp));
+    return etl::detail::pow(base, static_cast<double>(iexp));
 }
 
 /// Computes the value of base raised to the power exp
 /// \details https://en.cppreference.com/w/cpp/numeric/math/pow
 [[nodiscard]] constexpr auto pow(long double base, int iexp) -> long double
 {
-    return etl::pow(base, static_cast<long double>(iexp));
+    return etl::detail::pow(base, static_cast<long double>(iexp));
 }
 
 /// @}
