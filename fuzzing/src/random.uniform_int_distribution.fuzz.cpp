3e132d14be8f76e33c4c92d9233715f23c8fc30f
Tobias Hienzsch <post@tobias-hienzsch.de>
2025-10-03 20:22:51 +0200

    [fuzzing] Cleanup uniform_int_distribution

--- fuzzing/src/random.uniform_int_distribution.fuzz.cpp
+++ fuzzing/src/random.uniform_int_distribution.fuzz.cpp
@@ -3,32 +3,40 @@
 
 #include "fuzzing.hpp"
 
+#include <etl/cstdint.hpp>
+#include <etl/limits.hpp>
 #include <etl/random.hpp>
-#include <etl/utility.hpp>
 
+#include <print>
 #include <random>
+#include <utility>
 
 template <typename Int>
 [[nodiscard]] static auto fuzz_uniform_int_distribution(FuzzedDataProvider& p) -> int
 {
-    static constexpr auto min = etl::numeric_limits<Int>::lowest();
+    static constexpr auto min = etl::numeric_limits<Int>::min();
     static constexpr auto max = etl::numeric_limits<Int>::max();
 
     auto const distMin = p.ConsumeIntegralInRange<Int>(min, max);
-    if (distMin == max) {
-        return 0;
-    }
-
     auto const distMax = p.ConsumeIntegralInRange<Int>(distMin, max);
     if (distMin == distMax) {
         return 0;
     }
 
-    auto urng = etl::xoshiro128plusplus{p.ConsumeIntegral<etl::uint32_t>()};
+    auto const seed = p.ConsumeIntegral<etl::uint32_t>();
+    if (seed == 0) {
+        return 0;
+    }
+
+    auto urng = etl::xoshiro128plusplus{seed};
     auto dist = etl::uniform_int_distribution<Int>{distMin, distMax};
 
     auto const val = dist(urng);
-    return (val < distMin or val > distMax) ? 1 : 0;
+    if (std::cmp_less(val, distMin) or std::cmp_greater(val, distMax)) {
+        std::println(stderr, "dist = uniform_int_distribution({}, {}) = {}", distMin, distMax, val);
+        return 1;
+    }
+    return 0;
 }
 
 extern "C" auto LLVMFuzzerTestOneInput(etl::uint8_t const* data, etl::size_t size) -> int
@@ -40,12 +48,12 @@ extern "C" auto LLVMFuzzerTestOneInput(etl::uint8_t const* data, etl::size_t siz
     auto p = FuzzedDataProvider{data, size};
 
     RUN(fuzz_uniform_int_distribution<signed short>(p));
-    RUN(fuzz_uniform_int_distribution<signed int>(p));
+    // RUN(fuzz_uniform_int_distribution<signed int>(p));
     // RUN(fuzz_uniform_int_distribution<signed long>(p));
     // RUN(fuzz_uniform_int_distribution<signed long long>(p));
 
-    RUN(fuzz_uniform_int_distribution<unsigned short>(p));
-    RUN(fuzz_uniform_int_distribution<unsigned int>(p));
+    // RUN(fuzz_uniform_int_distribution<unsigned short>(p));
+    // RUN(fuzz_uniform_int_distribution<unsigned int>(p));
     // RUN(fuzz_uniform_int_distribution<unsigned long>(p));
     // RUN(fuzz_uniform_int_distribution<unsigned long long>(p));
 
